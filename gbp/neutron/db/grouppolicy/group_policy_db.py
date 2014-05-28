c805e5d7d60c26686bdba3eff07c273fb19344d1
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2014-05-27 20:59:07 -0700

    Group Policy DB-1: EP, EPG, L2 Policy, L3 Policy
    (Patch series identifier: GP-DB-1)
    
    This patch includes the DB layer for for Endpoint, Endpoint Group, L2
    Policy, and L3 Policy resources.
    
    In the context of the larger Group Policy Model, the Endpoint Group resource
    references Contracts, which will be introduced in a subsequent patch.
    
    The Plugin layer to back these resources will also be introduced in
    subsequent patches.
    
    Gerrit Spec: https://review.openstack.org/#/c/89469
    
    Partially-implements: blueprint group-based-policy-abstraction
    
    Change-Id: I6e1843e7f765ce004129a5a062b67eb57f3f26f6
    Author:    Sumit Naiksatam <sumitnaiksatam@gmail.com>
    Co-Authored-By:    Bob Kukura <kukura@noironetworks.com>
    Co-Authored-By:    Stephen Wong <s3wong@midokura.com>
    Co-Authored-By:    Mohammad Banikazemi <mb@us.ibm.com>
    Co-Authored-By:    Mandeep Dhami <dhami@noironetworks.com>
    Co-Authored-By:    Ivar Lazzaro <lazzaro@noironetworks.com>

---
+++ gbp/neutron/db/grouppolicy/group_policy_db.py
@@ -0,0 +1,377 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import sqlalchemy as sa
+from sqlalchemy import orm
+from sqlalchemy.orm import exc
+
+from neutron.common import log
+from neutron.db import common_db_mixin
+from neutron.db import model_base
+from neutron.db import models_v2
+from neutron.openstack.common import log as logging
+from neutron.openstack.common import uuidutils
+
+from gbp.neutron.extensions import group_policy as gpolicy
+
+
+LOG = logging.getLogger(__name__)
+MAX_IPV4_SUBNET_PREFIX_LENGTH = 31
+MAX_IPV6_SUBNET_PREFIX_LENGTH = 127
+
+
+class Endpoint(model_base.BASEV2, models_v2.HasId, models_v2.HasTenant):
+
+    """An Endpoint is the lowest unit of abstraction on which a policy is
+    applied. This Endpoint is unrelated to the Endpoint terminology used in
+    Keystone.
+    """
+
+    __tablename__ = 'gp_endpoints'
+    name = sa.Column(sa.String(50))
+    description = sa.Column(sa.String(255))
+    endpoint_group_id = sa.Column(sa.String(36),
+                                  sa.ForeignKey('gp_endpoint_groups.id'),
+                                  nullable=True)
+
+
+class EndpointGroup(model_base.BASEV2, models_v2.HasId, models_v2.HasTenant):
+    """Represents an Endpoint Group that is a collection of endpoints."""
+    __tablename__ = 'gp_endpoint_groups'
+    name = sa.Column(sa.String(50))
+    description = sa.Column(sa.String(255))
+    endpoints = orm.relationship(Endpoint, backref='endpoint_group')
+    l2_policy_id = sa.Column(sa.String(36),
+                             sa.ForeignKey('gp_l2_policies.id'),
+                             nullable=True)
+
+
+class L2Policy(model_base.BASEV2, models_v2.HasId, models_v2.HasTenant):
+    """Represents a L2 Policy for a collection of endpoint_groups."""
+    __tablename__ = 'gp_l2_policies'
+    name = sa.Column(sa.String(50))
+    description = sa.Column(sa.String(255))
+    endpoint_groups = orm.relationship(EndpointGroup, backref='l2_policy')
+    l3_policy_id = sa.Column(sa.String(36),
+                             sa.ForeignKey('gp_l3_policies.id'),
+                             nullable=True)
+
+
+class L3Policy(model_base.BASEV2, models_v2.HasId, models_v2.HasTenant):
+    """Represents a L3 Policy with a non-overlapping IP address space."""
+    __tablename__ = 'gp_l3_policies'
+    name = sa.Column(sa.String(50))
+    description = sa.Column(sa.String(255))
+    ip_version = sa.Column(sa.Integer, nullable=False)
+    ip_pool = sa.Column(sa.String(64), nullable=False)
+    subnet_prefix_length = sa.Column(sa.Integer, nullable=False)
+    l2_policies = orm.relationship(L2Policy, backref='l3_policy')
+
+
+class GroupPolicyDbPlugin(gpolicy.GroupPolicyPluginBase,
+                          common_db_mixin.CommonDbMixin):
+    """GroupPolicy plugin interface implementation using SQLAlchemy models."""
+
+    # TODO(Sumit): native bulk support
+    __native_bulk_support = False
+    __native_pagination_support = True
+    __native_sorting_support = True
+
+    def __init__(self, *args, **kwargs):
+        super(GroupPolicyDbPlugin, self).__init__(*args, **kwargs)
+
+    def _get_endpoint(self, context, endpoint_id):
+        try:
+            return self._get_by_id(context, Endpoint, endpoint_id)
+        except exc.NoResultFound:
+            raise gpolicy.EndpointNotFound(endpoint_id=endpoint_id)
+
+    def _get_endpoint_group(self, context, endpoint_group_id):
+        try:
+            return self._get_by_id(context, EndpointGroup, endpoint_group_id)
+        except exc.NoResultFound:
+            raise gpolicy.EndpointGroupNotFound(
+                endpoint_group_id=endpoint_group_id)
+
+    def _get_l2_policy(self, context, l2_policy_id):
+        try:
+            return self._get_by_id(context, L2Policy, l2_policy_id)
+        except exc.NoResultFound:
+            raise gpolicy.L2PolicyNotFound(l2_policy_id=l2_policy_id)
+
+    def _get_l3_policy(self, context, l3_policy_id):
+        try:
+            return self._get_by_id(context, L3Policy, l3_policy_id)
+        except exc.NoResultFound:
+            raise gpolicy.L3PolicyNotFound(
+                l3_policy_id=l3_policy_id)
+
+    def _make_endpoint_dict(self, ep, fields=None):
+        res = {'id': ep['id'],
+               'tenant_id': ep['tenant_id'],
+               'name': ep['name'],
+               'description': ep['description'],
+               'endpoint_group_id': ep['endpoint_group_id']}
+        return self._fields(res, fields)
+
+    def _make_endpoint_group_dict(self, epg, fields=None):
+        res = {'id': epg['id'],
+               'tenant_id': epg['tenant_id'],
+               'name': epg['name'],
+               'description': epg['description'],
+               'l2_policy_id': epg['l2_policy_id']}
+        res['endpoints'] = [ep['id']
+                            for ep in epg['endpoints']]
+        # TODO(Sumit): populate contracts when supported
+        res['provided_contracts'] = {}
+        res['consumed_contracts'] = {}
+        return self._fields(res, fields)
+
+    def _make_l2_policy_dict(self, l2p, fields=None):
+        res = {'id': l2p['id'],
+               'tenant_id': l2p['tenant_id'],
+               'name': l2p['name'],
+               'description': l2p['description'],
+               'l3_policy_id': l2p['l3_policy_id']}
+        res['endpoint_groups'] = [epg['id']
+                                  for epg in l2p['endpoint_groups']]
+        return self._fields(res, fields)
+
+    def _make_l3_policy_dict(self, l3p, fields=None):
+        res = {'id': l3p['id'],
+               'tenant_id': l3p['tenant_id'],
+               'name': l3p['name'],
+               'description': l3p['description'],
+               'ip_version': l3p['ip_version'],
+               'ip_pool': l3p['ip_pool'],
+               'subnet_prefix_length':
+               l3p['subnet_prefix_length']}
+        res['l2_policies'] = [l2p['id']
+                              for l2p in l3p['l2_policies']]
+        return self._fields(res, fields)
+
+    @staticmethod
+    def validate_subnet_prefix_length(ip_version, new_prefix_length):
+        if (new_prefix_length < 2) or (
+            ip_version == 4 and (
+                new_prefix_length > MAX_IPV4_SUBNET_PREFIX_LENGTH)) or (
+                    ip_version == 6 and (
+                        new_prefix_length > MAX_IPV6_SUBNET_PREFIX_LENGTH)):
+            raise gpolicy.InvalidDefaultSubnetPrefixLength(
+                length=new_prefix_length, protocol=ip_version)
+        # TODO(Sumit): Check that subnet_prefix_length is smaller
+        # than size of the ip_pool's subnet
+
+    @log.log
+    def create_endpoint(self, context, endpoint):
+        ep = endpoint['endpoint']
+        tenant_id = self._get_tenant_id_for_create(context, ep)
+        with context.session.begin(subtransactions=True):
+            ep_db = Endpoint(id=uuidutils.generate_uuid(),
+                             tenant_id=tenant_id,
+                             name=ep['name'],
+                             description=ep['description'],
+                             endpoint_group_id=ep['endpoint_group_id'])
+            context.session.add(ep_db)
+        return self._make_endpoint_dict(ep_db)
+
+    @log.log
+    def update_endpoint(self, context, endpoint_id, endpoint):
+        ep = endpoint['endpoint']
+        with context.session.begin(subtransactions=True):
+            ep_db = self._get_endpoint(context, endpoint_id)
+            ep_db.update(ep)
+        return self._make_endpoint_dict(ep_db)
+
+    @log.log
+    def delete_endpoint(self, context, endpoint_id):
+        with context.session.begin(subtransactions=True):
+            ep_db = self._get_endpoint(context, endpoint_id)
+            context.session.delete(ep_db)
+
+    @log.log
+    def get_endpoint(self, context, endpoint_id, fields=None):
+        ep = self._get_endpoint(context, endpoint_id)
+        return self._make_endpoint_dict(ep, fields)
+
+    @log.log
+    def get_endpoints(self, context, filters=None, fields=None,
+                      sorts=None, limit=None, marker=None,
+                      page_reverse=False):
+        marker_obj = self._get_marker_obj(context, 'endpoint', limit,
+                                          marker)
+        return self._get_collection(context, Endpoint,
+                                    self._make_endpoint_dict,
+                                    filters=filters, fields=fields,
+                                    sorts=sorts, limit=limit,
+                                    marker_obj=marker_obj,
+                                    page_reverse=page_reverse)
+
+    @log.log
+    def get_endpoints_count(self, context, filters=None):
+        return self._get_collection_count(context, Endpoint,
+                                          filters=filters)
+
+    @log.log
+    def create_endpoint_group(self, context, endpoint_group):
+        epg = endpoint_group['endpoint_group']
+        tenant_id = self._get_tenant_id_for_create(context, epg)
+        with context.session.begin(subtransactions=True):
+            epg_db = EndpointGroup(id=uuidutils.generate_uuid(),
+                                   tenant_id=tenant_id,
+                                   name=epg['name'],
+                                   description=epg['description'],
+                                   l2_policy_id=epg['l2_policy_id'])
+            # TODO(Sumit): handle contracts when supported
+            context.session.add(epg_db)
+        return self._make_endpoint_group_dict(epg_db)
+
+    @log.log
+    def update_endpoint_group(self, context, endpoint_group_id,
+                              endpoint_group):
+        epg = endpoint_group['endpoint_group']
+        with context.session.begin(subtransactions=True):
+            epg_db = self._get_endpoint_group(context, endpoint_group_id)
+            # TODO(Sumit): hanndle contracts when supported
+            epg_db.update(epg)
+        return self._make_endpoint_group_dict(epg_db)
+
+    @log.log
+    def delete_endpoint_group(self, context, endpoint_group_id):
+        with context.session.begin(subtransactions=True):
+            epg_db = self._get_endpoint_group(context, endpoint_group_id)
+            context.session.delete(epg_db)
+
+    @log.log
+    def get_endpoint_group(self, context, endpoint_group_id, fields=None):
+        epg = self._get_endpoint_group(context, endpoint_group_id)
+        return self._make_endpoint_group_dict(epg, fields)
+
+    @log.log
+    def get_endpoint_groups(self, context, filters=None, fields=None,
+                            sorts=None, limit=None, marker=None,
+                            page_reverse=False):
+        marker_obj = self._get_marker_obj(context, 'endpoint_group', limit,
+                                          marker)
+        return self._get_collection(context, EndpointGroup,
+                                    self._make_endpoint_group_dict,
+                                    filters=filters, fields=fields,
+                                    sorts=sorts, limit=limit,
+                                    marker_obj=marker_obj,
+                                    page_reverse=page_reverse)
+
+    @log.log
+    def get_endpoint_groups_count(self, context, filters=None):
+        return self._get_collection_count(context, EndpointGroup,
+                                          filters=filters)
+
+    @log.log
+    def create_l2_policy(self, context, l2_policy):
+        l2p = l2_policy['l2_policy']
+        tenant_id = self._get_tenant_id_for_create(context, l2p)
+        with context.session.begin(subtransactions=True):
+            l2p_db = L2Policy(id=uuidutils.generate_uuid(),
+                              tenant_id=tenant_id, name=l2p['name'],
+                              description=l2p['description'],
+                              l3_policy_id=l2p['l3_policy_id'])
+            context.session.add(l2p_db)
+        return self._make_l2_policy_dict(l2p_db)
+
+    @log.log
+    def update_l2_policy(self, context, l2_policy_id, l2_policy):
+        l2p = l2_policy['l2_policy']
+        with context.session.begin(subtransactions=True):
+            l2p_db = self._get_l2_policy(context, l2_policy_id)
+            l2p_db.update(l2p)
+        return self._make_l2_policy_dict(l2p_db)
+
+    @log.log
+    def delete_l2_policy(self, context, l2_policy_id):
+        with context.session.begin(subtransactions=True):
+            l2p_db = self._get_l2_policy(context, l2_policy_id)
+            context.session.delete(l2p_db)
+
+    @log.log
+    def get_l2_policy(self, context, l2_policy_id, fields=None):
+        l2p = self._get_l2_policy(context, l2_policy_id)
+        return self._make_l2_policy_dict(l2p, fields)
+
+    @log.log
+    def get_l2_policies(self, context, filters=None, fields=None,
+                        sorts=None, limit=None, marker=None,
+                        page_reverse=False):
+        marker_obj = self._get_marker_obj(context, 'l2_policy', limit,
+                                          marker)
+        return self._get_collection(context, L2Policy,
+                                    self._make_l2_policy_dict,
+                                    filters=filters, fields=fields,
+                                    sorts=sorts, limit=limit,
+                                    marker_obj=marker_obj,
+                                    page_reverse=page_reverse)
+
+    @log.log
+    def get_l2_policies_count(self, context, filters=None):
+        return self._get_collection_count(context, L2Policy,
+                                          filters=filters)
+
+    @log.log
+    def create_l3_policy(self, context, l3_policy):
+        l3p = l3_policy['l3_policy']
+        tenant_id = self._get_tenant_id_for_create(context, l3p)
+        self.validate_subnet_prefix_length(
+            l3p['ip_version'], l3p['subnet_prefix_length'])
+        with context.session.begin(subtransactions=True):
+            l3p_db = L3Policy(
+                id=uuidutils.generate_uuid(),
+                tenant_id=tenant_id, name=l3p['name'],
+                description=l3p['description'],
+                ip_version=l3p['ip_version'],
+                ip_pool=l3p['ip_pool'],
+                subnet_prefix_length=l3p['subnet_prefix_length'])
+            context.session.add(l3p_db)
+        return self._make_l3_policy_dict(l3p_db)
+
+    @log.log
+    def update_l3_policy(self, context, l3_policy_id, l3_policy):
+        l3p = l3_policy['l3_policy']
+        with context.session.begin(subtransactions=True):
+            l3p_db = self._get_l3_policy(context, l3_policy_id)
+            if 'subnet_prefix_length' in l3p:
+                self.validate_subnet_prefix_length(
+                    l3p_db.ip_version,
+                    l3p['subnet_prefix_length'])
+            l3p_db.update(l3p)
+        return self._make_l3_policy_dict(l3p_db)
+
+    @log.log
+    def delete_l3_policy(self, context, l3_policy_id):
+        with context.session.begin(subtransactions=True):
+            l3p_db = self._get_l3_policy(context, l3_policy_id)
+            context.session.delete(l3p_db)
+
+    @log.log
+    def get_l3_policy(self, context, l3_policy_id, fields=None):
+        l3p = self._get_l3_policy(context, l3_policy_id)
+        return self._make_l3_policy_dict(l3p, fields)
+
+    @log.log
+    def get_l3_policies(self, context, filters=None, fields=None,
+                        sorts=None, limit=None, marker=None,
+                        page_reverse=False):
+        marker_obj = self._get_marker_obj(context, 'l2_policy', limit,
+                                          marker)
+        return self._get_collection(context, L3Policy,
+                                    self._make_l3_policy_dict,
+                                    filters=filters, fields=fields,
+                                    sorts=sorts, limit=limit,
+                                    marker_obj=marker_obj,
+                                    page_reverse=page_reverse)
