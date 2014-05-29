afc551c3ab46ef389c8ca806ea0d1db2542abf87
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2014-05-29 01:10:19 -0700

    Group Policy Plugin-1: EP, EPG, L2 Policy, L3 Policy
    (Patch series identifier: GP-PLG-1)
    
    This patch includes the plugin and the policy driver framework to realize the
    Endpoint, Endpoint Group, L2 Policy, and L3 Policy resources.
    
    In the context of the larger Group Policy Model, the Endpoint Group resource
    references Contracts, which will be introduced in a subsequent patch.
    
    Gerrit Spec: https://review.openstack.org/#/c/89469
    
    Partially-implements: blueprint group-based-policy-abstraction
    
    Change-Id: Iffaf41d84e3acab6cf628ac0305489e4177c05de
    Author:    Sumit Naiksatam <sumitnaiksatam@gmail.com>
    Co-Authored-By: Bob Kukura <kukura@noironetworks.com>
    Co-Authored-By: Stephen Wong <s3wong@midokura.com>
    Co-Authored-By: Mohammad Banikazemi <mb@us.ibm.com>
    Co-Authored-By: Mandeep Dhami <dhami@noironetworks.com>

---
+++ gbp/neutron/tests/unit/services/grouppolicy/test_grouppolicy_plugin.py
@@ -0,0 +1,38 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from oslo.config import cfg
+
+import gbp.neutron.tests.unit.db.grouppolicy.test_group_policy_db as tdb
+
+
+cfg.CONF.import_opt('policy_drivers',
+                    'gbp.neutron.services.grouppolicy.config',
+                    group='group_policy')
+GP_PLUGIN_KLASS = (
+    "gbp.neutron.services.grouppolicy.plugin.GroupPolicyPlugin"
+)
+
+
+class GroupPolicyPluginTestCase(tdb.GroupPolicyDbTestCase):
+
+    def setUp(self, core_plugin=None, gp_plugin=None, ext_mgr=None):
+        super(GroupPolicyPluginTestCase, self).setUp(
+            gp_plugin=GP_PLUGIN_KLASS
+        )
+
+
+class TestGroupPolicyPluginGroupResources(
+    GroupPolicyPluginTestCase, tdb.TestGroupResources):
+
+    pass
