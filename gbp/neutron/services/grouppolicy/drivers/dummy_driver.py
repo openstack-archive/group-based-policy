248c950ebccfd8b9d0d277a5872414c26c0d972f
Ivar Lazzaro <ivarlazzaro@gmail.com>
2014-09-25 13:13:33 -0700

    Group Policy Plugin-1: EP, EPG, L2 Policy, L3 Policy
    (Patch series identifier: GP-PLG-1)
    This patch includes the plugin and the policy driver framework to realize the
    Endpoint, Endpoint Group, L2 Policy, and L3 Policy resources.
    In the context of the larger Group Policy Model, the Endpoint Group resource
    references Contracts, which will be introduced in a subsequent patch.
    Gerrit Spec: https://review.openstack.org/#/c/89469
    Partially-implements: blueprint group-based-policy-abstraction
    Change-Id: Iffaf41d84e3acab6cf628ac0305489e4177c05de
    Author:    Sumit Naiksatam <sumitnaiksatam@gmail.com>
    Co-Authored-By: Bob Kukura <kukura@noironetworks.com>
    Co-Authored-By: Stephen Wong <s3wong@midokura.com>
    Co-Authored-By: Mohammad Banikazemi <mb@us.ibm.com>
    Co-Authored-By: Mandeep Dhami <dhami@noironetworks.com>

---
+++ gbp/neutron/services/grouppolicy/drivers/dummy_driver.py
@@ -0,0 +1,118 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron.common import log
+
+from gbp.neutron.services.grouppolicy import group_policy_driver_api as api
+
+
+class NoopDriver(api.PolicyDriver):
+
+    @log.log
+    def initialize(self):
+        pass
+
+    @log.log
+    def create_endpoint_precommit(self, context):
+        pass
+
+    @log.log
+    def create_endpoint_postcommit(self, context):
+        pass
+
+    @log.log
+    def update_endpoint_precommit(self, context):
+        pass
+
+    @log.log
+    def update_endpoint_postcommit(self, context):
+        pass
+
+    @log.log
+    def delete_endpoint_precommit(self, context):
+        pass
+
+    @log.log
+    def delete_endpoint_postcommit(self, context):
+        pass
+
+    @log.log
+    def create_endpoint_group_precommit(self, context):
+        pass
+
+    @log.log
+    def create_endpoint_group_postcommit(self, context):
+        pass
+
+    @log.log
+    def update_endpoint_group_precommit(self, context):
+        pass
+
+    @log.log
+    def update_endpoint_group_postcommit(self, context):
+        pass
+
+    @log.log
+    def delete_endpoint_group_precommit(self, context):
+        pass
+
+    @log.log
+    def delete_endpoint_group_postcommit(self, context):
+        pass
+
+    @log.log
+    def create_l2_policy_precommit(self, context):
+        pass
+
+    @log.log
+    def create_l2_policy_postcommit(self, context):
+        pass
+
+    @log.log
+    def update_l2_policy_precommit(self, context):
+        pass
+
+    @log.log
+    def update_l2_policy_postcommit(self, context):
+        pass
+
+    @log.log
+    def delete_l2_policy_precommit(self, context):
+        pass
+
+    @log.log
+    def delete_l2_policy_postcommit(self, context):
+        pass
+
+    @log.log
+    def create_l3_policy_precommit(self, context):
+        pass
+
+    @log.log
+    def create_l3_policy_postcommit(self, context):
+        pass
+
+    @log.log
+    def update_l3_policy_precommit(self, context):
+        pass
+
+    @log.log
+    def update_l3_policy_postcommit(self, context):
+        pass
+
+    @log.log
+    def delete_l3_policy_precommit(self, context):
+        pass
+
+    @log.log
+    def delete_l3_policy_postcommit(self, context):
+        pass
