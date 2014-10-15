70d458f3d6993c5ec15d17f47a2bc1269a4516a9
Magesh GV <mageshgv@gmail.com>
2014-10-15 10:10:53 +0530

    GBP Service Chaining Plugin
    This patch implements service plugin for Service Chaining in GBP.
    This patch also introduces a Noop Driver for the Plugin
    A reference driver implementation will be introduced in a subsequent patch
    
    Gerrit Spec: https://review.openstack.org/#/c/125876/
    Partially-Implements: blueprint group-based-policy-service-chaining
    
    Change-Id: I6a89ed4129080c6e081b32cd94026f217cbfbc13

---
+++ gbp/neutron/tests/unit/services/servicechain/test_servicechain_plugin.py
@@ -0,0 +1,39 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from oslo.config import cfg
+
+import gbp.neutron.tests.unit.db.grouppolicy.test_servicechain_db as\
+                                                    test_servicechain_db
+
+cfg.CONF.import_opt('servicechain_drivers',
+                    'gbp.neutron.services.servicechain.config',
+                    group='servicechain')
+SC_PLUGIN_KLASS = (
+    "gbp.neutron.services.servicechain.servicechain_plugin.ServiceChainPlugin"
+)
+
+
+class ServiceChainPluginTestCase(test_servicechain_db.ServiceChainDbTestCase):
+
+    def setUp(self, core_plugin=None, sc_plugin=None):
+        if not sc_plugin:
+            sc_plugin = SC_PLUGIN_KLASS
+        super(ServiceChainPluginTestCase, self).setUp(core_plugin=core_plugin,
+                                                      sc_plugin=sc_plugin)
+
+
+class TestGroupPolicyPluginGroupResources(
+                    ServiceChainPluginTestCase,
+                    test_servicechain_db.TestServiceChainResources):
+    pass
