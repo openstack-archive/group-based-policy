6ef01752ab56a5c5962596a9c2938cf8d42763e8
Magesh GV <mageshgv@gmail.com>
2014-10-26 12:41:54 +0530

    One Convergence NVSD ServiceChain Driver(WIP)
    This patch adds the implementation of Service Chain driver for
    One Convergence NVSD Controller
    
    Change-Id: I10fd4c51cf13bd9c0a54f9168048d510e0e07d06
    Implements: blueprint gbp-oc-nvsd-servicechain-driver

---
+++ gbp/neutron/services/servicechain/drivers/oneconvergence_servicechain_driver.py
@@ -0,0 +1,334 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import eventlet
+eventlet.monkey_patch()
+from oslo.config import cfg
+import copy
+import sqlalchemy as sa
+
+from gbp.neutron.services.grouppolicy.drivers.oneconvergence import\
+                                                         nvsd_gbp_api
+from gbp.neutron.services.servicechain.drivers import simplechain_driver
+
+from neutron.common import log
+from neutron.db import model_base
+from neutron.openstack.common import log as logging
+from neutron.openstack.common import periodic_task
+
+from heatclient import client as heat_client
+
+LOG = logging.getLogger(__name__)
+
+
+#TODO:Remove update support for now till its properly added
+#Raise an exception in update_precommit
+class ServiceChainInstancePolicyMap(model_base.BASEV2):
+    """NVSD Policy attached to the Service Chain Instance."""
+
+    __tablename__ = 'nvsd_sc_instance_policy'
+    instance_id = sa.Column(sa.String(36),
+                            nullable=False, primary_key=True)
+    policy_id = sa.Column(sa.String(36),
+                          nullable=False, primary_key=True)
+
+
+class PendingServiceChainInsertions(object):
+    """Encapsulates a ServiceChain Insertion Operation"""
+    
+    def __init__(self, context, node_stacks, chain_instance_id,
+                 provider_epg, consumer_epg, classifier):
+        self.context = context
+        self.node_stacks = node_stacks
+        self.chain_instance_id = chain_instance_id
+        self.provider_epg = provider_epg
+        self.consumer_epg = consumer_epg
+        self.classifier = classifier
+
+
+class OneconvergenceServiceChainDriver(simplechain_driver.SimpleChainDriver, periodic_task.PeriodicTasks):
+
+    STATUSES = (CREATE_IN_PROGRESS, CREATE_FAILED, CREATE_COMPLETE
+                ) = ('CREATE_IN_PROGRESS', 'CREATE_FAILED', 'CREATE_COMPLETE')
+
+    def __init__(self):
+        self.pending_chain_insertions = list()
+        self.nvsd_api = nvsd_gbp_api.NVSDServiceApi()
+
+    @log.log
+    def create_servicechain_spec_precommit(self, context):
+        super(OneconvergenceServiceChainDriver,
+              self).create_servicechain_spec_precommit(context)
+
+    @log.log
+    def update_servicechain_spec_postcommit(self, context):
+        filters = {'servicechain_spec': [context._original_sc_spec['id']]}
+        sc_instances = context._plugin.get_servicechain_instances(
+                                    context._plugin_context, filters)
+        if sc_instances:
+            self._update_servicechain_instance(context,
+                                               sc_instances[0],
+                                               context._sc_spec)
+
+    @log.log
+    def create_servicechain_instance_postcommit(self, context):
+        super(OneconvergenceServiceChainDriver,
+              self).create_servicechain_instance_postcommit(context)
+        node_stacks = self._get_chain_stacks(context._plugin_context.session,
+                                           context.current['id'])
+        thread_context = copy.copy(context._plugin_context)
+        pendinginsertion = PendingServiceChainInsertions(thread_context,
+                                                         node_stacks,
+                                                         context.current['id'],
+                                                         context.current['provider_epg'],
+                                                         context.current['consumer_epg'],
+                                                         context.current['classifier'])
+        #FIXME(Magesh): periodic_tasks doesnt work. So using threads instead
+        #self.pending_chain_insertions.append(pendinginsertion)
+        eventlet.spawn_n(self._process_chain_processing, pendinginsertion)
+
+    @log.log
+    def update_servicechain_instance_postcommit(self, context):
+        original_spec_id = context._original_sc_instance.get(
+                                                    'servicechain_spec')
+        new_spec_id = context._sc_instance.get('servicechain_spec')
+        if original_spec_id != new_spec_id:
+            newspec = context._plugin.get_servicechain_spec(
+                                    context._plugin_context, new_spec_id)
+            self._update_servicechain_instance(context, context._sc_instance,
+                                               newspec)
+
+    @log.log
+    def delete_servicechain_instance_postcommit(self, context):
+        self.delete_nvsd_policy(context, context.current['id'])
+        self._delete_chain_policy_map(context._plugin_context.session,
+                                      context.current['id'])
+        super(OneconvergenceServiceChainDriver,
+              self).delete_servicechain_instance_postcommit(context)
+    
+    def _update_servicechain_instance(self, context, sc_instance, newspec):
+        self._delete_servicechain_instance_stacks(context._plugin_context,
+                                                  sc_instance['id'])
+        #Delete Policy and create new policy
+        sc_node_ids = newspec.get('nodes')
+        self._create_servicechain_instance_stacks(context,
+                                                  sc_node_ids,
+                                                  sc_instance)
+        node_stacks = self._get_chain_stacks(context._plugin_context.session,
+                                                     context.current['id'])
+        for node_stack in node_stacks:
+            stack_resources = HeatClient(context._plugin_context).resources.list(node_stack.stack_id)
+            for resource in stack_resources:
+                if resource.resource_type == "OC::ES::Service":
+                    service_id = resource.physical_resource_id
+    
+    def _delete_chain_policy_map(self, session, sc_instance_id):
+        with session.begin(subtransactions=True):
+            policy_id = session.query(ServiceChainInstancePolicyMap
+                                   ).filter_by(instance_id=sc_instance_id
+                                               ).one()
+            session.delete(policy_id)
+    
+    def _add_chain_policy_map(self, session, sc_instance_id, policy_id):
+        with session.begin(subtransactions=True):
+            chain_policy_map = ServiceChainInstancePolicyMap(
+                                            instance_id=sc_instance_id,
+                                            policy_id=policy_id)
+            session.add(chain_policy_map)
+
+    def _get_chain_policy_map(self, session, sc_instance_id):
+        with session.begin(subtransactions=True):
+            chain_policy_map = session.query(ServiceChainInstancePolicyMap
+                                      ).filter_by(instance_id=sc_instance_id
+                                                  ).one()
+        return chain_policy_map
+
+    #FIXME(Magesh): This doesnt work. So using threads instead
+    @periodic_task.periodic_task(run_immediately=True)
+    def periodic_chain_processing_task(self):
+        try:
+            if self.pending_chain_insertions:
+                #TODO(magesh): Avoid copying the list
+                for pending_chain in self.pending_chain_insertions[:]:
+                    if(self._perform_service_insertion(pending_chain)):
+                        self.pending_chain_insertions.remove(pending_chain)
+        except Exception as err:
+            LOG.error(_("Failed to process a pending Service Chain Insertion "
+                        "Error : %s"), err)
+
+    def _process_chain_processing(self, pending_chain):
+        while True:
+            if self._perform_service_insertion(pending_chain):
+                return
+
+    def nvsd_get_service(self, context, service_id):
+        return self.nvsd_api.get_nvsd_service(context,
+                                              service_id)
+
+    def create_nvsd_policy(self, context, left_group, right_group,
+                           classifier_id, nvsd_action_list):
+        #Create rule and policy in SC with the classifier and action list
+        rule_ids = []
+        #FIXME:NVSD doesnt accept multiple actions, but still needs actions
+        #as a list
+        for action in nvsd_action_list:
+            body = {'tenant_id': context.tenant,
+                    'user_id': context.user,
+                    'classifier': classifier_id,
+                    'actions': [action],
+                    'policies_attached': []}
+            rule = self.nvsd_api.create_policy_rule(context,
+                                                    body)
+            rule_ids.append(rule.get("id"))
+
+        body = {'tenant_id': context.tenant,
+                'user_id': context.user,
+                'left_group': left_group,
+                'right_group': right_group,
+                'rules': rule_ids}
+        nvsd_policy = self.nvsd_api.create_policy(context,
+                                                  body)
+        return nvsd_policy.get('id')
+
+    def delete_nvsd_policy(self, context, sc_instance_id):
+        chain_nvsd_policy_map = self._get_chain_policy_map(
+                            context._plugin_context.session, sc_instance_id)
+        nvsd_policy_id = chain_nvsd_policy_map.policy_id
+        nvsd_policy = self.nvsd_api.get_policy(context._plugin_context,
+                                               nvsd_policy_id)
+        self.nvsd_api.delete_policy(context._plugin_context,
+                                    nvsd_policy_id)
+        #TODO(Magesh): Check if this sequence works on NVSD
+        for rule_id in nvsd_policy.get("rules"):
+            rule = self.nvsd_api.get_policy_rule(context._plugin_context, rule_id)
+            self.nvsd_api.delete_policy_rule(context._plugin_context, rule_id)
+            for action_id in rule.get("actions"):
+                self.nvsd_api.delete_policy_action(context._plugin_context, action_id)    
+
+    def checkStackStatus(self, context, node_stacks):
+        for node_stack in node_stacks:
+            stack = HeatClient(context).stacks.get(node_stack.stack_id)
+            #CREATE_COMPLETE, CREATE_IN_PROGRESS, CREATE_FAILED
+            if stack.stack_status == self.CREATE_IN_PROGRESS:
+                return self.CREATE_IN_PROGRESS
+            elif stack.stack_status == self.CREATE_FAILED:
+                return self.CREATE_FAILED
+            elif stack.stack_status != self.CREATE_COMPLETE:
+                return self.CREATE_FAILED
+        return self.CREATE_COMPLETE
+    
+    def _fetch_serviceids_from_stack(self, context, node_stacks,
+                                     chain_instance_id):
+        #TODO(Magesh): Storing Service IDs in DB required ??
+        service_ids = []
+        for node_stack in node_stacks:
+            stack_resources = HeatClient(context).resources.list(
+                                                    node_stack.stack_id)
+            for resource in stack_resources:
+                if resource.resource_type == "OC::ES::Service":
+                    service_id = resource.physical_resource_id
+                    service_ids.append(service_id)
+                    break
+        return service_ids
+    
+    def create_nvsd_action(self, context, action_body):
+        return self.nvsd_api.create_policy_action(context,
+                                             action_body)
+    
+    def _create_nvsd_services_action(self, context, service_ids):
+        nvsd_action_list = []
+        copy_action = None
+        l2redirect_action = None
+        for service_id in service_ids:
+            service = self.nvsd_get_service(context, service_id)
+            if service['insertion_mode'] == "L2":
+                if not l2redirect_action:
+                    l2redirect_action = {"action_type": "L2REDIRECT",
+                                         'tenant_id': context.tenant,
+                                         'user_id': context.user,
+                                         "action_value_list": [{"service": service_id}]}
+                else:
+                    l2redirect_action['action_value_list'].append({"service": service_id})
+            elif service['insertion_mode'] == "TAP":
+                copy_action = {"action_type": "TAP",
+                               'tenant_id': context.tenant,
+                               'user_id': context.user,
+                               "action_value": service_id}
+        #Supporting only one TAP in a chain
+        if copy_action:
+            action = self.create_nvsd_action(context, copy_action)
+            nvsd_action_list.append(action['id'])
+        if l2redirect_action:
+            action = self.create_nvsd_action(context, l2redirect_action)
+            nvsd_action_list.append(action['id'])
+        return nvsd_action_list
+
+
+    def _perform_service_insertion(self, pending_chain):
+        context = pending_chain.context
+        node_stacks = pending_chain.node_stacks
+        chain_instance_id = pending_chain.chain_instance_id
+        
+        status = self.checkStackStatus(context, node_stacks)
+        if status == self.CREATE_IN_PROGRESS:
+            return False
+        elif status == self.CREATE_FAILED:
+            #TODO(Magesh): Status has to be added to ServiceChainInstance
+            #Update the Status to ERROR  at this point
+            return True
+        #TODO(Magesh): Service itself might be in PENDING_CREATE.
+        #We should either wait here or in Heat !!
+        #Services are created by now. Determine Service IDs an setup
+        #Traffic Steering.
+        service_ids = self._fetch_serviceids_from_stack(context, node_stacks,
+                                                        chain_instance_id)
+        nvsd_action_list = self._create_nvsd_services_action(context,
+                                                             service_ids)
+
+        left_group = pending_chain.consumer_epg
+        right_group = pending_chain.provider_epg
+        classifier_id = pending_chain.classifier
+        policy_id = self.create_nvsd_policy(context, left_group, right_group,
+                                            classifier_id, nvsd_action_list)
+        self._add_chain_policy_map(context.session,
+                                   chain_instance_id,
+                                   policy_id)
+        return True
+
+
+class HeatClient:
+    def __init__(self, context, password=None):
+        api_version = "1"
+        endpoint = "%s/%s" % (cfg.CONF.servicechain.heat_uri, context.tenant)
+        kwargs = {
+            'token': context.auth_token,
+            'username': context.user_name,
+            'password': password
+        }
+        self.client = heat_client.Client(api_version, endpoint, **kwargs)
+        self.stacks = self.client.stacks
+        self.resources = self.client.resources
+
+    def create(self, name, data, parameters=None):
+        fields = {
+            'stack_name': name,
+            'timeout_mins': 10,
+            'disable_rollback': True,
+            'password': data.get('password')
+        }
+        fields['template'] = data
+        fields['parameters'] = parameters
+        return self.stacks.create(**fields)
+
+    def list(self, stack_id):
+        return self.resources.list(stack_id)
+
