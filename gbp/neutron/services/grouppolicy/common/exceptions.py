6363367d1f20228105d68ab0436ad9a6c81f75f7
Ivar Lazzaro <ivarlazzaro@gmail.com>
2014-11-14 20:43:21 -0800

    Introduce shared attribute for GBP resources
    implents blueprint: bp/introduce-shared-attribute
    
    Change-Id: I6c74c6378c18048ff251abebd27ae05e9ed38036

--- gbp/neutron/services/grouppolicy/common/exceptions.py
+++ gbp/neutron/services/grouppolicy/common/exceptions.py
@@ -67,3 +67,33 @@ class NoSubnetAvailable(exceptions.ResourceExhausted, GroupPolicyException):
 
 class PolicyTargetGroupInUse(GroupPolicyBadRequest):
     message = _("Policy Target Group %(policy_target_group)s is in use")
+
+
+class SharedResourceReferenceError(GroupPolicyBadRequest):
+    message = _("Shared resource of type %(res_type)s with id %(res_id)s "
+                "can't reference the non shared resource of type "
+                "%(ref_type)s with id %(ref_id)s")
+
+
+class InvalidSharedResource(GroupPolicyBadRequest):
+    message = _("Resource of type %(type)s cannot be shared by driver "
+                "%(driver)s")
+
+
+class CrossTenantL2PolicyL3PolicyNotSupported(GroupPolicyBadRequest):
+    message = _("Cross tenancy not supported between L2Ps and L3Ps")
+
+
+class CrossTenantPolicyTargetGroupL2PolicyNotSupported(
+        GroupPolicyBadRequest):
+    message = _("Cross tenancy not supported between PTGs and L2Ps")
+
+
+class NonSharedNetworkOnSharedL2PolicyNotSupported(GroupPolicyBadRequest):
+    message = _("Non Shared Network can't be set for a shared L2 Policy")
+
+
+class InvalidSharedAttributeUpdate(GroupPolicyBadRequest):
+    message = _("Invalid shared attribute update. Shared resource %(id)s is"
+                "referenced by %(rid)s, which is either shared or owned by a "
+                "different tenant.")
