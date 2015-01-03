4393d0b21ad06badda6ee63ca8d9cf6001974648
Yi Yang <yyos1999@gmail.com>
2015-01-03 11:51:35 -0500

    Adopting preferred coding style
    1. Previous coding style was not preferred, which is replaced
    2. Update API to unregister endpoints
    
    Closes-Bug: 1405718
    Change-Id: I50c24ed7f1ff562e04475503f3de065a12cf9b88
    Signed-off-by: Yi Yang <yyos1999@gmail.com>
    Author: Yi Yang <yyos1999@gmail.com>

--- gbpservice/neutron/services/grouppolicy/drivers/odl/odl_manager.py
+++ gbpservice/neutron/services/grouppolicy/drivers/odl/odl_manager.py
@@ -21,19 +21,23 @@ LOG = logging.getLogger(__name__)
 cfg.CONF.import_opt(
     'odl_username',
     'gbpservice.neutron.services.grouppolicy.drivers.odl.config',
-    group='odl_driver')
+    group='odl_driver'
+)
 cfg.CONF.import_opt(
     'odl_password',
     'gbpservice.neutron.services.grouppolicy.drivers.odl.config',
-    group='odl_driver')
+    group='odl_driver'
+)
 cfg.CONF.import_opt(
     'odl_host',
     'gbpservice.neutron.services.grouppolicy.drivers.odl.config',
-    group='odl_driver')
+    group='odl_driver'
+)
 cfg.CONF.import_opt(
     'odl_port',
     'gbpservice.neutron.services.grouppolicy.drivers.odl.config',
-    group='odl_driver')
+    group='odl_driver'
+)
 
 
 class OdlManager(object):
@@ -59,18 +63,59 @@ class OdlManager(object):
         self._password = cfg.CONF.odl_driver.odl_password
         self._host = cfg.CONF.odl_driver.odl_host
         self._port = cfg.CONF.odl_driver.odl_port
-        self._base_url = 'http://' + self._host + ':' + \
-            self._port + '/restconf'
-        self._reg_ep_url = self._base_url + \
+        self._headers = {
+            'Content-type': 'application/yang.data+json',
+            'Accept': 'application/yang.data+json',
+        }
+
+        self._base_url = (
+            "http://%(host)s:%(port)s/restconf" %
+            {'host': self._host, 'port': self._port}
+        )
+        self._reg_ep_url = (
+            self._base_url +
             '/operations/openstack-endpoint:register-endpoint'
-        self._unreg_ep_url = self._base_url + \
-            '/operations/endpoint:unregister-endpoint'
-        self._tenants_url = self._base_url + '/config/policy:tenants'
-        self._policy_url = self._tenants_url + '/policy:tenant'
-        self._nodes_url = self._base_url + \
-            '/config/opendaylight-inventory:nodes'
-        self._headers = {'Content-type': 'application/yang.data+json',
-                         'Accept': 'application/yang.data+json'}
+        )
+        self._unreg_ep_url = (
+            self._base_url +
+            '/operations/openstack-endpoint:unregister-endpoint'
+        )
+        self._policy_url = (
+            self._base_url +
+            '/config/policy:tenants/policy:tenant/%(tenant_id)s'
+        )
+        self._action_url = (
+            self._policy_url +
+            '/subject-feature-instances/action-instance/%(action)s'
+        )
+        self._classifier_url = (
+            self._policy_url +
+            '/subject-feature-instances/classifier-instance/%(classifier)s'
+        )
+        self._l3ctx_url = (
+            self._policy_url +
+            '/l3-context/%(l3ctx)s'
+        )
+        self._l2bd_url = (
+            self._policy_url +
+            '/l2-bridge-domain/%(l2bd)s'
+        )
+        self._l2fd_url = (
+            self._policy_url +
+            '/l2-flood-domain/%(l2fd)s'
+        )
+        self._epg_url = (
+            self._policy_url +
+            '/policy:endpoint-group/%(epg)s'
+        )
+        self._subnet_url = (
+            self._policy_url +
+            '/subnet/%(subnet)s'
+        )
+        self._contract_url = (
+            self._policy_url +
+            '/policy:contract/%(contract)s'
+        )
 
     def _convert2ascii(self, obj):
         if isinstance(obj, dict):
@@ -87,26 +132,35 @@ class OdlManager(object):
         """Send json to the ODL controller."""
 
         medium = self._convert2ascii(obj) if obj else None
-        data = jsonutils.dumps(medium, indent=4, sort_keys=True) if medium \
+        url = self._convert2ascii(url)
+        data = (
+            jsonutils.dumps(medium, indent=4, sort_keys=True) if medium
             else None
-        LOG.debug("==========================================================")
+        )
+        LOG.debug("=========================================================")
         LOG.debug("Sending METHOD (%(method)s) URL (%(url)s)",
                   {'method': method, 'url': url})
-        LOG.debug("(%(data)s)",
-                  {'data': data})
-        LOG.debug("==========================================================")
-        r = requests.request(method, url=url,
-                             headers=headers, data=data,
-                             auth=auth.HTTPBasicAuth(self._username,
-                                                     self._password))
+        LOG.debug("(%(data)s)", {'data': data})
+        LOG.debug("=========================================================")
+        r = requests.request(
+            method,
+            url=url,
+            headers=headers,
+            data=data,
+            auth=auth.HTTPBasicAuth(self._username,
+                                    self._password)
+        )
         r.raise_for_status()
 
     def _is_tenant_created(self, tenant_id):
-        url = self._policy_url + '/' + tenant_id
-        r = requests.request('get', url=url,
-                             headers=self._headers,
-                             auth=auth.HTTPBasicAuth(self._username,
-                                                     self._password))
+        url = self._convert2ascii(self._policy_url % {'tenant_id': tenant_id})
+        r = requests.request(
+            'get',
+            url=url,
+            headers=self._headers,
+            auth=auth.HTTPBasicAuth(self._username,
+                                    self._password)
+        )
         if r.status_code == 200:
             return True
         elif r.status_code == 404:
@@ -124,108 +178,102 @@ class OdlManager(object):
             data = {"input": ep}
             self._sendjson('post', self._unreg_ep_url, self._headers, data)
 
-    def register_tenants(self, tenants):
-        data = {"policy:tenants": {"tenant": tenants.values()}}
-        self._sendjson('put', self._tenants_url, self._headers, data)
-
     def create_update_tenant(self, tenant_id, tenant):
-        url = self._policy_url + '/' + tenant_id
+        url = (self._policy_url % {'tenant_id': tenant_id})
         data = {"tenant": tenant}
         self._sendjson('put', url, self._headers, data)
 
     def create_action(self, tenant_id, action):
         """Create policy action"""
         self._touch_tenant(tenant_id)
-        url = self._policy_url + '/' + tenant_id + \
-            '/subject-feature-instances/action-instance/' + action['name']
+        url = (self._action_url %
+               {'tenant_id': tenant_id, 'action': action['name']})
         data = {"action-instance": action}
         self._sendjson('put', url, self._headers, data)
 
     def delete_action(self, tenant_id, action):
         """Delete policy action"""
-        url = self._policy_url + '/' + tenant_id + \
-            '/subject-feature-instances/action-instance/' + action['name']
+        url = (self._action_url %
+               {'tenant_id': tenant_id, 'action': action['name']})
         self._sendjson('delete', url, self._headers)
 
     def create_classifier(self, tenant_id, classifier):
         """Create policy classifier"""
         self._touch_tenant(tenant_id)
-        url = self._policy_url + '/' + tenant_id + \
-            '/subject-feature-instances/classifier-instance/' + \
-            classifier['name']
+        url = (self._classifier_url %
+               {'tenant_id': tenant_id, 'classifier': classifier['name']})
         data = {"classifier-instance": classifier}
         self._sendjson('put', url, self._headers, data)
 
     def delete_classifier(self, tenant_id, classifier):
         """Delete policy classifier"""
-        url = self._policy_url + '/' + tenant_id + \
-            '/subject-feature-instances/classifier-instance/' + \
-            classifier['name']
+        url = (self._classifier_url %
+               {'tenant_id': tenant_id, 'classifier': classifier['name']})
         self._sendjson('delete', url, self._headers)
 
     def create_update_l3_context(self, tenant_id, l3ctx):
         self._touch_tenant(tenant_id)
-        url = self._policy_url + '/' + tenant_id + \
-            '/l3-context/' + l3ctx['id']
+        url = (self._l3ctx_url %
+               {'tenant_id': tenant_id, 'l3ctx': l3ctx['id']})
         data = {"l3-context": l3ctx}
         self._sendjson('put', url, self._headers, data)
 
     def delete_l3_context(self, tenant_id, l3ctx):
-        url = self._policy_url + '/' + tenant_id + \
-            '/l3-context/' + l3ctx['id']
+        url = (self._l3ctx_url %
+               {'tenant_id': tenant_id, 'l3ctx': l3ctx['id']})
         self._sendjson('delete', url, self._headers)
 
     def create_update_l2_bridge_domain(self, tenant_id, l2bd):
         self._touch_tenant(tenant_id)
-        url = self._policy_url + '/' + tenant_id + \
-            '/l2-bridge-domain/' + l2bd['id']
+        url = (self._l2bd_url %
+               {'tenant_id': tenant_id, 'l2bd': l2bd['id']})
         data = {"l2-bridge-domain": l2bd}
         self._sendjson('put', url, self._headers, data)
 
     def delete_l2_bridge_domain(self, tenant_id, l2bd):
-        url = self._policy_url + '/' + tenant_id + \
-            '/l2-bridge-domain/' + l2bd['id']
+        url = (self._l2bd_url %
+               {'tenant_id': tenant_id, 'l2bd': l2bd['id']})
         self._sendjson('delete', url, self._headers)
 
     def create_update_l2_flood_domain(self, tenant_id, l2fd):
         self._touch_tenant(tenant_id)
-        url = self._policy_url + '/' + tenant_id + \
-            '/l2-flood-domain/' + l2fd['id']
+        url = (self._l2fd_url %
+               {'tenant_id': tenant_id, 'l2fd': l2fd['id']})
         data = {"l2-flood-domain": l2fd}
         self._sendjson('put', url, self._headers, data)
 
     def delete_l2_flood_domain(self, tenant_id, l2fd):
-        url = self._policy_url + '/' + tenant_id + \
-            '/l2-flood-domain/' + l2fd['id']
+        url = (self._l2fd_url %
+               {'tenant_id': tenant_id, 'l2fd': l2fd['id']})
         self._sendjson('delete', url, self._headers)
 
     def create_update_endpoint_group(self, tenant_id, epg):
         self._touch_tenant(tenant_id)
-        url = self._policy_url + '/' + tenant_id + \
-            '/policy:endpoint-group/' + epg['id']
+        url = (self._epg_url %
+               {'tenant_id': tenant_id, 'epg': epg['id']})
         data = {"endpoint-group": epg}
         self._sendjson('put', url, self._headers, data)
 
     def delete_endpoint_group(self, tenant_id, epg):
-        url = self._policy_url + '/' + tenant_id + \
-            '/policy:endpoint-group/' + epg['id']
+        url = (self._epg_url %
+               {'tenant_id': tenant_id, 'epg': epg['id']})
         self._sendjson('delete', url, self._headers)
 
     def create_update_subnet(self, tenant_id, subnet):
         self._touch_tenant(tenant_id)
-        url = self._policy_url + '/' + tenant_id + \
-            '/subnet/' + subnet['id']
+        url = (self._subnet_url %
+               {'tenant_id': tenant_id, 'subnet': subnet['id']})
         data = {"subnet": subnet}
         self._sendjson('put', url, self._headers, data)
 
     def delete_subnet(self, tenant_id, subnet):
-        url = self._policy_url + '/' + tenant_id + \
-            '/subnet/' + subnet['id']
+        url = (self._subnet_url %
+               {'tenant_id': tenant_id, 'subnet': subnet['id']})
         self._sendjson('delete', url, self._headers)
 
     def create_update_contract(self, tenant_id, contract):
-        url = self._policy_url + '/' + tenant_id + \
-            '/policy:contract/' + contract['id']
+        url = (self._contract_url %
+               {'tenant_id': tenant_id, 'contract': contract['id']})
         data = {"contract": contract}
         self._sendjson('put', url, self._headers, data)
 
