05ee4dfbac969a45e38647e99b0e35153496b887
Ivar Lazzaro <ivarlazzaro@gmail.com>
2015-01-08 20:11:17 +0100

    OVS Agent for APIC driver
    implements blueprint apic-ovs-agent
    
    Change-Id: I96d29bc9c3338425cb1538ff8bf3e2f1195d44ff

--- gbpservice/neutron/plugins/ml2/drivers/grouppolicy/apic/driver.py
+++ gbpservice/neutron/plugins/ml2/drivers/grouppolicy/apic/driver.py
@@ -14,8 +14,11 @@
 #    under the License.
 
 from neutron.common import constants as n_constants
+from neutron.extensions import portbindings
 from neutron.openstack.common import log
 from neutron.plugins.ml2 import driver_api as api
+from neutron.plugins.ml2.drivers import mech_agent
+from opflexagent import constants as ofcst
 
 from gbpservice.neutron.services.grouppolicy.drivers.cisco.apic import (
     apic_mapping as amap)
@@ -23,9 +26,38 @@ from gbpservice.neutron.services.grouppolicy.drivers.cisco.apic import (
 LOG = log.getLogger(__name__)
 
 
-class APICMechanismGBPDriver(api.MechanismDriver):
+class APICMechanismGBPDriver(mech_agent.AgentMechanismDriverBase):
+
+    def __init__(self):
+        self.vif_details = {portbindings.CAP_PORT_FILTER: False,
+                            portbindings.OVS_HYBRID_PLUG: False}
+        self.vif_type = portbindings.VIF_TYPE_OVS
+        super(APICMechanismGBPDriver, self).__init__(
+            ofcst.AGENT_TYPE_OPFLEX_OVS)
+
+    def try_to_bind_segment_for_agent(self, context, segment, agent):
+        if self.check_segment_for_agent(segment, agent):
+            context.set_binding(
+                segment[api.ID], self.vif_type, self.vif_details)
+            return True
+        else:
+            return False
+
+    def check_segment_for_agent(self, segment, agent):
+        network_type = segment[api.NETWORK_TYPE]
+        if network_type == ofcst.TYPE_OPFLEX:
+            opflex_mappings = agent['configurations'].get('opflex_networks',
+                                                          [])
+            LOG.debug(_("Checking segment: %(segment)s "
+                        "for physical network: %(mappings)s "),
+                      {'segment': segment, 'mappings': opflex_mappings})
+            return (opflex_mappings is None or
+                    segment[api.PHYSICAL_NETWORK] in opflex_mappings)
+        else:
+            return False
 
     def initialize(self):
+        super(APICMechanismGBPDriver, self).initialize()
         self._apic_gbp = None
 
     @property
@@ -49,3 +81,10 @@ class APICMechanismGBPDriver(api.MechanismDriver):
     def update_subnet_postcommit(self, context):
         self.apic_gbp.process_subnet_changed(context._plugin_context,
                                              context.original, context.current)
+
+    def bind_port(self, context):
+        super(APICMechanismGBPDriver, self).bind_port(context)
+        context._plugin._update_port_dict_binding(context.current,
+                                                  context._binding)
+        self.apic_gbp.process_port_bound(context._plugin_context,
+                                         context.current)
