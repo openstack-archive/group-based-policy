480cc48c09b93302f6556e83a4f1547c88ea917b
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2015-03-20 16:53:11 -0700

    Admin or Provider tenant to own implicit SCIs
    Whenever a Redirect action is provided/consumed GBP, the
    implicitly created SCI could be owned by different tenants
    depending on the actor triggering it.
    To make this consistent, this patch proposes to have a single
    configurable admin tenant that will own all the chain resources.
    When the said tenant is not configured, the provider PTG's
    tenant will be used instead.
    
    Change-Id: I4862b87c41b48344a53dbf72c004a8dc18c2aa99
    Closes-Bug: 1432816

--- gbpservice/neutron/db/grouppolicy/group_policy_mapping_db.py
+++ gbpservice/neutron/db/grouppolicy/group_policy_mapping_db.py
@@ -187,6 +187,11 @@ class GroupPolicyMappingDbPlugin(gpdb.GroupPolicyDbPlugin):
         return self._make_policy_target_dict(pt_db)
 
     @log.log
+    def get_policy_targets_count(self, context, filters=None):
+        return self._get_collection_count(context, PolicyTargetMapping,
+                                          filters=filters)
+
+    @log.log
     def get_policy_targets(self, context, filters=None, fields=None,
                            sorts=None, limit=None, marker=None,
                            page_reverse=False):
@@ -283,6 +288,11 @@ class GroupPolicyMappingDbPlugin(gpdb.GroupPolicyDbPlugin):
                                     page_reverse=page_reverse)
 
     @log.log
+    def get_l2_policies_count(self, context, filters=None):
+        return self._get_collection_count(context, L2PolicyMapping,
+                                          filters=filters)
+
+    @log.log
     def create_l3_policy(self, context, l3_policy):
         l3p = l3_policy['l3_policy']
         self.validate_ip_pool(l3p.get('ip_pool', None), l3p['ip_version'])
@@ -376,3 +386,8 @@ class GroupPolicyMappingDbPlugin(gpdb.GroupPolicyDbPlugin):
                                     sorts=sorts, limit=limit,
                                     marker_obj=marker_obj,
                                     page_reverse=page_reverse)
+
+    @log.log
+    def get_external_segments_count(self, context, filters=None):
+        return self._get_collection_count(context, ExternalSegmentMapping,
+                                          filters=filters)
