65489bbae486620baca7ad5dc48c482852d52c56
Ivar Lazzaro <ivarlazzaro@gmail.com>
2015-03-24 18:15:40 -0700

    Apic driver improvements
    - Introducing Opflex agent;
    - Multiple PTGs per L2Ps;
    - Multiple PTGs same subnet.
    
    Change-Id: I5dcb12daed669131270e589a01cee432b53cd61c

--- gbpservice/neutron/plugins/ml2/drivers/grouppolicy/apic/driver.py
+++ gbpservice/neutron/plugins/ml2/drivers/grouppolicy/apic/driver.py
@@ -13,18 +13,51 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from neutron.common import constants as n_constants
+from neutron.extensions import portbindings
 from neutron import manager
 from neutron.plugins.ml2 import driver_api as api
+from neutron.plugins.ml2.drivers import mech_agent
+from opflexagent import constants as ofcst
 from oslo_log import log
+
+from gbpservice.neutron.services.grouppolicy.drivers.cisco.apic import (
+    apic_mapping as amap)
 
 
 LOG = log.getLogger(__name__)
 
 
-class APICMechanismGBPDriver(api.MechanismDriver):
+class APICMechanismGBPDriver(mech_agent.AgentMechanismDriverBase):
+
+    def __init__(self):
+        self.vif_details = {portbindings.CAP_PORT_FILTER: False,
+                            portbindings.OVS_HYBRID_PLUG: False}
+        self.vif_type = portbindings.VIF_TYPE_OVS
+        super(APICMechanismGBPDriver, self).__init__(
+            ofcst.AGENT_TYPE_OPFLEX_OVS)
+
+    def try_to_bind_segment_for_agent(self, context, segment, agent):
+        if self.check_segment_for_agent(segment, agent):
+            context.set_binding(
+                segment[api.ID], self.vif_type, self.vif_details)
+            return True
+        else:
+            return False
+
+    def check_segment_for_agent(self, segment, agent):
+        network_type = segment[api.NETWORK_TYPE]
+        if network_type == ofcst.TYPE_OPFLEX:
+            opflex_mappings = agent['configurations'].get('opflex_networks')
+            LOG.debug(_("Checking segment: %(segment)s "
+                        "for mappings: %(mappings)s "),
+                      {'segment': segment, 'mappings': opflex_mappings})
+            return ((opflex_mappings is None) or
+                    (segment[api.PHYSICAL_NETWORK] in opflex_mappings))
+        else:
+            return False
 
     def initialize(self):
+        super(APICMechanismGBPDriver, self).initialize()
         self._apic_gbp = None
 
     @property
@@ -36,16 +69,30 @@ class APICMechanismGBPDriver(api.MechanismDriver):
         return self._apic_gbp
 
     def create_port_postcommit(self, context):
-        # DHCP Ports are created implicitly by Neutron, need to inform GBP
-        if (context.current.get('device_owner') ==
-                n_constants.DEVICE_OWNER_DHCP):
-            self.apic_gbp.create_dhcp_policy_target_if_needed(
-                context._plugin_context, context.current)
+        self.apic_gbp.process_port_added(
+            context._plugin_context, context.current)
 
     def update_port_postcommit(self, context):
         self.apic_gbp.process_port_changed(context._plugin_context,
                                            context.original, context.current)
 
+    def delete_port_precommit(self, context):
+        self.apic_gbp.process_pre_port_deleted(context._plugin_context,
+                                               context.current)
+
+    def delete_port_postcommit(self, context):
+        self.apic_gbp.process_port_deleted(context._plugin_context,
+                                           context.current)
+
     def update_subnet_postcommit(self, context):
         self.apic_gbp.process_subnet_changed(context._plugin_context,
                                              context.original, context.current)
+
+    def create_subnet_postcommit(self, context):
+        if not context.current['name'].startswith(amap.APIC_OWNED):
+            self.apic_gbp.process_subnet_added(context._plugin_context,
+                                               context.current)
+
+    def delete_subnet_postcommit(self, context):
+        self.apic_gbp.process_subnet_deleted(context._plugin_context,
+                                             context.current)
