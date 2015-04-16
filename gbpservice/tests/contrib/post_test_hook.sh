6dd9a66e6bddd7b3f8ac04a439ad403a7040f0ed
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2015-04-16 02:04:14 -0700

    Adding to gbpfunc tests to gate job
    Change-Id: I4c532a3f8905b989139111d6ea671b027a14ec0c
    (cherry picked from commit 603d6bdb9486480daa57150558d7f372d8f211af)

--- gbpservice/tests/contrib/post_test_hook.sh
+++ gbpservice/tests/contrib/post_test_hook.sh
@@ -5,6 +5,7 @@ set -xe
 GBP_DIR="$BASE/new/group-based-policy"
 TEMPEST_DIR="$BASE/new/tempest"
 SCRIPTS_DIR="/usr/local/jenkins/slave_scripts"
+LOGS_DIR="$BASE/new/logs"
 
 function generate_testr_results {
     # Give job user rights to access tox logs
@@ -27,6 +28,18 @@ function dsvm_functional_prep_func {
 
 prep_func="dsvm_functional_prep_func"
 
+# Run tests
+echo "Running gbpfunc test suite"
+set +e
+cd $BASE/new/devstack
+source openrc demo demo
+cd $BASE/new
+sudo git clone https://github.com/noironetworks/devstack -b jishnub/testsuites gbpfunctests
+cd gbpfunctests/testcases/testcases_func
+python suite_run.py -s func
+gbpfunc_exit_code=$?
+set -e
+
 cd $GBP_DIR
 # Prep the environment according to job's requirements.
 $prep_func
@@ -40,4 +53,20 @@ set -e
 
 # Collect and parse results
 generate_testr_results
-exit $testr_exit_code
+
+# Prepare the log files for Jenkins to upload
+set +e
+cd $LOGS_DIR
+for f in $(find . -name "*.log.2*"); do
+    sudo mv $f ${f/.log.*/.txt}
+done
+sudo gzip -9fk `find . -maxdepth 1 \! -type l -name "*.txt" | xargs ls -d`
+mv *.gz /opt/stack/logs/
+set -e
+
+# Check if any gbp exercises failed
+set +e
+exercises_exit_code=$(grep -cim1 "FAILED gbp*" $LOGS_DIR/stack.sh.log)
+set -e
+
+exit $(($exercises_exit_code+$gbpfunc_exit_code+$testr_exit_code))
