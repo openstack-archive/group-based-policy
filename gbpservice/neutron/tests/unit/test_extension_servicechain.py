3aacae6d3879ad8df83618fc8f2f0277386b3c14
Ivar Lazzaro <ivarlazzaro@gmail.com>
2015-05-03 20:58:42 -0700

    [WIP] introduce service profile model
    Partially implements blueprint node-centric-chain-plugin
    
    Change-Id: I2403e35d49a1e100f292f82082c7dad96d79fc29

--- gbpservice/neutron/tests/unit/test_extension_servicechain.py
+++ gbpservice/neutron/tests/unit/test_extension_servicechain.py
@@ -31,6 +31,7 @@ SERVICECHAIN_URI = 'servicechain'
 SERVICECHAIN_NODES_URI = SERVICECHAIN_URI + '/' + 'servicechain_nodes'
 SERVICECHAIN_SPECS_URI = SERVICECHAIN_URI + '/' + 'servicechain_specs'
 SERVICECHAIN_INSTANCES_URI = SERVICECHAIN_URI + '/' + 'servicechain_instances'
+SERVICE_PROFILE_URI = SERVICECHAIN_URI + '/' + 'service_profiles'
 
 
 class ServiceChainExtensionTestCase(test_extensions_base.ExtensionTestCase):
@@ -72,7 +73,7 @@ class ServiceChainExtensionTestCase(test_extensions_base.ExtensionTestCase):
     def _get_create_servicechain_node_attrs(self):
         return {
             'name': 'servicechain1',
-            'service_type': 'FIREWALL',
+            'service_profile_id': _uuid(),
             'tenant_id': _uuid(),
             'description': 'test servicechain node',
             'config': 'test_config'
@@ -87,7 +88,7 @@ class ServiceChainExtensionTestCase(test_extensions_base.ExtensionTestCase):
         servicechain_node_id = _uuid()
         data = {
             'servicechain_node': {
-                'service_type': 'FIREWALL',
+                'service_profile_id': _uuid(),
                 'tenant_id': _uuid(),
                 'config': 'test_config'
             }
