026f6e8a33c54ee5abe8e134a149be595a3d6a81
Ivar Lazzaro <ivarlazzaro@gmail.com>
2015-05-06 20:02:43 -0700

    [WIP] node composition plugin implementation
    Partially implements blueprint node-centric-chain-plugin
    
    Change-Id: I16d56ebb7bff0c621c68106039b4217cfac77755

--- gbpservice/neutron/tests/unit/services/servicechain/ncp/test_ncp_plugin.py
+++ gbpservice/neutron/tests/unit/services/servicechain/ncp/test_ncp_plugin.py
@@ -48,13 +48,7 @@ class NodeCompositionPluginTestCase(
         engine = db_api.get_engine()
         model_base.BASEV2.metadata.create_all(engine)
 
-    def test_node_shared(self):
-        pass
-
-    def test_profile_shared(self):
-        pass
-
-    def test_spec_shared(self):
+    def test_spec_ordering_list_servicechain_instances(self):
         pass
 
     def test_context_attributes(self):
@@ -83,14 +77,13 @@ class NodeCompositionPluginTestCase(
         self.assertIsNotNone(ctx.session)
         self.assertIsNotNone(ctx.admin_context)
         self.assertIsNotNone(ctx.admin_session)
-        self.assertEqual(ctx.instance, instance)
-        self.assertEqual(ctx.provider, provider)
-        self.assertEqual(ctx.consumer, consumer)
-        self.assertEqual(ctx.management, management)
-        self.assertEqual(ctx.management, management)
-        self.assertEqual(ctx.relevant_specs, [spec])
+        self.assertEqual(instance['id'], ctx.instance['id'])
+        self.assertEqual(provider['id'], ctx.provider['id'])
+        self.assertEqual(consumer['id'], ctx.consumer['id'])
+        self.assertEqual(management['id'], ctx.management['id'])
+        self.assertEqual([spec['id']], [x['id'] for x in ctx.relevant_specs])
         self.assertIsNone(ctx.original_node)
-        self.assertIsNone(ctx.service_targets)
+        self.assertEqual(0, len(ctx.get_service_targets()))
 
     def test_context_relevant_specs(self):
         plugin_context = n_context.get_admin_context()
@@ -99,24 +92,15 @@ class NodeCompositionPluginTestCase(
         spec_used = self.create_servicechain_spec(
             nodes=[node_used['id']])['servicechain_spec']
 
-        node_unused = self._create_profiled_servicechain_node(
-            service_type="TYPE", config='{}')['servicechain_node']
-        spec_unused = self.create_servicechain_spec(
-            nodes=[node_unused['id']])['servicechain_spec']
-
         provider = self.create_policy_target_group()['policy_target_group']
         instance = self.create_servicechain_instance(
             provider_ptg_id=provider['id'],
-            servicechain_specs=[spec_used['id'],
-                                spec_unused['id']])['servicechain_instance']
-        self.assertEqual(len(instance['servicechain_specs']), 2)
+            servicechain_specs=[spec_used['id']])['servicechain_instance']
 
         ctx = ncp_context.get_node_driver_context(
             self.plugin, plugin_context, instance, node_used)
-        self.assertEqual(ctx.relevant_specs, [spec_used])
-
-
-class TestNcpNodeDriverManager(NodeCompositionPluginTestCase):
+        self.assertEqual([spec_used['id']],
+                         [x['id'] for x in ctx.relevant_specs])
 
     def test_manager_initialized(self):
         mgr = self.plugin.driver_manager
