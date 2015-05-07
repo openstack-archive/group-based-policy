da7b904cfa0f60ed559d7f4a190a4b1918c8abb5
Ivar Lazzaro <ivarlazzaro@gmail.com>
2015-05-06 20:02:43 -0700

    [WIP] node composition plugin implementation
    Partially implements blueprint node-centric-chain-plugin
    
    Change-Id: I16d56ebb7bff0c621c68106039b4217cfac77755

---
+++ gbpservice/neutron/services/servicechain/plugins/ncp/exceptions.py
@@ -0,0 +1,43 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Exceptions used by NodeCompositionPlugin and drivers."""
+
+from neutron.common import exceptions
+
+
+class NodeDriverError(exceptions.NeutronException):
+    """Node driver call failed."""
+    message = _("%(method)s failed.")
+
+
+class NodeCompositionPluginException(exceptions.NeutronException):
+    """Base for node driver exceptions returned to user."""
+    pass
+
+
+class NodeCompositionPluginBadRequest(exceptions.BadRequest,
+                                      NodeCompositionPluginException):
+    """Base for node driver bad request exceptions returned to user."""
+    pass
+
+
+class OneSpecPerInstanceAllowed(NodeCompositionPluginBadRequest):
+    message = _("The Node Composition Plugin only supports one Servicechain"
+                "Spec per Servicechain Instance.")
+
+
+class NoDriverAvailableForAction(NodeCompositionPluginBadRequest):
+    message = _("The Node Composition Plugin can't find any Node Driver "
+                "available for executing %(action)s on node %(node_id)s. "
+                "This may be caused by a Servicechain Node misconfiguration "
+                "or an unsupported Service Profile.")
