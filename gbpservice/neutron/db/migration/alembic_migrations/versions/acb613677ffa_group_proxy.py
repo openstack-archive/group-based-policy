23af592e11932b4ab68b59fe7609573fb4421adf
Ivar Lazzaro <ivarlazzaro@gmail.com>
2015-07-31 19:28:06 -0700

    proxy ptg extension
    Driver extension that allows users to specify a 'proxied_group_id'
    during PTG creation. Whenever a PTG proxies another group, it is
    expected to intercept all the traffic before sending it to the
    original destination. Proxy groups can modify the traffic if needed.
    This is a useful construct for the Traffic Stitching Plumber.
    
    Partially implements blueprint node-centric-chain-plugin
    
    (cherry picked from commit 090f94a381e787b62bf17b8448ea2a29df76092e)
    (cherry picked from commit 535c9e7f12c1750f9a85beb613e093a2984577fb)
    
    Conflicts:
    	gbpservice/neutron/services/grouppolicy/extension_manager.py
    	gbpservice/neutron/tests/unit/db/grouppolicy/test_group_policy_mapping_db.py
    	gbpservice/neutron/tests/unit/services/grouppolicy/test_extension_driver_api.py
    
    Change-Id: Ic48daa82d3434c515a39daff438fbf0a86c2ba49

---
+++ gbpservice/neutron/db/migration/alembic_migrations/versions/acb613677ffa_group_proxy.py
@@ -0,0 +1,64 @@
+# Copyright 2014 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""Proxy Group Mapping
+
+Revision ID: acb613677ffa
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'acb613677ffa'
+down_revision = 'c2a9d04c8cef'
+
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+
+    op.create_table(
+        'gp_group_proxy_mappings',
+        sa.Column('policy_target_group_id', sa.String(length=36),
+                  nullable=False),
+        sa.Column('proxied_group_id', sa.String(length=36)),
+        sa.Column('proxy_group_id', sa.String(length=36)),
+        sa.Column('proxy_type', sa.String(length=24)),
+        sa.PrimaryKeyConstraint('policy_target_group_id'),
+        sa.ForeignKeyConstraint(['policy_target_group_id'],
+                                ['gp_policy_target_groups.id'],
+                                ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(['proxied_group_id'],
+                                ['gp_policy_target_groups.id']),
+        sa.ForeignKeyConstraint(['proxy_group_id'],
+                                ['gp_policy_target_groups.id'],
+                                ondelete='SET NULL')
+    )
+    op.create_table(
+        'gp_proxy_gateway_mappings',
+        sa.Column('policy_target_id', sa.String(length=36), nullable=False),
+        sa.Column('proxy_gateway', sa.Boolean, nullable=False, default=False),
+        sa.Column('group_default_gateway', sa.Boolean, nullable=False,
+                  default=False),
+        sa.PrimaryKeyConstraint('policy_target_id'),
+        sa.ForeignKeyConstraint(['policy_target_id'],
+                                ['gp_policy_targets.id'],
+                                ondelete='CASCADE',
+                                name='group_proxy_mapping_fk_ptg_id'),
+    )
+
+
+def downgrade():
+    pass
