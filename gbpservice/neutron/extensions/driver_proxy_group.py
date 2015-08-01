2645af6e48e88be1fb4775ca4e8374e8002fee34
Ivar Lazzaro <ivarlazzaro@gmail.com>
2015-07-31 19:28:06 -0700

    [WIP] proxy ptg extension
    Driver extension that allows users to specify a 'proxied_group_id'
    during PTG creation. Whenever a PTG proxies another group, it is
    expected to intercept all the traffic before sending it to the
    original destination. Proxy groups can modify the traffic if needed.
    This is a useful construct for the Traffic Stitching Plumber.
    
    Partially implements blueprint node-centric-chain-plugin
    
    Change-Id: Idc185df5b0c7e61ef800ca449911656a8c1d2b87

---
+++ gbpservice/neutron/extensions/driver_proxy_group.py
@@ -0,0 +1,103 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron.api import extensions
+from neutron.api.v2 import attributes as attr
+from neutron.common import exceptions as nexc
+
+from gbpservice.neutron.extensions import group_policy as gp
+from gbpservice.neutron.services.grouppolicy.common import exceptions as gp_exc
+
+PROXY_TYPE_L2 = 'l2'
+PROXY_TYPE_L3 = 'l3'
+DEFAULT_PROXY_TYPE = PROXY_TYPE_L3
+
+
+class ProxyGroupBadRequest(gp_exc.GroupPolicyBadRequest):
+    message = _("Invalid input for Proxy Group extension, reason: %(msg)s")
+
+
+class InvalidProxiedGroup(nexc.InvalidInput, ProxyGroupBadRequest):
+    message = _("Proxied group %(group_id)s already has a proxy.")
+
+
+class ProxyTypeSetWithoutProxiedPTG(nexc.InvalidInput, ProxyGroupBadRequest):
+    message = _("Proxy type can't be set without a proxied PTG.")
+
+
+class InvalidProxyGatewayGroup(nexc.InvalidInput, ProxyGroupBadRequest):
+    message = _("Proxy gateway can't be set for non proxy PTG %(group_id)s.")
+
+
+EXTENDED_ATTRIBUTES_2_0 = {
+    gp.POLICY_TARGET_GROUPS: {
+        'proxied_group_id': {
+            'allow_post': True, 'allow_put': False,
+            'validate': {'type:uuid_or_none': None}, 'is_visible': True,
+            'default': attr.ATTR_NOT_SPECIFIED,
+            'enforce_policy': True},
+        'proxy_type': {
+            'allow_post': True, 'allow_put': False,
+            'validate': {'type:values': ['l2', 'l3', None]},
+            'is_visible': True, 'default': attr.ATTR_NOT_SPECIFIED,
+            'enforce_policy': True},
+        'proxy_group_id': {
+            'allow_post': False, 'allow_put': False,
+            'validate': {'type:uuid_or_none': None}, 'is_visible': True,
+            'enforce_policy': True},
+    },
+    gp.POLICY_TARGETS: {
+        # This policy target will be used to reach the -proxied- PTG
+        'proxy_gateway': {
+            'allow_post': True, 'allow_put': False, 'default': False,
+            'convert_to': attr.convert_to_boolean,
+            'is_visible': True, 'required_by_policy': True,
+            'enforce_policy': True},
+        # This policy target is the default gateway for the -current- PTG
+        # Only for internal use.
+        'group_default_gateway': {
+            'allow_post': True, 'allow_put': False, 'default': False,
+            'convert_to': attr.convert_to_boolean,
+            'is_visible': True, 'required_by_policy': True,
+            'enforce_policy': True},
+    },
+}
+
+
+class Driver_proxy_group(extensions.ExtensionDescriptor):
+
+    @classmethod
+    def get_name(cls):
+        return "group policy poxy group extension"
+
+    @classmethod
+    def get_alias(cls):
+        return "proxy_group"
+
+    @classmethod
+    def get_description(cls):
+        return _("Add proxy_group_id attribute to policy target groups.")
+
+    @classmethod
+    def get_namespace(cls):
+        return ("http://docs.openstack.org/ext/neutron/grouppolicy/"
+                "proxy_group/api/v1.0")
+
+    @classmethod
+    def get_updated(cls):
+        return "2015-07-31T10:00:00-00:00"
+
+    def get_extended_resources(self, version):
+        if version == "2.0":
+            return EXTENDED_ATTRIBUTES_2_0
+        else:
+            return {}
---
+++ gbpservice/neutron/tests/unit/services/grouppolicy/test_group_proxy_extension.py
@@ -0,0 +1,143 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron import context as n_ctx
+from sqlalchemy.orm import exc as orm_exc
+
+from gbpservice.neutron.db.grouppolicy.extensions import group_proxy_db
+from gbpservice.neutron.tests.unit.services.grouppolicy import (
+    test_extension_driver_api as test_ext_base)
+
+
+class ExtensionDriverTestCase(test_ext_base.ExtensionDriverTestBase):
+
+    _extension_drivers = ['proxy_group']
+    _extension_path = None
+
+    def test_proxy_group_extension(self):
+        ptg = self.create_policy_target_group()['policy_target_group']
+        self.assertIsNone(ptg['proxy_group_id'])
+        self.assertIsNone(ptg['proxied_group_id'])
+        self.assertIsNone(ptg['proxy_type'])
+
+        ptg_proxy = self.create_policy_target_group(
+            proxied_group_id=ptg['id'])['policy_target_group']
+        self.assertIsNone(ptg_proxy['proxy_group_id'])
+        self.assertEqual(ptg['id'], ptg_proxy['proxied_group_id'])
+        self.assertEqual('l3', ptg_proxy['proxy_type'])
+
+        # Verify relationship added
+        ptg = self.show_policy_target_group(ptg['id'])['policy_target_group']
+        self.assertEqual(ptg_proxy['id'], ptg['proxy_group_id'])
+        self.assertIsNone(ptg['proxied_group_id'])
+
+        pt = self.create_policy_target(
+            policy_target_group_id=ptg_proxy['id'])['policy_target']
+        self.assertFalse(pt['proxy_gateway'])
+        self.assertFalse(pt['group_default_gateway'])
+        pt = self.create_policy_target(
+            policy_target_group_id=ptg_proxy['id'],
+            proxy_gateway=True, group_default_gateway=True)['policy_target']
+        self.assertTrue(pt['proxy_gateway'])
+        self.assertTrue(pt['group_default_gateway'])
+        pt = self.show_policy_target(pt['id'])['policy_target']
+        self.assertTrue(pt['proxy_gateway'])
+        self.assertTrue(pt['group_default_gateway'])
+
+    def test_preexisting_pt(self):
+        ptg = self.create_policy_target_group()['policy_target_group']
+        pt = self.create_policy_target(
+            policy_target_group_id=ptg['id'])['policy_target']
+        self.assertTrue('proxy_gateway' in pt)
+        self.assertTrue('group_default_gateway' in pt)
+
+        # Forcefully delete the entry in the proxy table, and verify that it's
+        # fixed by the subsequent GET
+        admin_context = n_ctx.get_admin_context()
+        mapping = admin_context.session.query(
+            group_proxy_db.ProxyGatewayMapping).filter_by(
+                policy_target_id=pt['id']).one()
+        admin_context.session.delete(mapping)
+        query = admin_context.session.query(
+            group_proxy_db.ProxyGatewayMapping).filter_by(
+                policy_target_id=pt['id'])
+        self.assertRaises(orm_exc.NoResultFound, query.one)
+
+        # Showing the object just ignores the extension
+        pt = self.show_policy_target(pt['id'],
+                                     expected_res_status=200)['policy_target']
+        self.assertFalse('proxy_gateway' in pt)
+        self.assertFalse('group_default_gateway' in pt)
+
+        # Updating the object just ignores the extension
+        pt = self.update_policy_target(
+            pt['id'], name='somenewname',
+            expected_res_status=200)['policy_target']
+        self.assertEqual('somenewname', pt['name'])
+        self.assertFalse('proxy_gateway' in pt)
+        self.assertFalse('group_default_gateway' in pt)
+
+    def test_proxy_group_multiple_proxies(self):
+        # same PTG proxied multiple times will fail
+        ptg = self.create_policy_target_group()['policy_target_group']
+        self.create_policy_target_group(proxied_group_id=ptg['id'])
+        # Second proxy will fail
+        res = self.create_policy_target_group(proxied_group_id=ptg['id'],
+                                              expected_res_status=400)
+        self.assertEqual('InvalidProxiedGroup', res['NeutronError']['type'])
+
+    def test_proxy_group_chain_proxy(self):
+        # Verify no error is raised when chaining multiple proxy PTGs
+        ptg0 = self.create_policy_target_group()['policy_target_group']
+        ptg1 = self.create_policy_target_group(
+            proxied_group_id=ptg0['id'],
+            expected_res_status=201)['policy_target_group']
+        self.create_policy_target_group(proxied_group_id=ptg1['id'],
+                                        expected_res_status=201)
+
+    def test_proxy_group_no_update(self):
+        ptg0 = self.create_policy_target_group()['policy_target_group']
+        ptg1 = self.create_policy_target_group()['policy_target_group']
+        ptg_proxy = self.create_policy_target_group(
+            proxied_group_id=ptg0['id'])['policy_target_group']
+        self.update_policy_target_group(
+            ptg_proxy['id'], proxied_group_id=ptg1['id'],
+            expected_res_status=400)
+
+    def test_different_proxy_type(self):
+        ptg = self.create_policy_target_group()['policy_target_group']
+        ptg_proxy = self.create_policy_target_group(
+            proxied_group_id=ptg['id'], proxy_type='l2')['policy_target_group']
+        self.assertEqual('l2', ptg_proxy['proxy_type'])
+
+        ptg_proxy = self.show_policy_target_group(
+            ptg_proxy['id'])['policy_target_group']
+        self.assertEqual('l2', ptg_proxy['proxy_type'])
+
+    def test_proxy_type_fails(self):
+        ptg = self.create_policy_target_group()['policy_target_group']
+        res = self.create_policy_target_group(proxy_type='l2',
+                                              expected_res_status=400)
+        self.assertEqual('ProxyTypeSetWithoutProxiedPTG',
+                         res['NeutronError']['type'])
+
+        self.create_policy_target_group(proxied_group_id=ptg['id'],
+                                        proxy_type='notvalid',
+                                        expected_res_status=400)
+
+    def test_proxy_gateway_no_proxy(self):
+        ptg = self.create_policy_target_group()['policy_target_group']
+        res = self.create_policy_target(
+            policy_target_group_id=ptg['id'], proxy_gateway=True,
+            expected_res_status=400)
+        self.assertEqual('InvalidProxyGatewayGroup',
+                         res['NeutronError']['type'])
