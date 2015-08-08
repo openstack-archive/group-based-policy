b326f005308846bae7cdc97847f8a189c2ab6c5f
Ivar Lazzaro <ivarlazzaro@gmail.com>
2015-08-07 19:09:47 -0700

    introducing traffic stitching plumber
    Partially implements blueprint node-centric-chain-plugin
    
    (cherry picked from commit 203f544bf711c96f731ed1d4f44da6cc02d6057e)
    (cherry picked from commit 92bceb773264b798ca45889099223c0a77a8a2f9)
    
    Conflicts:
    	gbpservice/neutron/tests/unit/db/grouppolicy/test_group_policy_mapping_db.py
    	gbpservice/neutron/tests/unit/services/grouppolicy/test_grouppolicy_plugin.py
    
    Change-Id: Ic91a5297e4fcf48ebba0a7f9956628fe01a53b74

--- gbpservice/neutron/tests/unit/db/grouppolicy/test_group_policy_mapping_db.py
+++ gbpservice/neutron/tests/unit/db/grouppolicy/test_group_policy_mapping_db.py
@@ -18,7 +18,6 @@ from neutron.db import api as db_api
 from neutron.db import model_base
 from neutron.tests.unit import test_l3_plugin
 from neutron.tests.unit import testlib_api
-from oslo.config import cfg
 
 from gbpservice.neutron.db.grouppolicy import group_policy_mapping_db as gpmdb
 from gbpservice.neutron.tests.unit.db.grouppolicy import (
@@ -41,15 +40,15 @@ SC_PLUGIN_KLASS = (
 class GroupPolicyMappingDbTestCase(tgpdb.GroupPolicyDbTestCase,
                                    test_l3_plugin.L3NatTestCaseMixin):
 
-    def setUp(self, core_plugin=None, gp_plugin=None, service_plugins=None):
+    def setUp(self, core_plugin=None, gp_plugin=None, service_plugins=None,
+              sc_plugin=None):
         testlib_api.SqlTestCase._TABLES_ESTABLISHED = False
         if not gp_plugin:
             gp_plugin = DB_GP_PLUGIN_KLASS
         if not service_plugins:
-            service_plugins = {'l3_plugin_name': "router",
-                               'gp_plugin_name': gp_plugin,
-                               'servicechain_plugin': SC_PLUGIN_KLASS}
-        cfg.CONF.set_override('allow_overlapping_ips', True)
+            service_plugins = {
+                'l3_plugin_name': "router", 'gp_plugin_name': gp_plugin,
+                'servicechain_plugin': sc_plugin or SC_PLUGIN_KLASS}
         super(GroupPolicyMappingDbTestCase, self).setUp(
             core_plugin=core_plugin, gp_plugin=gp_plugin,
             service_plugins=service_plugins
