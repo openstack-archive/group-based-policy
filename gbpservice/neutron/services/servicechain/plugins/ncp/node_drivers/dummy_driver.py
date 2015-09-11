bbe5ff53ebd065995fae2989a7d7a9011413f0ec
Ivar Lazzaro <ivarlazzaro@gmail.com>
2015-09-11 18:54:11 +0200

    api to notify ncp plugin when prs consumer added/removed
    This patch provides the internal API between NCP and its
    drivers to react when a consumed is added/removed to/from
    a chain. The implementation will come in a subsequent patch.
    
    Partially implements blueprint node-centric-chain-plugin
    
    (cherry picked from commit c5db82e429f2efe3bb9d3511f26bfa625f97879d)
    
    Change-Id: Id2b1f838dba230645db1621faedd280752df3cbf

--- gbpservice/neutron/services/servicechain/plugins/ncp/node_drivers/dummy_driver.py
+++ gbpservice/neutron/services/servicechain/plugins/ncp/node_drivers/dummy_driver.py
@@ -57,6 +57,14 @@ class NoopNodeDriver(driver_base.NodeDriverBase):
         pass
 
     @log.log
+    def update_node_consumer_ptg_added(self, context, policy_target_group):
+        pass
+
+    @log.log
+    def update_node_consumer_ptg_removed(self, context, policy_target_group):
+        pass
+
+    @log.log
     def notify_chain_parameters_updated(self, context):
         pass
 
