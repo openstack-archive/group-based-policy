1449215aafda0386ea6bc3d81d13ab01bc00410a
Ivar Lazzaro <ivarlazzaro@gmail.com>
2015-09-30 16:25:00 -0700

    add cluster_id attribute to PTs for HA
    Partially implements blueprint node-centric-chain-plugin
    
    Change-Id: If4f4752b04a0b27df1a296798e0289567a074b86

---
+++ gbpservice/neutron/db/migration/alembic_migrations/versions/4121adfbac30_pt_cluster_id.py
@@ -0,0 +1,33 @@
+# Copyright 2014 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+# revision identifiers, used by Alembic.
+revision = '4121adfbac30'
+down_revision = 'acb613677ffa'
+
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column(
+        'gp_policy_targets',
+        sa.Column('cluster_id', sa.String(255), server_default='')
+    )
+
+
+def downgrade():
+    pass
