9a6ab1ecf1d1fc2f5706d485f5c2e3783eb064c8
Ivar Lazzaro <ivarlazzaro@gmail.com>
2015-10-06 20:29:02 -0700

    make sg mapping pluggable
    For better compatibility with existing deployments
    and increased experimentation velocity, the concept
    of security group manager is introduced in this patch.
    A SG manager is a pluggable piece in the RMD which manages
    the mapping between GBP constructs and SG rules.
    
    Implements: blueprint sg-managers-in-rmd
    
    Change-Id: I3f481a659122596a9336f8d164f3d584ce8047fb

--- gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/apic_mapping.py
+++ gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/apic_mapping.py
@@ -44,8 +44,9 @@ from gbpservice.neutron.services.grouppolicy.drivers.cisco.apic import (
     name_manager as name_manager)
 from gbpservice.neutron.services.grouppolicy.drivers.cisco.apic import (
     nova_client as nclient)
+from gbpservice.neutron.services.grouppolicy.drivers.sg_managers import (
+    dummy_manager as sg_manager)
 from gbpservice.neutron.services.grouppolicy import group_policy_context
-
 
 HOST_SNAT_POOL = 'host-snat-pool-for-internal-use'
 HOST_SNAT_POOL_PORT = 'host-snat-pool-port-for-internal-use'
@@ -197,7 +198,8 @@ class ApicMappingDriver(api.ResourceMappingDriver,
         return ApicMappingDriver.manager
 
     def initialize(self):
-        super(ApicMappingDriver, self).initialize()
+        super(ApicMappingDriver, self).initialize(
+            sg_manager=sg_manager.DummyManager())
         self._setup_rpc_listeners()
         self._setup_rpc()
         self.apic_manager = ApicMappingDriver.get_apic_manager()
