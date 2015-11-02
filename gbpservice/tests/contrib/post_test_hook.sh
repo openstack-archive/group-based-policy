3a9420c46f497c557afd394b5a7ff425708775b2
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2015-11-02 10:05:21 -0800

    [WIP] Aligning with liberty dependencies
    Change-Id: Ia1800b633e3172bdcabfefed6bead1d460dd590e

--- gbpservice/tests/contrib/post_test_hook.sh
+++ gbpservice/tests/contrib/post_test_hook.sh
@@ -23,16 +23,10 @@ cd $GBP_FUNC_DIR/testcases
 python suite_run.py upstream
 gbpfunc_exit_code=$?
 
-# Run functional tests
-cd $GBP_DIR
-echo "Running group-based-policy dsvm-functional test suite"
-sudo -H tox -e dsvm-functional
-testr_exit_code=$?
-
 # Collect and parse results
 generate_testr_results
 
 # Prepare the log files for Jenkins to upload
 prepare_logs
 
-exit $(($exercises_exit_code+$gbpfunc_exit_code+$testr_exit_code))
+exit $(($exercises_exit_code+$gbpfunc_exit_code))
