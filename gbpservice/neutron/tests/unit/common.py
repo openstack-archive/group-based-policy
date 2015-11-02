cf797f506241196ee1bdc0514dc1ae0511322a88
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2015-11-02 10:05:21 -0800

    [WIP] Aligning with liberty dependencies
    Change-Id: Ia1800b633e3172bdcabfefed6bead1d460dd590e

--- gbpservice/neutron/tests/unit/common.py
+++ gbpservice/neutron/tests/unit/common.py
@@ -10,7 +10,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from neutron.openstack.common import uuidutils
+from oslo_utils import uuidutils
 
 _uuid = uuidutils.generate_uuid
 
