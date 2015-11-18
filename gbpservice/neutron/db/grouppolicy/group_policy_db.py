e9c4ed7d344c680c670b3f7a6739192dabf9e2d9
mageshgv <mageshgv@gmail.com>
2015-11-18 17:45:43 +0530

    Support IP protocol Numbers in GBP
    Change-Id: I5ad04fe00e1466b8b054f3b107bc81b4e9df2334
    Closes-Bug: #1499916

--- gbpservice/neutron/db/grouppolicy/group_policy_db.py
+++ gbpservice/neutron/db/grouppolicy/group_policy_db.py
@@ -12,7 +12,6 @@
 
 import netaddr
 from neutron.api.v2 import attributes as attr
-from neutron.common import constants
 from neutron.common import log
 from neutron import context
 from neutron.db import common_db_mixin
@@ -244,11 +243,7 @@ class PolicyClassifier(gquota.GBPQuotaBase, model_base.BASEV2, models_v2.HasId,
     __tablename__ = 'gp_policy_classifiers'
     name = sa.Column(sa.String(50))
     description = sa.Column(sa.String(255))
-    protocol = sa.Column(sa.Enum(constants.PROTO_NAME_TCP,
-                                 constants.PROTO_NAME_UDP,
-                                 constants.PROTO_NAME_ICMP,
-                                 name="protocol_type"),
-                         nullable=True)
+    protocol = sa.Column(sa.String(50))
     port_range_min = sa.Column(sa.Integer)
     port_range_max = sa.Column(sa.Integer)
     direction = sa.Column(sa.Enum(gp_constants.GP_DIRECTION_IN,
