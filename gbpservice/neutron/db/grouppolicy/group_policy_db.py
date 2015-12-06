884d606bf8c568d294361edac01dded9de455d9f
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2015-12-06 01:56:16 -0800

    Allow disabling of default route via DHCP
    By default the route to the default gw is always injected via DHCP.
    This patch adds an attribute "inject_default_route" to the l2_policy
    which allows controlling this behavior. The default behavior is still
    maintained.
    
    The apic_mapping driver is updated to support both setting and unsetting
    this attribute.
    
    The resource_mapping driver currently only supports setting this attribute.
    
    Change-Id: Ifdf6d51ae3a115f3d643b74e12d5693be3d15307

--- gbpservice/neutron/db/grouppolicy/group_policy_db.py
+++ gbpservice/neutron/db/grouppolicy/group_policy_db.py
@@ -133,6 +133,7 @@ class L2Policy(gquota.GBPQuotaBase, model_base.BASEV2, models_v2.HasId,
     l3_policy_id = sa.Column(sa.String(36),
                              sa.ForeignKey('gp_l3_policies.id'),
                              nullable=True)
+    inject_default_route = sa.Column(sa.Boolean)
     shared = sa.Column(sa.Boolean)
 
 
@@ -853,6 +854,7 @@ class GroupPolicyDbPlugin(gpolicy.GroupPolicyPluginBase,
                'name': l2p['name'],
                'description': l2p['description'],
                'l3_policy_id': l2p['l3_policy_id'],
+               'inject_default_route': l2p.get('inject_default_route', True),
                'shared': l2p.get('shared', False), }
         res['policy_target_groups'] = [
             ptg['id'] for ptg in l2p['policy_target_groups']]
@@ -1261,6 +1263,8 @@ class GroupPolicyDbPlugin(gpolicy.GroupPolicyPluginBase,
                               tenant_id=tenant_id, name=l2p['name'],
                               description=l2p['description'],
                               l3_policy_id=l2p['l3_policy_id'],
+                              inject_default_route=l2p.get(
+                                  'inject_default_route', True),
                               shared=l2p.get('shared', False))
             context.session.add(l2p_db)
         return self._make_l2_policy_dict(l2p_db)
--- gbpservice/neutron/db/grouppolicy/group_policy_mapping_db.py
+++ gbpservice/neutron/db/grouppolicy/group_policy_mapping_db.py
@@ -327,6 +327,8 @@ class GroupPolicyMappingDbPlugin(gpdb.GroupPolicyDbPlugin):
                                      description=l2p['description'],
                                      l3_policy_id=l2p['l3_policy_id'],
                                      network_id=l2p['network_id'],
+                                     inject_default_route=l2p.get(
+                                         'inject_default_route', True),
                                      shared=l2p.get('shared', False))
             context.session.add(l2p_db)
         return self._make_l2_policy_dict(l2p_db)
