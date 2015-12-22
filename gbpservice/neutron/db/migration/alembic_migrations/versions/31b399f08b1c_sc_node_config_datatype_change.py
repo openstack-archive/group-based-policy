7ac2b681c4e1c70843f4996ff19dcab40ffcf9b2
Vikash082 <vikash.kumar@oneconvergence.com>
2015-12-22 09:59:57 -0800

    Changing data type of 'config' column of 'sc_nodes'
    1. Closes-bug: 1528751
    2. New datatype is TEXT.
    
    Change-Id: I4d774d148f3309c23c0c30b5531cb9f6b206b7a6
    (cherry picked from commit c071c4ac704dfe8991a6f62cebac655f3a4f9844)
    (cherry picked from commit 3cff06d0db9731714aeb980bef21b9dc5b52a79d)

---
+++ gbpservice/neutron/db/migration/alembic_migrations/versions/31b399f08b1c_sc_node_config_datatype_change.py
@@ -0,0 +1,39 @@
+# Copyright 2015 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""sc_node config datatype change
+
+Revision ID: 31b399f08b1c
+Revises: 7ef98f287d6
+Create Date: 2015-12-22 09:15:44.917899
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '31b399f08b1c'
+down_revision = '7ef98f287d6'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.alter_column('sc_nodes', 'config',
+                    existing_type=sa.String(4096),
+                    type_=sa.TEXT)
+
+
+def downgrade():
+    pass
