fc1ef58e9808e7484cca913eac93ccc4c158d254
mak-454 <ahmed.khan@oneconvergence.com>
2016-01-14 23:19:17 +0530

    Multiprocess framework code

---
+++ gbpservice/neutron/nsf/core/cfg.py
@@ -0,0 +1,48 @@
+from oslo.config import cfg
+
+SERVER='rpc'
+
+OPTS = [
+    cfg.IntOpt(
+        'periodic_interval',
+        default=10,
+        help=_('Seconds between periodic task runs')
+    ),
+    cfg.StrOpt(
+        'oc_es_user',
+        default='adminii',
+        help=_('Seconds between periodic task runs')
+    ),
+    cfg.StrOpt(
+        'oc_es_password',
+        default='default',
+        help=_('Seconds between periodic task runs'),
+        secret="True"
+    ),
+    cfg.StrOpt(
+        'oc_es_tenant',
+        default='admin',
+        help=_('Seconds between periodic task runs')
+    ),
+    cfg.StrOpt(
+        'ext_network',
+        default='',
+        help=_('Seconds between periodic task runs')
+    ),
+    cfg.IntOpt(
+        'workers',
+        default=1,
+        help=_('#of workers to create.')
+    ),
+    cfg.StrOpt(
+        'RpcLoadBalancer',
+        default='RoundRobin',
+        choices=['RoundRobin', 'StickyRoundRobin'],
+        help=_('Check sc/core/lb.py for supported rpc lb algos')
+    ),
+    cfg.StrOpt(
+        'modules_dir',
+        default='sc.visibility_ahmed.modules',
+        help=_('Modules path to import ')
+    )
+]
