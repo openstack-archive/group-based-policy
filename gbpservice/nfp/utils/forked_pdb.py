9ae864095e78e0bc52acac711d5f51b2841eec4d
mak <ahmed.khan@oneconvergence.com>
2016-02-19 15:37:05 +0530

    NFP - Multi process framework implementation
    Multi process support for NFP components
    all the required platform initializations
    rpc & event handling are implemented.
    Is based on python multiprocessing.
    Supports:
    a) Autload of NFP modules & their init.
    b) RPC init & endpoint registration.
    c) Event registration.
    d) Multiple worker process.
    e) Threadpool per worker.
    f) Event loadbalancing across workers.
    g) *aas RPC Agent state reporting support.
    h) Periodic polling for an event.
    i) Individual spacing for periodic events.
    j) Event binding & sequencing.
    
    Change-Id: I99bfe8ada82c8a14302616102505541805e9021a
    Implements: blueprint gbp-network-services-framework
    Co-Authored-By: Yogesh Rajmane <yogesh.rajmane@oneconvergence.com>

---
+++ gbpservice/nfp/utils/forked_pdb.py
@@ -0,0 +1,38 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pdb
+import sys
+
+"""For debugging inside a child process.
+
+    import pdb;pdb.set_trace() does not work
+    with python multiprocessing.
+    Instead use below pdb class to debug inside
+    a worker process / child process.
+"""
+
+
+class ForkedPdb(pdb.Pdb):
+
+    """A Pdb subclass that may be used
+    from a forked multiprocessing child
+
+    """
+
+    def interaction(self, *args, **kwargs):
+        _stdin = sys.stdin
+        try:
+            sys.stdin = file('/dev/stdin')
+            pdb.Pdb.interaction(self, *args, **kwargs)
+        finally:
+            sys.stdin = _stdin
