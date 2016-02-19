3fdc219d2cf89a44062738c6cfc847a6197d10c8
mak <ahmed.khan@oneconvergence.com>
2016-02-19 15:37:05 +0530

    NFP - Multi process framework implementation.
    Multi process support for NFP components,
    all the required platform initializations,
    rpc & event handling are implemented.
    Is based on python multiprocessing.
    Supports,
    a) Autload of NFP modules & their init.
    b) RPC init & endpoint registration.
    c) Event registration.
    d) Multiple worker process.
    e) Threadpool per worker.
    f) Event loadbalancing across workers.
    g) *aas RPC Agent state reporting support.
    h) Periodic polling for an event.
    i) Individual spacing for periodic events.
    j) Event binding & sequencing.
    
    Change-Id: I99bfe8ada82c8a14302616102505541805e9021a
    Implements: blueprint gbp-network-services-framework

---
+++ gbpservice/nfp/core/event.py
@@ -0,0 +1,278 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import Queue
+import time
+
+from oslo_log import log as oslo_logging
+
+from gbpservice.nfp.core import common as nfp_common
+from gbpservice.nfp.core import poll as nfp_poll
+from gbpservice.nfp.core import threadpool as nfp_tp
+
+LOG = oslo_logging.getLogger(__name__)
+PID = os.getpid()
+identify = nfp_common.identify
+
+"""Definition of an 'EVENT' in NFP framework.
+
+    NFP modules instantiates object of this class to define and
+    create internal events.
+"""
+
+
+class Event(object):
+
+    def __init__(self, **kwargs):
+        self.serialize = kwargs.get(
+            'serialize') if 'serialize' in kwargs else False
+        self.binding_key = kwargs.get(
+            'binding_key') if 'binding_key' in kwargs else None
+        self.id = kwargs.get('id')
+        self.key = kwargs.get('key')
+        self.data = kwargs.get('data') if 'data' in kwargs else None
+        self.handler = kwargs.get('handler') if 'handler' in kwargs else None
+        self.poll_event = None  # Not to be used by user
+        self.worker_attached = None  # Not to be used by user
+        self.last_run = None  # Not to be used by user
+        self.max_times = -1  # Not to be used by user
+
+    def identify(self):
+        return "(id=%s,key=%s)" % (self.id, self.key)
+
+"""Handles the sequencing of related events.
+
+    If Event needs to be sequenced it is queued otherwise
+    it is scheduled. Caller will fetch the sequenced events
+    waiting to be scheduled in subsequent calls.
+"""
+
+
+class EventSequencer(object):
+
+    def __init__(self, sc):
+        self._sc = sc
+        """
+        sequenced events are stored in following format :
+        {'pid':{'binding_key':{'in_use':True, 'queue':[]}}}
+        """
+        self._sequencer_map = {}
+
+    def add(self, ev):
+        """Add the event to the sequencer.
+
+            Checks if there is already a related event scheduled,
+            if not, will not queue the event. If yes, then will
+            queue this event.
+            Returns True(queued)/False(not queued).
+        """
+        queued = False
+        LOG.debug("Sequence event %s" % (ev.identify()))
+        self._sc.lock()
+        if ev.worker_attached not in self._sequencer_map:
+            self._sequencer_map[ev.worker_attached] = {}
+        seq_map = self._sequencer_map[ev.worker_attached]
+        if ev.binding_key in seq_map.keys():
+            queued = True
+            LOG.debug(
+                "There is already an event in progress"
+                "Queueing event %s" % (ev.identify()))
+
+            seq_map[ev.binding_key]['queue'].append(ev)
+        else:
+            LOG.debug(
+                "Scheduling first event to exec"
+                "Event %s" % (ev.identify()))
+            seq_map[ev.binding_key] = {'in_use': True, 'queue': []}
+        self._sc.unlock()
+        return queued
+
+    def copy(self):
+        """Returns the copy of sequencer_map to caller. """
+        self._sc.lock()
+        copy = dict(self._sequencer_map)
+        self._sc.unlock()
+        return copy
+
+    def remove(self, ev):
+        """Removes an event from sequencer map.
+
+            If this is the last related event in the map, then
+            the complete entry is deleted from sequencer map.
+        """
+        self._sc.lock()
+        self._sequencer_map[ev.worker_attached][
+            ev.binding_key]['queue'].remove(ev)
+        self._sc.unlock()
+
+    def delete_eventmap(self, ev):
+        """Internal method to delete event map, if it is empty. """
+        self._sc.lock()
+        seq_map = self._sequencer_map[ev.worker_attached][ev.binding_key]
+        if seq_map['queue'] == []:
+            LOG.debug(
+                "No more events in the seq map -"
+                "Deleting the entry (%d) (%s)"
+                % (ev.worker_attached, ev.binding_key))
+            del self._sequencer_map[ev.worker_attached][ev.binding_key]
+        self._sc.unlock()
+
+"""Handles the processing of evens in event queue.
+
+    Executes in the context of worker process, runs in loop to fetch
+    the events and process them. As processing, invokes the registered
+    handler for the event.
+"""
+
+
+class EventQueueHandler(object):
+
+    def __init__(self, sc, qu, ehs):
+        # Pool of green threads per process
+        self._tpool = nfp_tp.ThreadPool()
+        self._evq = qu
+        self._ehs = ehs
+        self._sc = sc
+
+    def _get(self):
+        """Internal function to get an event for processing.
+
+            First checks in sequencer map - these events could be
+            waiting for long.
+            If no events, then fetch the events from event_queue -
+            listener process adds events into this queue.
+            Returns the event to be processed.
+        """
+        # Check if any event can be pulled from serialize_map - this evs may be
+        # waiting long enough
+        LOG.debug("Checking serialize Q for events long pending")
+        ev = self._sc.sequencer_get_event()
+        if not ev:
+            LOG.debug(
+                "No event pending in sequencer Q - "
+                "checking the event Q")
+            try:
+                ev = self._evq.get(timeout=0.1)
+            except Queue.Empty:
+                pass
+            if ev:
+                LOG.debug(
+                    "Checking if the ev %s to be serialized"
+                    % (ev.identify()))
+                """
+                If this event needs to be serialized and is first event
+                then the same is returned back, otherwise None is
+                returned. If event need not be serialized then it is
+                returned.
+                """
+                ev = self._sc.sequencer_put_event(ev)
+        return ev
+
+    def _cancelled(self, eh, ev):
+        """Internal function to cancel an event.
+
+            Removes it from poll_queue also.
+            Invokes the 'poll_event_cancel' method of the
+            registered handler if it is implemented.
+        """
+        LOG.info(
+            "Event %s cancelled -"
+            "invoking %s handler's poll_event_cancel method"
+            % (ev.identify(), identify(eh)))
+        try:
+            self._sc.poll_event_done(ev)
+            eh.poll_event_cancel(ev)
+        except AttributeError:
+            LOG.info(
+                "Handler %s does not implement"
+                "poll_event_cancel method" % (identify(eh)))
+
+    def _poll_event(self, eh, ev):
+        """Internal function to handle the poll event.
+
+            Poll task adds the timedout events to the worker process.
+            This method handles such timedout events in worker context.
+            Invoke the decorated timeout handler for the event, if any.
+            (or) invoke the default 'handle_poll_event' method of registered
+            handler.
+            """
+        LOG.debug(
+            "Event %s to be scheduled to handler %s"
+            % (ev.identify(), identify(eh)))
+
+        # Event handler can implement decorated timeout methods only if it
+        # is dervied from periodic_task. Checking here.
+        if isinstance(eh, nfp_poll.PollEventDesc):
+            # Check if this event has a decorated timeout method
+            peh = eh.get_poll_event_desc(ev)
+            if peh:
+                t = self._tpool.dispatch(peh, eh, ev)
+                LOG.info(
+                    "Dispatched method %s of handler %s"
+                    "for event %s to thread %s"
+                    % (identify(peh), identify(eh),
+                        ev.identify(), t.identify()))
+
+            else:
+                t = self._tpool.dispatch(eh.handle_poll_event, ev)
+                LOG.info(
+                    "Dispatched handle_poll_event() of handler %s"
+                    "for event %s to thread %s"
+                    % (identify(eh),
+                        ev.identify(), t.identify()))
+        else:
+            t = self._tpool.dispatch(eh.handle_poll_event, ev)
+            LOG.info(
+                "Dispatched handle_poll_event() of handler %s"
+                "for event %s to thread %s"
+                % (identify(eh),
+                    ev.identify(), t.identify()))
+
+    def run(self, qu):
+        """Worker process loop to fetch & process events from event queue.
+
+            Gets the events from event queue which is
+            python multiprocessing.queue.
+            Listener process adds events into this queue for worker process
+            to handle it.
+            Handles 3 different type of events -
+            a) POLL_EVENT - Event added by poller due to timeout.
+            b) POLL_EVENT_CANCELLED - Event added by poller due to event
+                getting cancelled as it timedout configured number of
+                max times.
+            c) EVENT - Internal event added by listener process.
+        """
+        LOG.info("Started worker process - %s" % (PID))
+        while True:
+            ev = self._get()
+            if ev:
+                LOG.debug("Got event %s" % (ev.identify()))
+                eh = self._ehs.get(ev)
+                if not ev.poll_event:
+                    t = self._tpool.dispatch(eh.handle_event, ev)
+                    LOG.debug(
+                        "Event %s is not poll event - "
+                        "disptaching handle_event() of handler %s"
+                        "to thread %s"
+                        % (ev.identify(), identify(eh), t.identify()))
+                else:
+                    if ev.poll_event == 'POLL_EVENT_CANCELLED':
+                        LOG.debug(
+                            "Got cancelled event %s" % (ev.identify()))
+                        self._cancelled(eh, ev)
+                    else:
+                        LOG.info(
+                            "Got POLL Event %s scheduling"
+                            % (ev.identify()))
+                        self._poll_event(eh, ev)
+            time.sleep(0)  # Yield the CPU
---
+++ gbpservice/nfp/core/test/visibility.py
@@ -0,0 +1,186 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import time
+
+from oslo_config import cfg
+from oslo_log import log as logging
+import oslo_messaging as messaging
+
+from gbpservice.nfp.core.main import Event
+from gbpservice.nfp.core import poll
+from gbpservice.nfp.core.poll import PollEventDesc
+from gbpservice.nfp.core.rpc import RpcAgent
+
+
+LOG = logging.getLogger(__name__)
+
+VISIBILITY_RPC_TOPIC = "visiblity_topic"
+
+
+def rpc_init(sc, conf):
+    rpcmgr = RpcManager(conf, sc)
+    agent = RpcAgent(
+        sc,
+        host=cfg.CONF.host,
+        topic=VISIBILITY_RPC_TOPIC,
+        manager=rpcmgr
+    )
+    sc.register_rpc_agents([agent])
+
+
+def events_init(sc):
+    evs = [
+        Event(id='SERVICE_CREATE', handler=Agent(sc)),
+        Event(id='SERVICE_DELETE', handler=Agent(sc)),
+        Event(id='SERVICE_DUMMY_EVENT', handler=Agent(sc))]
+    sc.register_events(evs)
+
+
+def module_init(sc, conf):
+    events_init(sc)
+    rpc_init(sc, conf)
+
+
+def unit_test(conf, sc):
+    for i in range(0, 1):
+        test_service_create(conf, sc)
+
+
+def test_service_create(conf, sc):
+    """Write the unit test logic here
+    """
+    service1 = {'id': 'sc2f2b13-e284-44b1-9d9a-2597e216271a',
+                'tenant': '40af8c0695dd49b7a4980bd1b47e1a1b',
+                'servicechain': 'sc2f2b13-e284-44b1-9d9a-2597e2161c',
+                'servicefunction': 'sf2f2b13-e284-44b1-9d9a-2597e216561d',
+                'vip_id': '13948da4-8dd9-44c6-adef-03a6d8063daa',
+                'service_vendor': 'haproxy',
+                'service_type': 'loadbalancer',
+                'ip': '192.168.20.199'
+                }
+    # Collector(service).create()
+    ev = sc.new_event(id='SERVICE_CREATE', data=service1,
+                      binding_key=service1['id'],
+                      key=service1['id'], serialize=True)
+    sc.post_event(ev)
+    service2 = {'id': 'sc2f2b13-e284-44b1-9d9a-2597e216272a',
+                'tenant': '40af8c0695dd49b7a4980bd1b47e1a2b',
+                'servicechain': 'sc2f2b13-e284-44b1-9d9a-2597e216562c',
+                'servicefunction': 'sf2f2b13-e284-44b1-9d9a-2597e216562d',
+                'mac_address': 'fa:16:3e:3f:93:05',
+                'service_vendor': 'vyos',
+                'service_type': 'firewall',
+                'ip': '192.168.20.197'
+                }
+    ev = sc.new_event(id='SERVICE_CREATE', data=service2,
+                      binding_key=service2['id'],
+                      key=service2['id'], serialize=True)
+    sc.post_event(ev)
+    service3 = {'id': 'sc2f2b13-e284-44b1-9d9a-2597e216273a',
+                'tenant': '40af8c0695dd49b7a4980bd1b47e1a2b',
+                'servicechain': 'sc2f2b13-e284-44b1-9d9a-2597e216563c',
+                'servicefunction': 'sf2f2b13-e284-44b1-9d9a-2597e216563d',
+                'mac_address': 'fa:16:3e:3f:93:05',
+                'service_vendor': 'vyos',
+                'service_type': 'vpn',
+                'ip': '192.168.20.197'
+                }
+
+    ev = sc.new_event(id='SERVICE_CREATE', data=service3,
+                      binding_key=service3['id'],
+                      key=service3['id'], serialize=True)
+    sc.post_event(ev)
+
+    time.sleep(5)
+    ev = sc.new_event(id='SERVICE_DELETE', data=service1,
+                      binding_key=service1['id'],
+                      key=service1['id'], serialize=True)
+    sc.post_event(ev)
+
+    ev = sc.new_event(id='SERVICE_DUMMY_EVENT', key='dummy_event')
+    sc.post_event(ev)
+
+
+class Collector(object):
+
+    def __init__(self, service):
+        self._service = service
+
+    def create(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+class RpcManager(object):
+    RPC_API_VERSION = '1.0'
+    target = messaging.Target(version=RPC_API_VERSION)
+
+    def __init__(self, conf, sc):
+        super(RpcManager, self).__init__()
+        self.conf = conf
+        self._sc = sc
+
+    def service_created(self, context, **kwargs):
+        pass
+
+    def service_deleted(self, context, **kwargs):
+        pass
+
+
+class Agent(PollEventDesc):
+
+    def __init__(self, sc):
+        self._sc = sc
+
+    def handle_poll_event(self, ev):
+        self._handle_poll_event(ev)
+
+    def handle_event(self, ev):
+        LOG.debug("Process ID :%d" % (os.getpid()))
+        if ev.id == 'SERVICE_CREATE':
+            self._handle_create_event(ev)
+        elif ev.id == 'SERVICE_DELETE':
+            self._handle_delete_event(ev)
+        elif ev.id == 'SERVICE_DUMMY_EVENT':
+            self._handle_dummy_event(ev)
+
+    def _handle_create_event(self, ev):
+        """Driver logic here.
+        """
+        self._sc.event_done(ev)
+        self._sc.poll_event(ev)
+
+    def _handle_dummy_event(self, ev):
+        self._sc.poll_event(ev, max_times=2)
+
+    def _handle_delete_event(self, ev):
+        """Driver logic here.
+        """
+        self._sc.event_done(ev)
+        self._sc.poll_event_done(ev)
+
+    @poll.poll_event_desc(event='SERVICE_CREATE', spacing=1)
+    def service_create_poll_event(self, ev):
+        LOG.debug("Poll event (%s)" % (str(ev)))
+
+    @poll.poll_event_desc(event='SERVICE_DUMMY_EVENT', spacing=10)
+    def service_dummy_poll_event(self, ev):
+        LOG.debug("Poll event (%s)" % (str(ev)))
+
+    def _handle_poll_event(self, ev):
+        """Driver logic here
+        """
+        LOG.debug("Poll event (%s)" % (str(ev)))
