e1163524f38515fc5ea3735fdee07a6991cd7f33
mak <ahmed.khan@oneconvergence.com>
2016-02-19 15:37:05 +0530

    NFP - Multi process framework implementation
    Framework provides multi process execution
    environment support for NFP modules.
    Multi processing is implemented using
    oslo_service and python multiprocessing library.
    All the common initializations of
    NFP modules for conf, log, processes,
    rpc, events are implemented in the framework.
    
    Supports:
    a) Autloads NFP modules & their init.
    b) RPC init & endpoint registration.
    c) Event registration.
    d) Multiple worker processes.
    e) Threadpool per worker.
    f) Event loadbalancing across workers.
    g) *aas RPC Agent state reporting support.
    h) Periodic polling for an event.
    i) Individual spacing for periodic events.
    j) Event binding & sequencing.
    
    Change-Id: I99bfe8ada82c8a14302616102505541805e9021a
    Implements: blueprint gbp-network-services-framework
    Co-Authored-By: Yogesh Rajmane <yogesh.rajmane@oneconvergence.com>
    (cherry picked from commit d43fbe9a11a08659f62562d25854b2d38bba7b76)
    (cherry picked from commit 2ed869752616d7f1726dbefd0786beaab00543f0)

---
+++ gbpservice/nfp/core/module.py
@@ -0,0 +1,110 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import abc
+import six
+
+
+def poll_event_desc(*args, **kwargs):
+    """Poll Event Decorator.
+
+    NFP modules can define the poll handlers using
+    this decorator.
+    """
+    def decorator(f):
+        f._desc = True
+        f._spacing = kwargs.pop('spacing', 0)
+        f._event = kwargs.pop('event', None)
+        return f
+
+    return decorator
+
+"""Meta class. """
+
+
+class _Meta(type):
+
+    def __init__(cls, names, bases, dict_):
+        """Metaclass that allows us to collect decorated periodic tasks."""
+        super(_Meta, cls).__init__(names, bases, dict_)
+
+        try:
+            cls._poll_desc_table = dict(cls._poll_desc_table)
+        except AttributeError:
+            cls._poll_desc_table = {}
+
+        for value in cls.__dict__.values():
+            if getattr(value, '_desc', False):
+                desc = value
+                cls._poll_desc_table[desc._event] = desc
+
+"""Base class for nfp event handlers.
+
+Nfp modules derive and implement event handlers
+of this class.
+"""
+
+
+@six.add_metaclass(_Meta)
+class NfpEventHandler(object):
+    # __metaclass__ = ABCMeta
+
+    def __init__(self):
+        super(NfpEventHandler, self).__init__()
+
+    def get_poll_desc_table(self):
+        return self._poll_desc_table
+
+    @abc.abstractmethod
+    def handle_event(self, event):
+        """To handle an event.
+
+        :param event: Object of 'Event' class.
+
+        Returns: None
+        """
+        pass
+
+    @abc.abstractmethod
+    def handle_poll_event(self, event):
+        """To handle a poll event.
+
+        Core framework will inovke this method of event handler
+        when an event timesout.
+
+        :param event: Object of 'Event' class.
+
+        Returns: {'poll':True/False, 'event':<Updated event>}
+            'poll': To repoll for the event.
+            'event': Updated event, if not passed core will
+                repoll on the old event.
+        """
+        pass
+
+    @abc.abstractmethod
+    def event_cancelled(self, event, reason):
+        """Notifies that an event is cancelled by core.
+
+        Event could get cancelled,
+            a) Event expired. Module can set lifetime for
+                an event. If event is not complete with in
+                the time, it is auto expired by core.
+            b) Event max timedout. Module can set max number
+                of times to poll for an event. Event is cancelled
+                after the max times.
+
+        :param event: Cancelled event. Object of 'Event' class.
+        :param reason: Reason for cancellation. String.
+
+        Returns: None
+        """
+        pass
