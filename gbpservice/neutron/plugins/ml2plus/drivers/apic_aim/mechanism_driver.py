e3158ede46f0512603ca44bf10a746ded764a90c
Robert Kukura <kukura@noironetworks.com>
2016-03-23 13:21:49 -0400

    [WIP] New APIC mechanism driver
    This is a very preliminary work-in-progress version of a new APIC
    mechanism driver utilizing the ACI Integration Module (AIM) library
    concurrently being developed. This driver requires the Ml2Plus
    extended driver APIs.
    
    Currently, only the tenant and network name mapping and the AIM
    integation for bridge domains is implemented. Units tests are also
    insufficient.
    
    Change-Id: I82df32f0880d6a0d53b305f6c6391fcbea049d1b

---
+++ gbpservice/neutron/plugins/ml2plus/drivers/apic_aim/mechanism_driver.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2016 Cisco Systems Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from aim import aim_manager
+from aim.api import resource as aim_resource
+from keystoneclient.v2_0 import client as keyclient
+from neutron._i18n import _LI
+from oslo_config import cfg
+from oslo_log import log
+
+from gbpservice.neutron.plugins.ml2plus import driver_api
+from gbpservice.neutron.plugins.ml2plus.drivers.apic_aim import apic_mapper
+from gbpservice.neutron.plugins.ml2plus.drivers.apic_aim import model
+
+LOG = log.getLogger(__name__)
+
+
+class ApicMechanismDriver(driver_api.MechanismDriver):
+
+    def __init__(self):
+        LOG.info(_LI("APIC AIM MD __init__"))
+
+    def initialize(self):
+        LOG.info(_LI("APIC AIM MD initializing"))
+        self.db = model.DbModel()
+        self.name_mapper = apic_mapper.APICNameMapper(
+            self.db, log, keyclient, cfg.CONF.keystone_authtoken)
+        self.aim = aim_manager.AimManager()
+
+    def ensure_tenant(self, plugin_context, tenant_id):
+        LOG.info(_LI("APIC AIM MD ensuring tenant_id: %s"), tenant_id)
+
+        tenant_name = self.name_mapper.tenant(plugin_context, tenant_id)
+        LOG.info(_LI("Mapped tenant_id %(id)s to %(apic_name)s"),
+                 {'id': tenant_id, 'apic_name': tenant_name})
+
+    def create_network_precommit(self, context):
+        LOG.info(_LI("APIC AIM MD creating network: %s"), context.current)
+
+        tenant_id = context.current['tenant_id']
+        tenant_name = self.name_mapper.tenant(context._plugin_context,
+                                              tenant_id, None)
+        LOG.info(_LI("Mapped tenant_id %(id)s to %(apic_name)s"),
+                 {'id': tenant_id, 'apic_name': tenant_name})
+
+        id = context.current['id']
+        name = context.current['name']
+        network_name = self.name_mapper.network(context._plugin_context,
+                                                id, name)
+        LOG.info(_LI("Mapped network_id %(id)s with name %(name)s to "
+                     "%(apic_name)s"),
+                 {'id': id, 'name': name, 'apic_name': network_name})
+
+        aim_context = aim_manager.AimContext(context._plugin_context.session)
+        bd = aim_resource.BridgeDomain(tenant_rn=str(tenant_name),
+                                       rn=str(network_name))
+        self.aim.create(aim_context, bd)
+
+    def delete_network_precommit(self, context):
+        LOG.info(_LI("APIC AIM MD deleting network: %s"), context.current)
+
+        tenant_id = context.current['tenant_id']
+        tenant_name = self.name_mapper.tenant(context._plugin_context,
+                                              tenant_id, None)
+        LOG.info(_LI("Mapped tenant_id %(id)s to %(apic_name)s"),
+                 {'id': tenant_id, 'apic_name': tenant_name})
+
+        id = context.current['id']
+        name = context.current['name']
+        network_name = self.name_mapper.network(context._plugin_context,
+                                                id, name)
+        LOG.info(_LI("Mapped network_id %(id)s with name %(name)s to "
+                     "%(apic_name)s"),
+                 {'id': id, 'name': name, 'apic_name': network_name})
+
+        aim_context = aim_manager.AimContext(context._plugin_context.session)
+        bd = aim_resource.BridgeDomain(tenant_rn=str(tenant_name),
+                                       rn=str(network_name))
+        self.aim.delete(aim_context, bd)
