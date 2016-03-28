303c9a3b6e44ddc590f2553b1924d0ed8272e06f
Jagadish Nadimpalli <jagadish.nadimpalli@oneconvergence.com>
2016-03-29 00:43:57 +0530

    NFP - Base Configurator Reference Implementation
    A basic configurator implementation which consumes
    all the configuration APIs from NFP components and
    just returns SUCCESS.
    Showcases the complete NFP path without actual service
    configuration.
    Executes in a nfp proxy namespace.
    
    Change-Id: Ib9d1608ecb1836e0a5a8dc7489c9adde03ddb4c8
    Implements: blueprint gbp-network-services-framework
    Co-Authored-By: Deepak S <in.live.in@live.in>
    Co-Authored-By: Hitesh Hapani <hitesh.hapani@oneconvergence.com>

---
+++ gbpservice/nfp/base_configurator/api/v1/app.py
@@ -0,0 +1,29 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# REVISIT (mak): reference_configurator/ has same file.
+# can be put in a common dir and used in both base_configurator
+# and reference_configurator
+
+
+import pecan
+
+
+def setup_app(config):
+
+    app_conf = dict(config.app)
+
+    return pecan.make_app(
+        app_conf.pop('root'),
+        logging=getattr(config, 'logging', {}),
+        **app_conf
+    )
