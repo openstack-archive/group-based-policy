a6aaca00e95b942642b74437cf3e9dcc331904e2
mageshgv <mageshgv@gmail.com>
2016-03-28 16:51:18 +0530

    NFP - DB Framework and DB Model Implementation
    This patch adds the DB Model implementation for NFP Orchestrator
    process. This DB layer framework is going to be consumed by NFP
    Orchestrator process which will be introduced in a subsequent
    patch.
    
    Change-Id: Ib249dfc9c1e5c10736d87781fcfadc5a930e2cc8
    Implements: blueprint gbp-network-services-framework

---
+++ gbpservice/nfp/orchestrator/db/api.py
@@ -0,0 +1,73 @@
+# Copyright 2011 VMware, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import contextlib
+
+from oslo_config import cfg
+from oslo_db import api as oslo_db_api
+from oslo_db.sqlalchemy import session
+from sqlalchemy import exc
+
+
+_FACADE = None
+
+MAX_RETRIES = 10
+retry_db_errors = oslo_db_api.wrap_db_retry(
+    max_retries=MAX_RETRIES,
+    retry_on_disconnect=True,
+    retry_on_deadlock=True,
+    retry_on_request=True,
+)
+
+
+def _create_facade_lazily():
+    global _FACADE
+
+    if _FACADE is None:
+        _FACADE = session.EngineFacade.from_config(cfg.CONF, sqlite_fk=True)
+
+    return _FACADE
+
+
+def get_engine():
+    """Helper method to grab engine."""
+    facade = _create_facade_lazily()
+    return facade.get_engine()
+
+
+def dispose():
+    # Don't need to do anything if an enginefacade hasn't been created
+    if _FACADE is not None:
+        get_engine().pool.dispose()
+
+
+def get_session(autocommit=True, expire_on_commit=True, use_slave=False):
+    """Helper method to grab session."""
+    facade = _create_facade_lazily()
+    return facade.get_session(autocommit=autocommit,
+                              expire_on_commit=expire_on_commit,
+                              use_slave=use_slave)
+
+
+@contextlib.contextmanager
+def autonested_transaction(sess):
+    """This is a convenience method to not bother with 'nested' parameter."""
+    try:
+        session_context = sess.begin_nested()
+    except exc.InvalidRequestError:
+        session_context = sess.begin(subtransactions=True)
+    finally:
+        with session_context as tx:
+            yield tx
