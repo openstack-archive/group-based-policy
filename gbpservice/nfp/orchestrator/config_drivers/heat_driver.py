df73a0ad9e12f14eae75b94531a639b258bab898
mageshgv <mageshgv@gmail.com>
2016-03-28 18:16:46 +0530

    NFP - Service Orchestrator module
    This module handles the lifecycle of a logical service. This module
    interacts with the Device Orchestrator module by means of the NFP
     Events. The Device Orchestrator module and the Heat configuration
    library will be introduced in subsequent patches.
    
    Change-Id: Iee97de5e55b240fbb12d41e861de5b063d615f4a
    Implements: blueprint gbp-network-services-framework
    Co-Authored-By: Ashutosh Mishra <mca.ashu4@gmail.com>
    Co-Authored-By: Akash Deep <akash.deep@oneconvergence.com>

---
+++ gbpservice/nfp/orchestrator/config_drivers/heat_driver.py
@@ -0,0 +1,62 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class HeatDriver(object):
+
+    def __init__(self, config):
+        pass
+
+    def apply_config(self, network_function_details):
+        pass
+
+    def is_config_complete(self, stack_id, tenant_id):
+        pass
+
+    def check_config_complete(self, nfp_context):
+        pass
+
+    def is_config_delete_complete(self, stack_id, tenant_id):
+        pass
+
+    def delete_config(self, stack_id, tenant_id):
+        pass
+
+    def update_config(self, network_function_details, stack_id):
+        pass
+
+    def handle_policy_target_added(self, network_function_details,
+                                   policy_target):
+        pass
+
+    def handle_policy_target_removed(self, network_function_details,
+                                     policy_target):
+        pass
+
+    def notify_chain_parameters_updated(self, network_function_details):
+        pass
+
+    def handle_consumer_ptg_added(self, network_function_details,
+                                  policy_target_group):
+        pass
+
+    def handle_consumer_ptg_removed(self, network_function_details,
+                                    policy_target_group):
+        pass
+
+    def handle_policy_target_operations(self, network_function_details,
+                                        policy_target, operation):
+        pass
+
+    def handle_consumer_ptg_operations(self, network_function_details,
+                                       policy_target_group, operation):
+        pass
