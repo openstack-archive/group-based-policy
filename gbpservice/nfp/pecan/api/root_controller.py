4cde4fe6ac1b79d946ab446bc28e2702b22a2332
Jagadish Nadimpalli <jagadish.nadimpalli@oneconvergence.com>
2016-03-29 00:43:57 +0530

    NFP - Base Configurator Reference Implementation
    A basic configurator implementation which consumes
    all the configuration APIs from NFP components and
    just returns SUCCESS.
    Showcases the complete NFP path without actual service
    configuration.
    Executes in a nfp proxy namespace.
    
    Change-Id: Ib9d1608ecb1836e0a5a8dc7489c9adde03ddb4c8
    Implements: blueprint gbp-network-services-framework
    Co-Authored-By: Deepak S <in.live.in@live.in>
    Co-Authored-By: Hitesh Hapani <hitesh.hapani@oneconvergence.com>

---
+++ gbpservice/nfp/pecan/api/root_controller.py
@@ -0,0 +1,43 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pecan
+
+from gbpservice.nfp.pecan import constants
+
+
+class RootController(object):
+    """This is root controller that forward the request to __init__.py
+    file inside controller folder inside v1
+
+    """
+    _controllers = {}
+
+    for name, controller in constants.controllers.items():
+        try:
+            _controllers.update({name: __import__(controller,
+                                                  globals(),
+                                                  locals(),
+                                                  ['controllers'], -1)})
+        except:
+           pass
+
+    if pecan.mode == constants.base_with_vm:
+        v1 = _controllers[constants.REFERENCE_CONTROLLER].V1Controller()
+    elif pecan.mode == constants.base:
+        v1 = _controllers[constants.BASE_CONTROLLER].V1Controller()
+
+    @pecan.expose()
+    def get(self):
+        return {'versions': [{'status': 'CURRENT',
+                              'updated': '2014-12-11T00:00:00Z',
+                              'id': 'v1'}]}
