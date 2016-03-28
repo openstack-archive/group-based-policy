c55da2f91df9daacd510a81445ea52da086e60b3
rajendramachani <rajendra.machani@oneconvergence.com>
2016-03-28 17:46:56 +0530

    Openstack client library for NFP
    Change-Id: I5f94290c4fc5955b6797ca9789b7f7d8325d8add
    Implements: blueprint gbp-network-services-framework

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,4 +1,5 @@
 GBP="Group-Based Policy"
+[[ $ENABLE_NFP = True ]] && NFP="Network Function Plugin"
 
 function gbp_configure_nova {
     iniset $NOVA_CONF neutron allow_duplicate_networks "True"
@@ -25,6 +26,24 @@ function gbp_configure_neutron {
     iniset $NEUTRON_CONF quotas quota_floatingip "-1"
 }
 
+function nfp_configure_neutron {
+    iniset $NEUTRON_CONF keystone_authtoken admin_tenant_name "service"
+    iniset $NEUTRON_CONF keystone_authtoken admin_user "neutron"
+    iniset $NEUTRON_CONF keystone_authtoken admin_password "admin_pass"
+    iniset $NEUTRON_CONF group_policy policy_drivers "implicit_policy,resource_mapping,chain_mapping"
+    iniset $NEUTRON_CONF node_composition_plugin node_plumber "admin_owned_resources_apic_plumber"
+    iniset $NEUTRON_CONF node_composition_plugin node_drivers "nfp_node_driver"
+    iniset $NEUTRON_CONF admin_owned_resources_apic_tscp plumbing_resource_owner_user "neutron"
+    iniset $NEUTRON_CONF admin_owned_resources_apic_tscp plumbing_resource_owner_password "admin_pass"
+    iniset $NEUTRON_CONF admin_owned_resources_apic_tscp plumbing_resource_owner_tenant_name "service"
+    iniset $NEUTRON_CONF group_policy_implicit_policy default_ip_pool "11.0.0.0/8"
+    iniset $NEUTRON_CONF group_policy_implicit_policy default_proxy_ip_pool "192.169.0.0/16"
+    iniset $NEUTRON_CONF group_policy_implicit_policy default_external_segment_name "default"
+    iniset $NEUTRON_CONF device_lifecycle_drivers drivers "haproxy, vyos"
+    iniset $NEUTRON_CONF nfp_node_driver is_service_admin_owned "True"
+    iniset $NEUTRON_CONF nfp_node_driver svc_management_ptg_name "svc_management_ptg"
+}
+
 # Process contract
 if is_service_enabled group-policy; then
     if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
@@ -36,18 +55,34 @@ if is_service_enabled group-policy; then
         gbp_configure_nova
         gbp_configure_heat
         gbp_configure_neutron
+        [[ $ENABLE_NFP = True ]] && nfp_configure_neutron
 #        install_apic_ml2
 #        install_aim
 #        init_aim
         install_gbpclient
         install_gbpservice
+        [[ $ENABLE_NFP = True ]] && install_nfpgbpservice
         init_gbpservice
+        [[ $ENABLE_NFP = True ]] && init_nfpgbpservice
         install_gbpheat
         install_gbpui
         stop_apache_server
-	start_apache_server
+        start_apache_server
+        if [[ $ENABLE_NFP = True ]]; then
+            echo_summary "Configuring $NFP"
+            nfp_configure_neutron
+            install_nfpgbpservice
+            init_nfpgbpservice
+        fi
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         echo_summary "Initializing $GBP"
+        if [[ $ENABLE_NFP = True ]]; then
+            echo_summary "Initializing $NFP"
+            assign_user_role_credential
+            create_nfp_gbp_resources
+            get_router_namespace
+            copy_nfp_files_and_start_process
+        fi
     fi
 
     if [[ "$1" == "unstack" ]]; then
