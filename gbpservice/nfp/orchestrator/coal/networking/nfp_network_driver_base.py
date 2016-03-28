fb61d0eaefd7478f4682e027a7281e498df724ee
rajendramachani <rajendra.machani@oneconvergence.com>
2016-03-28 17:46:56 +0530

    Openstack client library for NFP
    Change-Id: I5f94290c4fc5955b6797ca9789b7f7d8325d8add
    Implements: blueprint gbp-network-services-framework

---
+++ gbpservice/nfp/orchestrator/coal/networking/nfp_network_driver_base.py
@@ -0,0 +1,38 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class NFPNetworkDriverBase(object):
+    """ NFP Network Driver Base class
+
+    Handles ports, operations on them
+    """
+    def __init__(self):
+        pass
+
+    def setup_traffic_steering(self):
+        pass
+
+    def create_port(self, token, admin_id, net_id, name=None):
+        pass
+
+    def delete_port(self, token, port_id):
+        pass
+
+    def get_port_id(self, token, port_id):
+        pass
+
+    def get_port_details(self, token, port_id):
+        pass
+
+    def set_promiscuos_mode(self, token, port_id):
+        pass
