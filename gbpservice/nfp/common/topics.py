13919790e04a966b6ed9210b231864d8c60547dc
mageshgv <mageshgv@gmail.com>
2016-03-28 18:16:46 +0530

    NFP - Service Orchestrator module
    This module handles the lifecycle of a logical service. This module
    interacts with the Device Orchestrator module by means of the NFP
     Events. The Device Orchestrator module and the Heat configuration
    library will be introduced in subsequent patches.
    
    Change-Id: Iee97de5e55b240fbb12d41e861de5b063d615f4a
    Implements: blueprint gbp-network-services-framework
    Co-Authored-By: Ashutosh Mishra <mca.ashu4@gmail.com>
    Co-Authored-By: Akash Deep <akash.deep@oneconvergence.com>

---
+++ gbpservice/nfp/common/topics.py
@@ -0,0 +1,17 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+NFP_NSO_TOPIC = "nfp-service-orchestrator"
+NFP_NODE_DRIVER_CALLBACK_TOPIC = "nfp-node-driver-callbacks"
+NFP_NDO_CONFIGURATOR_TOPIC = "nfp-ndo-configurator"
+NFP_CONFIGURATOR_NDO_TOPIC = "nfp-ndo-notification-topic"
+NFP_NSO_CONFIGURATOR_TOPIC = "nfp-nso-notification-topic"
