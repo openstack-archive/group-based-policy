272325f291981039a06837f99e0210a9c4907c44
Jagadish Nadimpalli <jagadish.nadimpalli@oneconvergence.com>
2016-03-29 00:43:57 +0530

    NFP - Base Configurator Reference Implementation
    A basic configurator implementation which consumes
    all the configuration APIs from NFP components and
    just returns SUCCESS.
    Showcases the complete NFP path without actual service
    configuration.
    Executes in a nfp proxy namespace.
    
    Change-Id: Ib9d1608ecb1836e0a5a8dc7489c9adde03ddb4c8
    Implements: blueprint gbp-network-services-framework
    Co-Authored-By: Deepak S <in.live.in@live.in>
    Co-Authored-By: Hitesh Hapani <hitesh.hapani@oneconvergence.com>

---
+++ gbpservice/nfp/pecan/api/config.py
@@ -0,0 +1,61 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# Server Specific Configurations
+server = {
+    'port': '8080',
+    'host': '0.0.0.0'
+}
+
+# Pecan Application Configurations
+app = {
+    'root': 'root_controller.RootController',
+    'modules': ['v1'],
+    'debug': True,
+    'errors': {
+        404: '/error/404',
+        '__force_dict__': True
+    }
+}
+
+logging = {
+    'root': {'level': 'INFO', 'handlers': ['console']},
+    'loggers': {
+        'pecanlog': {'level': 'INFO',
+                     'handlers': ['console'],
+                     'propagate': False},
+        'pecan': {'level': 'INFO',
+                  'handlers': ['console'],
+                  'propagate': False},
+        'py.warnings': {'handlers': ['console']},
+        '__force_dict__': True
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'color'
+        }
+    },
+    'formatters': {
+        'simple': {
+            'format': ('%(asctime)s %(levelname)-5.5s [%(name)s]'
+                       '[%(threadName)s] %(message)s')
+        },
+        'color': {
+            '()': 'pecan.log.ColorFormatter',
+            'format': ('%(asctime)s [%(padded_color_levelname)s] [%(name)s]'
+                       '[%(threadName)s] %(message)s'),
+            '__force_dict__': True
+                 }
+                 }
+}
