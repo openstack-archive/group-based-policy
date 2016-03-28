00ce959233df1eb36f1a4fcb0a2816a7db63591e
rajendramachani <rajendra.machani@oneconvergence.com>
2016-03-28 21:56:31 +0530

    NFP Orchestration drivers with their unit tests
    Change-Id: Ib7ebd58e5b8db0d43d31ac75fbce0900bed1c3bb
    Implements: blueprint gbp-network-services-framework
    Co-Authored-By: ashutosh mishra <mca.ashu4@gmail.com>
    Co-Authored-By: Yogesh Rajmane <yogesh.rajmane@oneconvergence.com>

--- gbpservice/nfp/common/constants.py
+++ gbpservice/nfp/common/constants.py
@@ -10,6 +10,10 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+FIREWALL = 'firewall'
+LOADBALANCER = 'loadbalancer'
+VPN = 'vpn'
+
 GBP_MODE = "gbp"
 NEUTRON_MODE = "neutron"
 NOVA_MODE = "nova"
