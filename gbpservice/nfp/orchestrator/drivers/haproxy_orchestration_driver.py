e2350a6396f2a343f05611f0b6e08485ca6d6132
rajendramachani <rajendra.machani@oneconvergence.com>
2016-03-28 21:56:31 +0530

    NFP Orchestration drivers with their unit tests
    Change-Id: Ib7ebd58e5b8db0d43d31ac75fbce0900bed1c3bb
    Implements: blueprint gbp-network-services-framework

---
+++ gbpservice/nfp/orchestrator/drivers/haproxy_orchestration_driver.py
@@ -0,0 +1,165 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron._i18n import _LE
+from oslo_log import log as logging
+
+from gbpservice.nfp.common import constants as nfp_constants
+from gbpservice.nfp.common import exceptions
+from gbpservice.nfp.orchestrator.drivers import (
+    orchestration_driver_base as odb
+)
+
+LOG = logging.getLogger(__name__)
+
+
+class HaproxyOrchestrationDriver(odb.OrchestrationDriverBase):
+
+    def __init__(self, config=None, supports_device_sharing=True,
+                 supports_hotplug=True, max_interfaces=10):
+        super(HaproxyOrchestrationDriver, self).__init__(
+            config,
+            supports_device_sharing=supports_device_sharing,
+            supports_hotplug=supports_hotplug,
+            max_interfaces=max_interfaces)
+        self.service_vendor = 'Haproxy'
+
+    @odb._set_network_handler
+    def get_network_function_device_config_info(self, device_data,
+                                                network_handler=None):
+        """ Get the configuration information for NFD
+
+        :param device_data: NFD device
+        :type device_data: dict
+
+        :returns: None -- On Failure
+        :returns: dict -- It has the following scheme
+        {
+            'info': {
+                'version': <int>
+            },
+            'config': [
+                {
+                    'resource': 'interfaces',
+                    'kwargs': {
+                        'service_type': <str>,
+                        ...
+                    }
+                },
+                {
+                    'resource': 'routes',
+                    'kwargs': {
+                        'service_type': <str>,
+                        ...
+                    }
+                }
+            ]
+        }
+
+        :raises: exceptions.IncompleteData
+        """
+        if (
+            any(key not in device_data
+                for key in ['service_vendor',
+                            'network_model',
+                            'mgmt_ip_address',
+                            'ports',
+                            'service_details']) or
+
+            any(key not in port
+                for port in device_data['ports']
+                for key in ['id',
+                            'port_classification',
+                            'port_model'])
+        ):
+            raise exceptions.IncompleteData()
+
+        try:
+            token = (device_data['token']
+                     if device_data.get('token')
+                     else self.identity_handler.get_admin_token())
+        except Exception:
+            self._increment_stats_counter('keystone_token_get_failures')
+            LOG.error(_LE('Failed to get token'
+                          ' for get device config info operation'))
+            return None
+
+        provider_ip = None
+        provider_mac = None
+        provider_cidr = None
+        consumer_ip = None
+        consumer_mac = None
+        consumer_cidr = None
+        consumer_gateway_ip = None
+
+        for port in device_data['ports']:
+            if port['port_classification'] == nfp_constants.PROVIDER:
+                try:
+                    (provider_ip, provider_mac, provider_cidr, dummy) = (
+                            network_handler.get_port_details(token, port['id'])
+                    )
+                except Exception:
+                    self._increment_stats_counter('port_details_get_failures')
+                    LOG.error(_LE('Failed to get provider port details'
+                                  ' for get device config info operation'))
+                    return None
+            elif port['port_classification'] == nfp_constants.CONSUMER:
+                try:
+                    (consumer_ip, consumer_mac, consumer_cidr,
+                     consumer_gateway_ip) = (
+                            network_handler.get_port_details(token, port['id'])
+                    )
+                except Exception:
+                    self._increment_stats_counter('port_details_get_failures')
+                    LOG.error(_LE('Failed to get consumer port details'
+                                  ' for get device config info operation'))
+                    return None
+
+        return {
+            'info': {
+                'version': 1
+            },
+            'config': [
+                {
+                    'resource': 'interfaces',
+                    'kwargs': {
+                        'mgmt_ip': device_data['mgmt_ip_address'],
+                        'service_vendor': device_data['service_vendor'],
+                        'provider_ip': provider_ip,
+                        'provider_cidr': provider_cidr,
+                        'provider_interface_position': 2,
+                        'stitching_ip': consumer_ip,
+                        'stitching_cidr': consumer_cidr,
+                        'stitching_interface_position': 3,
+                        'provider_mac': provider_mac,
+                        'stitching_mac': consumer_mac,
+                        'service_type': (device_data['service_details'][
+                            'service_type'].lower())
+                    }
+                },
+                {
+                    'resource': 'routes',
+                    'kwargs': {
+                        'mgmt_ip': device_data['mgmt_ip_address'],
+                        'service_vendor': device_data['service_vendor'],
+                        'source_cidrs': ([provider_cidr, consumer_cidr]
+                                         if consumer_cidr
+                                         else [provider_cidr]),
+                        'destination_cidr': consumer_cidr,
+                        'gateway_ip': consumer_gateway_ip,
+                        'provider_interface_position': 2,
+                        'service_type': (device_data['service_details'][
+                            'service_type'].lower())
+                    }
+                }
+            ]
+        }
