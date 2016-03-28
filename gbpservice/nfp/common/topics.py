d538b7f069ce76e2e33bdac1470ee52c237a969a
Ashutosh Mishra <mca.ashu4@gmail.com>
2016-03-28 21:07:29 +0530

    GBP NFP - Added Base mode support in Service Orchestrator, Device won't get created in base mode support.
    Change-Id: Ia13caec590584ea8958abdffff09a6fef9dcb226
    Implements: blueprint gbp-network-services-framework

--- gbpservice/nfp/common/topics.py
+++ gbpservice/nfp/common/topics.py
@@ -10,4 +10,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+NFP_NSO_TOPIC = "nfp-service-orchestrator"
 NFP_NODE_DRIVER_CALLBACK_TOPIC = "nfp-node-driver-callbacks"
+NFP_NDO_CONFIGURATOR_TOPIC = "nfp-ndo-configurator"
+NFP_CONFIGURATOR_NDO_TOPIC = "nfp-configurator-ndo"
+NFP_NSO_CONFIGURATOR_TOPIC = "nfp-configurator-nso"
