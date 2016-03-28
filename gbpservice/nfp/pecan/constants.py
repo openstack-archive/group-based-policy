75e241001f817219ae118bce77974e6c4509acf6
Jagadish Nadimpalli <jagadish.nadimpalli@oneconvergence.com>
2016-03-29 00:43:57 +0530

    NFP - Base Configurator Reference Implementation
    A basic configurator implementation which consumes
    all the configuration APIs from NFP components and
    just returns SUCCESS.
    Showcases the complete NFP path without actual service
    configuration.
    Executes in a nfp proxy namespace.
    
    Change-Id: Ib9d1608ecb1836e0a5a8dc7489c9adde03ddb4c8
    Implements: blueprint gbp-network-services-framework
    Co-Authored-By: Deepak S <in.live.in@live.in>
    Co-Authored-By: Hitesh Hapani <hitesh.hapani@oneconvergence.com>

---
+++ gbpservice/nfp/pecan/constants.py
@@ -0,0 +1,26 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+BASE_CONTROLLER = 'base_controller'
+REFERENCE_CONTROLLER = 'reference_controller'
+
+
+controllers = {
+    BASE_CONTROLLER: 'gbpservice.nfp.base_configurator.controllers',
+    REFERENCE_CONTROLLER: ('gbpservice.tests.contrib'
+                           '.nfp_service.reference_configurator.controllers')
+}
+
+base_with_vm = 'base_with_vm'
+base = 'base'
+modes = [base, base_with_vm]
