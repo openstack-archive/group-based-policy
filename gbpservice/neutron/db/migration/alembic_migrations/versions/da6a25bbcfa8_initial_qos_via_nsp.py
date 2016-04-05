77a74dfb71b70cccde5251299d3b6df96209bac5
Igor Duarte Cardoso <igor.duarte.cardoso@intel.com>
2016-04-05 15:23:23 +0100

    Quality of Service support via NSPs
    QoS support for PTs inside PTG, as 2 new NSP param types.
    
    The new NSP param types, qos_maxrate and qos_burstrate, map to the
    Neutron QoS Policy resource, which then gets associated to a QoS
    bandwith limit rule with a certain maximum rate and/or burst rate (in
    Kbps and Kb respectively) set.
    
    Change-Id: I3950a13c482d7a7e1fa03667a543628aaa36ee6e
    Implements: blueprint initial-support-qos

---
+++ gbpservice/neutron/db/migration/alembic_migrations/versions/da6a25bbcfa8_initial_qos_via_nsp.py
@@ -0,0 +1,48 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""initial_qos_via_nsp
+
+Revision ID: da6a25bbcfa8
+Revises: c1aab79622fe
+Create Date: 2016-11-14 23:59:59.999999
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'da6a25bbcfa8'
+down_revision = '5629167be1d1'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table(
+        'gpm_qos_policy_mappings',
+        sa.Column('service_policy_id', sa.String(length=36), nullable=False),
+        sa.Column('qos_policy_id', sa.String(length=36), nullable=False),
+        sa.PrimaryKeyConstraint('service_policy_id', 'qos_policy_id'),
+        sa.ForeignKeyConstraint(['service_policy_id'],
+                                ['gp_network_service_policies.id'],
+                                ondelete='CASCADE',
+                                name='gbp_qos_policy_mapping_nsp_fk'),
+        sa.ForeignKeyConstraint(['qos_policy_id'],
+                                ['qos_policies.id'],
+                                ondelete='RESTRICT',
+                                name='gbp_qos_policy_mapping_qosp_fk')
+    )
+
+
+def downgrade():
+    op.drop_table('gpm_qos_policy_mappings')
