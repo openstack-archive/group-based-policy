ad02e6d0ebbf14977073a53cb23adb7aed5fdcb5
dpaks <in.live.in@live.in>
2016-06-22 17:03:55 +0530

    NFP - Advanced Mode Configurator
    Advanced mode configurator runs over the cloud inside a vm launched on service management network.
    
    a) Implements NFP service configuration APIs described in NFP blue print. Listed below,
    	a.1) create_network_function_device_config
    	a.2) delete_network_function_device_config
    	a.3) create_network_function_config
    	a.4) delete_network_function_config
    	a.5) get_notifications
    b) Supports framework for service specific drivers. Service drivers translates generic config APIs into service specific configuration APIs. Currently configurator stack includes following drivers,
    	b.1) vyos firewall
    	b.2) vyos vpn
    	b.3) haproxy lb
    c) Supports health monitor API. Asynchronously polls for service to be UP and notifies under the cloud.
    d) Supports asynchronous notifications for,
    	d.1) Configuration API result, SUCCESS/FAILURE
    	d.2) Initial Health check status of a service
    	d.3) Heat request
    
    Change-Id: I1a065a8b0b7b751ec0b1b7037ccf3bf515b6b04d
    Implements: blueprint gbp-network-services-framework

---
+++ gbpservice/nfp/configurator/api/config.py
@@ -0,0 +1,87 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# Server Specific Configurations
+server = {
+    'port': '8080',
+    'host': '0.0.0.0'
+}
+
+# Pecan Application Configurations
+app = {
+    'root': 'root_controller.RootController',
+    'modules': ['v1'],
+    'debug': True,
+    'errors': {
+        404: '/error/404',
+        '__force_dict__': True
+    }
+}
+
+logging = {
+    'root': {'level': 'INFO', 'handlers': ['console', 'logfile']},
+    'loggers': {
+        'pecanlog': {'level': 'INFO',
+                     'handlers': ['console', 'logfile'],
+                     'propagate': False},
+        'pecan': {'level': 'INFO',
+                  'handlers': ['console', 'logfile'],
+                  'propagate': False},
+        'py.warnings': {'handlers': ['console', 'logfile']},
+        '__force_dict__': True
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'color'
+        },
+        'logfile': {
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/nfp/nfp_pecan.log',
+            'level': 'INFO'
+        }
+    },
+    'formatters': {
+        'simple': {
+            'format': ('%(asctime)s %(levelname)-5.5s [%(name)s]'
+                       '[%(threadName)s] %(message)s')
+        },
+        'color': {
+            '()': 'pecan.log.ColorFormatter',
+            'format': ('%(asctime)s [%(padded_color_levelname)s] [%(name)s]'
+                       '[%(threadName)s] %(message)s'),
+            '__force_dict__': True
+        }
+    }
+}
+
+cloud_services = [
+    {'service_name': 'configurator',
+     'topic': 'configurator',
+     'reporting_interval': '10',  # in seconds
+     'apis': ['CONFIGURATION']
+     },
+
+    {'service_name': 'visibility',
+     'topic': 'visibility',
+     'reporting_interval': '10',  # in seconds
+     'apis': ['VISIBILITY']
+     },
+]
+
+# Custom Configurations must be in Python dictionary format::
+#
+# foo = {'bar':'baz'}
+#
+# All configurations are accessible at::
+# pecan.conf
