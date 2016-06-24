4963704ff5a381be8355a31d6507570bf70ddd97
dpaks <in.live.in@live.in>
2016-06-24 17:43:44 +0530

    NFP (contrib) - Configurator
    NFP configurator runs over the cloud inside a vm launched
    on service management network.
    
    a) Implements NFP service configuration APIs described in NFP blue
       print. Listed below,
    	a.1) create_network_function_device_config
    	a.2) delete_network_function_device_config
    	a.3) create_network_function_config
    	a.4) delete_network_function_config
    	a.5) get_notifications
    b) Supports framework for service specific drivers. Service drivers
       translates generic config APIs into service specific configuration
       APIs. Currently configurator stack includes following drivers,
    	b.1) vyos FW
    	b.2) vyos VPN
    	b.3) haproxy LB
    c) Supports health monitor API. Asynchronously polls for service to be
       UP and notifies under the cloud.
    d) Supports asynchronous notifications for,
    	d.1) Configuration API result, SUCCESS/FAILURE
    	d.2) Initial Health check status of a service
    	d.3) Heat request
    
    Change-Id: I63ae91648ccfec1ff28c584594c74e7bc36efe36
    Implements: blueprint gbp-network-services-framework
    (cherry picked from commit d2aa07e5e9dc27dbc601044c1ca26bb67835998f)
    (cherry picked from commit 5e685eba7ce7e60362304735ed5e93fcdd28bd29)

---
+++ gbpservice/contrib/tests/unit/nfp/configurator/lib/test_filter.py
@@ -0,0 +1,185 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+import filter_base
+from gbpservice.contrib.nfp.configurator.lib import data_filter
+import mock
+
+
+class FilterTest(filter_base.BaseTestCase):
+    """Test class to test data_filter.py using unittest framework """
+    def __init__(self, *args, **kwargs):
+        super(FilterTest, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        """Prepare setup for every test case.
+        """
+        super(FilterTest, self).setUp()
+        self.context = {}
+        self.filter_obj = data_filter.Filter(None, None)
+
+    def tearDown(self):
+        """ Reset values after test case execution.
+        """
+        super(FilterTest, self).tearDown()
+        self.context = {}
+
+    def _make_test(self, context, method, **filters):
+        """ To reduce the boilerplate. """
+        retval = self.filter_obj.call(self.context,
+                                      self.filter_obj.make_msg(method,
+                                                               **filters))
+        return retval
+
+    def _make_vpn_service_context(self):
+        """Make the context for the vpn service
+
+        Returns: vpn service context
+
+        """
+        service_info = self._test_get_vpn_info()
+        self.context['service_info'] = service_info
+        return self.context
+
+    def _make_lb_service_context(self):
+        """Make the context for the lb service
+
+        Returns: lb service context
+
+        """
+        service_info = self._test_get_lb_info()
+        self.context['service_info'] = service_info
+        return self.context
+
+    def _make_fw_service_context(self):
+        """Make the context for the fw service
+
+        Returns: fw service context
+
+        """
+        service_info = self._test_get_fw_info()
+        self.context['service_info'] = service_info
+        return self.context
+
+    def test_make_msg(self):
+        """Test make_msg() of data_filter.py
+        """
+        retval = self.filter_obj.make_msg('get_logical_device',
+                                          pool_id=self.pools[0]['id'])
+        self.assertEqual(retval, {'method': 'get_logical_device',
+                                  'args': {'pool_id': self.pools[0]['id']}})
+
+    def test_make_msg_empty(self):
+        """Test make_msg() of data_filter.py
+        """
+        retval = self.filter_obj.make_msg('get_logical_device')
+        self.assertEqual(retval, {'args': {}, 'method': 'get_logical_device'})
+
+    def test_call(self):
+        """Test call() of data_filter.py
+        """
+        with mock.patch.object(self.filter_obj, "call") as call_mock:
+            call_mock.return_value = True
+            retval = self._make_test(self._make_lb_service_context(),
+                                     'get_logical_device',
+                                     pool_id=[self.pools[0]['id']])
+            self.assertTrue(retval)
+
+    def test_get_vpn_service_with_tenantid(self):
+        """Test get_vpn_services() of data_filter.py by passing
+           only tenant_id in filters
+        """
+        retval = self._make_test(self._make_vpn_service_context(),
+                                 'get_vpn_services',
+                                 filters=(
+                            {'tenant_id': [self.vpnservices[0]['tenant_id']]}))
+
+        self.assertEqual(retval, [self.vpnservices[0], self.vpnservices[1]])
+
+    def test_get_vpn_service_with_ids(self):
+        """Test get_vpn_services() of data_filter.py by passing
+           vpn service ids in filters
+        """
+        retval = self._make_test(self._make_vpn_service_context(),
+                                 'get_vpn_services',
+                                 ids=[self.vpnservices[0]['id'],
+                                      self.vpnservices[1]['id']])
+        self.assertEqual(retval, [self.vpnservices[0], self.vpnservices[1]])
+
+    def test_get_ipsec_conns(self):
+        """Test get_ipsec_conns() of data_filter.py
+        """
+        retval = self._make_test(
+                self._make_vpn_service_context(),
+                'get_ipsec_conns',
+                tenant_id=[self.ipsec_site_connections[0]['tenant_id']],
+                peer_address=[self.ipsec_site_connections[0]['peer_address']])
+        self.assertEqual(retval, self.ipsec_site_connections)
+
+    def test_get_logical_device(self):
+        """Test get_logical_device() of data_filter.py
+        """
+        retval = self._make_test(self._make_lb_service_context(),
+                                 'get_logical_device',
+                                 pool_id=self.pools[0]['id'])
+
+        self.ports[0]['fixed_ips'] = self.subnets[1]
+        self.vips[0]['port'] = self.ports[0]
+        expected = {'pool': self.pools[0],
+                    'vip': self.vips[0],
+                    'members': self.members[0],
+                    'healthmonitors': {}
+                    }
+        self.assertNotEqual(retval, expected)
+
+    def test_get_vpn_servicecontext_ipsec_service_type(self):
+        """Test get_vpn_servicecontext() of data_filter.py
+           based on ipsec service type
+        """
+        service_info = self._test_get_vpn_info()
+        self.context['service_info'] = service_info
+        retval = self.filter_obj._get_vpn_servicecontext(
+                    self.context,
+                    {'tenant_id': self.vpnservices[0]['tenant_id'],
+                     'vpnservice_id': self.vpnservices[0]['id'],
+                     'ipsec_site_connections':
+                     self.ipsec_site_connections[0]['id']})
+
+        expected = {'service': self.vpnservices[0],
+                    'siteconns': [{'connection':
+                                   self.ipsec_site_connections[0],
+                                   'ikepolicy': self.ikepolicies[0],
+                                   'ipsecpolicy': self.ipsecpolicies[0]
+                                   }]}
+
+        self.assertEqual(retval, [expected])
+
+    def test_get_vpn_servicecontext_ipsec_service_type_with_tenantid(self):
+        """Test get_vpn_servicecontext() of data_filter.py
+           based on ipsec service type and tenant_id
+        """
+        service_info = self._test_get_vpn_info()
+        self.context['service_info'] = service_info
+        retval = self.filter_obj._get_vpn_servicecontext(
+                    self.context,
+                    {'tenant_id': self.vpnservices[0]['tenant_id'],
+                     })
+
+        expected = {'service': self.vpnservices[0],
+                    'siteconns': [{'connection':
+                                   self.ipsec_site_connections[0],
+                                   'ikepolicy': self.ikepolicies[0],
+                                   'ipsecpolicy': self.ipsecpolicies[0]
+                                   }]}
+
+        self.assertEqual(retval, [expected])
