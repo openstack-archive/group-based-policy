c4db63b6a9d77440226e9792fd16fbcc46a121d3
dpaks <in.live.in@live.in>
2016-06-24 17:56:00 +0530

    NFP (contrib) - Service Agents
    This changeset contains service specific agents. It supports vendor
    specific drivers for nfp-service, firewall, VPN and load balancer
    services.
    Nfp-service supports config-string which can be either heat or ansible.
    Service agents receive requests from under the cloud, parses and
    demultiplexes them into the respective service drivers.
    
    Change-Id: I3cfe17790e6a6109f48ccf35f61f0b5f91dc1a13
    Implements: blueprint gbp-network-services-framework
    Co-Authored-By: Suresh Dharavath <suresh.dharavath@oneconvergence.com>
    Co-Authored-By: ashutosh mishra <mca.ashu4@gmail.com>
    (cherry picked from commit 5e2384f1029ba5af955b97fd3ae5d8a1d0c73dd5)
    (cherry picked from commit a97840ef787b69aadb83dd5e1d76719679b209fa)

---
+++ gbpservice/contrib/tests/unit/nfp/configurator/agents/test_vpn_agent.py
@@ -0,0 +1,91 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+import mock
+
+from gbpservice.contrib.nfp.configurator.agents import vpn
+from gbpservice.contrib.nfp.configurator.lib import vpn_constants as const
+from gbpservice.contrib.tests.unit.nfp.configurator.test_data import (
+        vpn_test_data as vpn_data)
+
+from neutron.tests import base
+
+
+class VPNaasRpcManagerTestCase(base.BaseTestCase):
+    """
+    Implements test cases for RPC manager methods of vpn agent
+    """
+    def __init__(self, *args, **kwargs):
+        super(VPNaasRpcManagerTestCase, self).__init__(*args, **kwargs)
+        self.test_dict = vpn_data.VPNTestData()
+        self.conf = self.test_dict.conf
+        self.sc = mock.Mock()
+        self.rpcmgr = vpn.VPNaasRpcManager(self.conf, self.sc)
+
+    def test_vpnservice_updated(self):
+        resource_data = self.test_dict._create_ipsec_site_conn_obj()
+        with mock.patch.object(self.sc, 'new_event',
+                               return_value='foo'), (
+             mock.patch.object(self.sc, 'post_event')) as mock_post_event:
+            self.rpcmgr.vpnservice_updated(
+                        self.test_dict.make_service_context(),
+                        resource_data=resource_data)
+            mock_post_event.assert_called_with('foo')
+
+
+class VPNaasEventHandlerTestCase(base.BaseTestCase):
+    """
+    Implements test cases for RPC manager methods of vpn agent
+    """
+    def __init__(self, *args, **kwargs):
+        super(VPNaasEventHandlerTestCase, self).__init__(*args, **kwargs)
+        self.test_dict = vpn_data.VPNTestData()
+        self.sc = self.test_dict.sc
+        self.conf = self.test_dict.conf
+        self.handler = vpn.VPNaasEventHandler(self.test_dict.sc,
+                                              self.test_dict.drivers)
+        self.ev = vpn_data.FakeEvent()
+        self.rpc_sender = vpn.VpnaasRpcSender(self.sc)
+        self.driver = mock.Mock()
+
+    def test_handle_event(self):
+        """
+        Test to handle the vpn agent's vpnservice_updated method to
+        handle various vpn operations
+
+        """
+        with mock.patch.object(self.handler,
+                               '_get_driver',
+                               return_value=self.test_dict.drivers), (
+             mock.patch.object(
+                     self.driver,
+                     'vpnservice_updated')) as mock_vpnservice_updated:
+            self.handler._vpnservice_updated(self.ev, self.driver)
+            mock_vpnservice_updated.assert_called_with(self.ev.data['context'],
+                                                       self.ev.data[
+                                                           'resource_data'])
+
+    def test_sync(self):
+        """
+        Test to handle the vpn service status like ACTIVE, ERROR
+        after the configurations.
+
+        """
+        with mock.patch.object(self.rpc_sender,
+                               'get_vpn_servicecontext'), (
+             mock.patch.object(self.driver,
+                               'check_status',
+                               return_value=const.STATE_ACTIVE)):
+
+            self.handler.service_driver = self.driver
+            self.assertEqual(self.handler.sync(self.ev), {'poll': False})
