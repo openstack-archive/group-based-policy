1a4d7adc09c1d37eccf8efb2c9fdc9c2cbdd86da
dpaks <in.live.in@live.in>
2016-06-24 17:43:44 +0530

    NFP - Configurator
    NFP configurator runs over the cloud inside a vm launched
    on service management network.
    
    a) Implements NFP service configuration APIs described in NFP blue
       print. Listed below,
    	a.1) create_network_function_device_config
    	a.2) delete_network_function_device_config
    	a.3) create_network_function_config
    	a.4) delete_network_function_config
    	a.5) get_notifications
    b) Supports framework for service specific drivers. Service drivers
       translates generic config APIs into service specific configuration
       APIs. Currently configurator stack includes following drivers,
    	b.1) vyos FW
    	b.2) vyos VPN
    	b.3) haproxy LB
    c) Supports health monitor API. Asynchronously polls for service to be
       UP and notifies under the cloud.
    d) Supports asynchronous notifications for,
    	d.1) Configuration API result, SUCCESS/FAILURE
    	d.2) Initial Health check status of a service
    	d.3) Heat request
    
    Change-Id: I63ae91648ccfec1ff28c584594c74e7bc36efe36
    Implements: blueprint gbp-network-services-framework

---
+++ gbpservice/contrib/tests/unit/nfp/configurator/lib/test_demuxer.py
@@ -0,0 +1,72 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from gbpservice.contrib.nfp.configurator.lib import demuxer
+from gbpservice.contrib.tests.unit.nfp.configurator.test_data import (
+                                                        fw_test_data as fo)
+from neutron.tests import base
+
+
+class ServiceAgentDemuxerTestCase(base.BaseTestCase):
+    """ Implements test cases for demuxer of configurator. """
+    def __init__(self, *args, **kwargs):
+        super(ServiceAgentDemuxerTestCase, self).__init__(*args, **kwargs)
+        self.fo = fo.FakeObjects()
+        self.demuxer = demuxer.ServiceAgentDemuxer()
+        self.maxDiff = None
+
+    def test_get_service_type(self):
+        """ Tests that demuxer extracts the service type from
+        request data.
+
+        Returns: none
+
+        """
+
+        expected_val = 'firewall'
+
+        request_data = self.fo.fake_request_data_generic_single(self)
+        actual_val = self.demuxer.get_service_type(request_data)
+
+        self.assertEqual(actual_val, expected_val)
+
+    def test_get_service_agent_info_generic_config(self):
+        """ Tests that demuxer extracts and prepares a list of configurations
+        of generic config for each request inside the request_data.
+
+        Returns: none
+
+        """
+
+        expected_val = self.fo.fake_sa_req_list()
+
+        request_data = self.fo.fake_request_data_generic_bulk()
+        actual_val, service_type = self.demuxer.get_service_agent_info(
+                                'create', 'firewall', request_data, True)
+
+        self.assertEqual(actual_val, expected_val)
+
+    def test_get_service_agent_info_firewall(self):
+        """ Tests that demuxer extracts and prepares a list of configuration
+        of firewall for each request inside the request_data.
+
+        Returns: none
+
+        """
+
+        expected_val = self.fo.fake_sa_req_list_fw()
+
+        request_data = self.fo.fake_request_data_fw()
+        actual_val, service_type = self.demuxer.get_service_agent_info(
+                                'create', 'firewall', request_data, False)
+
+        self.assertEqual(actual_val, expected_val)
