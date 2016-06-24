2865fcf46f41021419baacb5af7f64f5732468e6
dpaks <in.live.in@live.in>
2016-06-24 17:56:00 +0530

    NFP - Service Agents
    This changeset contains service specific agents. It supports vendor
    specific drivers for nfp-service, firewall, VPN and load balancer
    services.
    Nfp-service supports config-string which can be either heat or ansible.
    Service agents receive requests from under the cloud, parses and
    demultiplexes them into the respective service drivers.
    
    Change-Id: I3cfe17790e6a6109f48ccf35f61f0b5f91dc1a13
    Implements: blueprint gbp-network-services-framework

---
+++ gbpservice/contrib/nfp/configurator/agents/firewall.py
@@ -0,0 +1,444 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import oslo_messaging as messaging
+import requests
+
+from gbpservice.contrib.nfp.configurator.agents import agent_base
+from gbpservice.contrib.nfp.configurator.lib import constants as common_const
+from gbpservice.contrib.nfp.configurator.lib import fw_constants as const
+from gbpservice.contrib.nfp.configurator.lib import utils as load_driver
+from gbpservice.nfp.core import event as nfp_event
+from gbpservice.nfp.core import log as nfp_logging
+from gbpservice.nfp.core import module as nfp_api
+
+LOG = nfp_logging.getLogger(__name__)
+
+
+class FwaasRpcSender(agent_base.AgentBaseEventHandler):
+    """ Implements Fwaas response path to Neutron plugin.
+
+    Methods of this class are invoked by the FwaasEventHandler class
+    for sending response from driver to the Fwaas Neutron plugin.
+
+    """
+
+    def __init__(self, sc, host, drivers, rpcmgr):
+        super(FwaasRpcSender, self).__init__(sc, drivers, rpcmgr)
+        self.host = host
+
+    def set_firewall_status(self, agent_info,
+                            firewall_id, status, firewall=None):
+        """ Enqueues the response from FwaaS operation to neutron plugin.
+
+        :param context: Neutron context
+        :param firewall_id: id of firewall resource
+        :param status: ACTIVE/ ERROR
+
+        """
+
+        msg = {'info': {'service_type': const.SERVICE_TYPE,
+                        'context': agent_info['context']},
+               'notification': [{
+                     'resource': agent_info['resource'],
+                     'data': {'firewall_id': firewall_id,
+                              'host': self.host,
+                              'status': status,
+                              'notification_type': (
+                                    'set_firewall_status'),
+                              'firewall': firewall}}]
+               }
+        self.notify._notification(msg)
+
+    def firewall_deleted(self, agent_info, firewall_id, firewall=None):
+        """ Enqueues the response from FwaaS operation to neutron plugin.
+
+        :param context: Neutron context
+        :param firewall_id: id of firewall resource
+
+        """
+
+        msg = {'info': {'service_type': const.SERVICE_TYPE,
+                        'context': agent_info['context']},
+               'notification': [{
+                     'resource': agent_info['resource'],
+                     'data': {'firewall_id': firewall_id,
+                              'host': self.host,
+                              'notification_type': (
+                                    'firewall_deleted'),
+                              'firewall': firewall}}]
+               }
+        self.notify._notification(msg)
+
+
+class FWaasRpcManager(agent_base.AgentBaseRPCManager):
+    """ Implements FWaasRpcManager class which receives requests
+        from Configurator to Agent.
+
+    Methods of this class are invoked by the configurator. Events are
+    created according to the requests received and enqueued to worker queues.
+
+    """
+
+    RPC_API_VERSION = '1.0'
+    target = messaging.Target(version=RPC_API_VERSION)
+
+    def __init__(self, sc, conf):
+        """Instantiates child and parent class objects.
+
+        :param sc: Service Controller object that is used to communicate
+        with process model core file.
+        :param conf: Configuration object that is used for configuration
+        parameter access.
+
+        """
+
+        super(FWaasRpcManager, self).__init__(sc, conf)
+
+    def _create_event(self, context, firewall, host, method):
+        """ Creates and enqueues the events to the worker queues.
+
+        :param context: Neutron context
+        :param firewall: Firewall resource object from neutron fwaas plugin
+        :param host: Name of the host machine
+        :param method: CREATE_FIREWALL/UPDATE_FIREWALL/DELETE_FIREWALL
+
+        """
+
+        arg_dict = {'context': context,
+                    'firewall': firewall,
+                    'host': host}
+        # REVISIT(mak): How to send large data ?
+        # New API required to send over unix sockert ?
+        context['service_info'] = {}
+        # ev = self.sc.new_event(id=method, data={}, key=None)
+        ev = self.sc.new_event(id=method, data=arg_dict, key=None)
+        self.sc.post_event(ev)
+
+    def create_firewall(self, context, firewall, host):
+        """ Receives request to create firewall from configurator
+
+        """
+
+        msg = ("FwaasRpcReceiver received Create Firewall request.")
+        LOG.debug(msg)
+        self._create_event(context, firewall,
+                           host, const.FIREWALL_CREATE_EVENT)
+
+    def update_firewall(self, context, firewall, host):
+        """ Receives request to update firewall from configurator
+
+        """
+
+        msg = ("FwaasRpcReceiver received Update Firewall request.")
+        LOG.debug(msg)
+        self._create_event(context, firewall,
+                           host, const.FIREWALL_UPDATE_EVENT)
+
+    def delete_firewall(self, context, firewall, host):
+        """ Receives request to delete firewall from configurator
+
+        """
+
+        msg = ("FwaasRpcReceiver received Delete Firewall request.")
+        LOG.debug(msg)
+        self._create_event(context, firewall,
+                           host, const.FIREWALL_DELETE_EVENT)
+
+
+class FWaasEventHandler(nfp_api.NfpEventHandler):
+    """ Handler class which invokes firewall driver methods
+
+    Worker processes dequeue the worker queues and invokes the
+    appropriate handler class methods for Fwaas methods.
+
+    """
+
+    def __init__(self, sc, drivers, rpcmgr, conf):
+        """ Instantiates class object.
+
+        :param sc: Service Controller object that is used to communicate
+        with process model core file.
+        :param drivers: dictionary of driver name to object mapping
+        :param rpcmgr: FwaasRpcManager class object
+
+        """
+
+        self.sc = sc
+        self.conf = conf
+        self.drivers = drivers
+        self.host = self.conf.host
+        self.rpcmgr = rpcmgr
+        self.plugin_rpc = FwaasRpcSender(sc, self.host,
+                                         self.drivers, self.rpcmgr)
+
+    def _get_driver(self, service_vendor):
+        """ Retrieves driver object given the service type
+
+        """
+
+        driver_id = const.SERVICE_TYPE + service_vendor
+        return self.drivers[driver_id]
+
+    def _is_firewall_rule_exists(self, fw):
+        """ Checks if firewall rules are present in the request data
+
+        :param fw: Firewall resource object
+
+        """
+
+        if not fw['firewall_rule_list']:
+            return False
+        else:
+            return True
+
+    def handle_event(self, ev):
+        """ Demultiplexes the firewall request to appropriate
+        driver methods.
+
+        :param ev: event object sent from process model event handler
+
+        """
+
+        try:
+            msg = ("Worker process with ID: %s starting to "
+                   "handle task: %s of type firewall. "
+                   % (os.getpid(), ev.id))
+            LOG.debug(msg)
+
+            # The context here in ev.data is the neutron context that was
+            # renamed to context in the agent_base. This erstwhile
+            # neutron context contains the agent info which in turn contains
+            # the API context alongside other relevant information like
+            # service vendor and type. Agent info is constructed inside
+            # the demuxer library.
+            service_vendor = ev.data['context']['agent_info']['service_vendor']
+            driver = self._get_driver(service_vendor)
+
+            self.method = getattr(driver, "%s" % (ev.id.lower()))
+            self.invoke_driver_for_plugin_api(ev)
+        except Exception as err:
+            msg = ("Failed to perform the operation: %s. %s"
+                   % (ev.id, str(err).capitalize()))
+            LOG.error(msg)
+
+    def invoke_driver_for_plugin_api(self, ev):
+        """ Invokes the appropriate driver methods
+
+        :param ev: event object sent from process model event handler
+
+        """
+
+        context = ev.data['context']
+        agent_info = context.pop('agent_info')
+        firewall = ev.data.get('firewall')
+        host = ev.data.get('host')
+
+        if ev.id == const.FIREWALL_CREATE_EVENT:
+            if not self._is_firewall_rule_exists(firewall):
+                msg = ("Firewall status set to ACTIVE")
+                LOG.debug(msg)
+                return self.plugin_rpc.set_firewall_status(
+                                agent_info, firewall['id'],
+                                common_const.STATUS_ACTIVE, firewall)
+            # Added to handle in service vm agents. VM agent will add
+            # default DROP rule.
+            # if not self._is_firewall_rule_exists(firewall):
+            #     self.plugin_rpc.set_firewall_status(
+            #         context, firewall['id'], const.STATUS_ACTIVE)
+            try:
+                status = self.method(context, firewall, host)
+            except Exception as err:
+                self.plugin_rpc.set_firewall_status(
+                    agent_info, firewall['id'], common_const.STATUS_ERROR)
+                msg = ("Failed to configure Firewall and status is "
+                       "changed to ERROR. %s." % str(err).capitalize())
+                LOG.error(msg)
+            else:
+                self.plugin_rpc.set_firewall_status(
+                    agent_info, firewall['id'], status, firewall)
+                msg = ("Configured Firewall and status set to %s" % status)
+                LOG.info(msg)
+
+        elif ev.id == const.FIREWALL_DELETE_EVENT:
+            if not self._is_firewall_rule_exists(firewall):
+                return self.plugin_rpc.firewall_deleted(
+                    agent_info, firewall['id'], firewall)
+            try:
+                status = self.method(context, firewall, host)
+            except requests.ConnectionError:
+                # REVISIT(VIKASH): It can't be correct everytime
+                msg = ("There is a connection error for firewall %r of "
+                       "tenant %r. Assuming either there is serious "
+                       "issue with VM or data path is completely "
+                       "broken. For now marking that as delete."
+                       % (firewall['id'], firewall['tenant_id']))
+                LOG.warning(msg)
+                self.plugin_rpc.firewall_deleted(
+                    agent_info, firewall['id'], firewall)
+
+            except Exception as err:
+                # REVISIT(VIKASH): Is it correct to raise ? As the subsequent
+                # attempt to clean will only re-raise the last one.And it
+                # can go on and on and may not be ever recovered.
+                self.plugin_rpc.set_firewall_status(
+                    agent_info, firewall['id'], common_const.STATUS_ERROR)
+                msg = ("Failed to delete Firewall and status is "
+                       "changed to ERROR. %s." % str(err).capitalize())
+                LOG.error(msg)
+                # raise(err)
+            else:
+                if status == common_const.STATUS_ERROR:
+                    self.plugin_rpc.set_firewall_status(
+                        agent_info, firewall['id'], status)
+                else:
+                    msg = ("Firewall %r deleted of tenant: %r" % (
+                           firewall['id'], firewall['tenant_id']))
+                    LOG.info(msg)
+                    self.plugin_rpc.firewall_deleted(
+                        agent_info, firewall['id'], firewall)
+
+        elif ev.id == const.FIREWALL_UPDATE_EVENT:
+            if not self._is_firewall_rule_exists(firewall):
+                return self.plugin_rpc.set_firewall_status(
+                                agent_info,
+                                common_const.STATUS_ACTIVE, firewall)
+            try:
+                status = self.method(context, firewall, host)
+            except Exception as err:
+                self.plugin_rpc.set_firewall_status(
+                    agent_info, firewall['id'], common_const.STATUS_ERROR)
+                msg = ("Failed to update Firewall and status is "
+                       "changed to ERROR. %s." % str(err).capitalize())
+                LOG.error(msg)
+            else:
+                self.plugin_rpc.set_firewall_status(
+                    agent_info, firewall['id'], status, firewall)
+                msg = ("Updated Firewall and status set to %s" % status)
+                LOG.info(msg)
+        else:
+            msg = ("Wrong call to Fwaas event handler.")
+            raise Exception(msg)
+
+
+def events_init(sc, drivers, rpcmgr, conf):
+    """Registers events with core service controller.
+
+    All the events will come to handle_event method of class instance
+    registered in 'handler' field.
+
+    :param drivers: Driver instances registered with the service agent
+    :param rpcmgr: Instance to receive all the RPC messages from configurator
+    module.
+
+    Returns: None
+
+    """
+
+    event_id_list = [const.FIREWALL_CREATE_EVENT,
+                     const.FIREWALL_UPDATE_EVENT,
+                     const.FIREWALL_DELETE_EVENT]
+    evs = []
+    for event in event_id_list:
+        evs.append(nfp_event.Event(id=event, handler=FWaasEventHandler(
+            sc, drivers, rpcmgr, conf)))
+    sc.register_events(evs)
+
+
+def load_drivers(conf):
+    """Imports all the driver files corresponding to this agent.
+
+    Returns: Dictionary of driver objects with a specified service type and
+    vendor name
+
+    """
+
+    ld = load_driver.ConfiguratorUtils()
+    drivers = ld.load_drivers(const.DRIVERS_DIR)
+
+    for service_type, driver_name in drivers.iteritems():
+        driver_obj = driver_name(conf=conf)
+        drivers[service_type] = driver_obj
+
+    return drivers
+
+
+def register_service_agent(cm, sc, conf, rpcmgr):
+    """Registers Fwaas service agent with configurator module.
+
+    :param cm: Instance of configurator module
+    :param sc: Instance of core service controller
+    :param conf: Instance of oslo configuration
+    :param rpcmgr: Instance containing RPC methods which are invoked by
+    configurator module on corresponding RPC message arrival
+
+    """
+
+    service_type = const.SERVICE_TYPE
+    cm.register_service_agent(service_type, rpcmgr)
+
+
+def init_agent(cm, sc, conf):
+    """Initializes Fwaas agent.
+
+    :param cm: Instance of configuration module
+    :param sc: Instance of core service controller
+    :param conf: Instance of oslo configuration
+
+    """
+
+    try:
+        drivers = load_drivers(conf)
+    except Exception as err:
+        msg = ("Fwaas failed to load drivers. %s"
+               % (str(err).capitalize()))
+        LOG.error(msg)
+        raise Exception(err)
+    else:
+        msg = ("Fwaas loaded drivers successfully.")
+        LOG.debug(msg)
+
+    rpcmgr = FWaasRpcManager(sc, conf)
+    try:
+        events_init(sc, drivers, rpcmgr, conf)
+    except Exception as err:
+        msg = ("Fwaas Events initialization unsuccessful. %s"
+               % (str(err).capitalize()))
+        LOG.error(msg)
+        raise Exception(err)
+    else:
+        msg = ("Fwaas Events initialization successful.")
+        LOG.debug(msg)
+
+    try:
+        register_service_agent(cm, sc, conf, rpcmgr)
+    except Exception as err:
+        msg = ("Fwaas service agent registration unsuccessful. %s"
+               % (str(err).capitalize()))
+        LOG.error(msg)
+        raise Exception(err)
+    else:
+        msg = ("Fwaas service agent registration successful.")
+        LOG.debug(msg)
+
+    msg = ("FIREWALL as a Service Module Initialized.")
+    LOG.info(msg)
+
+
+def init_agent_complete(cm, sc, conf):
+    """ Initializes periodic tasks
+
+    """
+
+    msg = (" Firewall agent init complete")
+    LOG.info(msg)
---
+++ gbpservice/contrib/tests/unit/nfp/configurator/agents/test_firewall.py
@@ -0,0 +1,270 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from neutron.tests import base
+from oslo_config import cfg
+
+from gbpservice.contrib.nfp.configurator.agents import firewall as fw
+from gbpservice.contrib.nfp.configurator.lib import constants as const
+from gbpservice.contrib.nfp.configurator.lib import fw_constants as fw_const
+from gbpservice.contrib.tests.unit.nfp.configurator.test_data import (
+                                                        fw_test_data as fo)
+
+
+class FWaasRpcManagerTestCase(base.BaseTestCase):
+    """ Implements test cases for RPC manager methods of firewall agent.
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(FWaasRpcManagerTestCase, self).__init__(*args, **kwargs)
+        self.fo = fo.FakeObjects()
+
+    @mock.patch(__name__ + '.fo.FakeObjects.sc')
+    @mock.patch(__name__ + '.fo.FakeObjects.conf')
+    def _get_FWaasRpcManager_object(self, conf, sc):
+        """ Retrieves RPC manager object of firewall agent.
+
+        :param sc: mocked service controller object of process model framework
+        :param conf: mocked OSLO configuration file
+
+        Returns: object of firewall's RPC manager and service controller
+
+        """
+
+        agent = fw.FWaasRpcManager(sc, conf)
+        return agent, sc
+
+    def _test_event_creation(self, method):
+        """ Tests event creation and enqueueing for create/update/delete
+        operation of firewall agent's RPC manager.
+
+        Returns: none
+
+        """
+
+        agent, sc = self._get_FWaasRpcManager_object()
+        context = {}
+        arg_dict = {'context': context,
+                    'firewall': self.fo.firewall,
+                    'host': self.fo.host}
+        with mock.patch.object(sc, 'new_event', return_value='foo') as (
+                                                            mock_sc_event), (
+             mock.patch.object(sc, 'post_event')) as mock_sc_rpc_event:
+            call_method = getattr(agent, method.lower())
+            call_method(context, self.fo.firewall, self.fo.host)
+
+            mock_sc_event.assert_called_with(id=method,
+                                             data=arg_dict, key=None)
+            mock_sc_rpc_event.assert_called_with('foo')
+
+    def test_create_firewall_fwaasrpcmanager(self):
+        """ Implements test case for create firewall method
+        of firewall agent's RPC manager.
+
+        Returns: none
+
+        """
+
+        self._test_event_creation(fw_const.FIREWALL_CREATE_EVENT)
+
+    def test_update_firewall_fwaasrpcmanager(self):
+        """ Implements test case for update firewall method
+        of firewall agent's RPC manager.
+
+        Returns: none
+
+        """
+
+        self._test_event_creation(fw_const.FIREWALL_UPDATE_EVENT)
+
+    def test_delete_firewall_fwaasrpcmanager(self):
+        """ Implements test case for delete firewall method
+        of firewall agent's RPC manager.
+
+        Returns: none
+
+        """
+
+        self._test_event_creation(fw_const.FIREWALL_DELETE_EVENT)
+
+
+class FwaasHandlerTestCase(base.BaseTestCase):
+    """ Implements test cases for event handler methods
+    of firewall agent.
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(FwaasHandlerTestCase, self).__init__(*args, **kwargs)
+        self.fo = fo.FakeObjects()
+        self.ev = fo.FakeEventFirewall()
+        self.ev.data['context']['agent_info']['resource'] = 'firewall'
+
+    @mock.patch(__name__ + '.fo.FakeObjects.rpcmgr')
+    @mock.patch(__name__ + '.fo.FakeObjects.drivers')
+    @mock.patch(__name__ + '.fo.FakeObjects.sc')
+    def _get_FwHandler_objects(self, sc, drivers, rpcmgr):
+        """ Retrieves event handler object of firewall agent.
+
+        :param sc: mocked service controller object of process model framework
+        :param drivers: list of driver objects for firewall agent
+        :param rpcmgr: object of configurator's RPC manager
+
+        Returns: object of firewall agents's event handler
+
+        """
+
+        with mock.patch.object(cfg, 'CONF') as mock_cfg:
+            mock_cfg.configure_mock(host='foo')
+            agent = fw.FWaasEventHandler(sc, drivers, rpcmgr, mock_cfg)
+        return agent
+
+    def _test_handle_event(self, rule_list_info=True):
+        """ Test handle event method of firewall agent for various
+        device configuration operations.
+
+        :param rule_list_info: an atrribute of firewall resource object
+        sent from plugin which contains the firewall rules.
+
+        Returns: None
+
+        """
+
+        agent = self._get_FwHandler_objects()
+        driver = mock.Mock()
+
+        with mock.patch.object(
+             agent.plugin_rpc, 'set_firewall_status') as (
+                                                    mock_set_fw_status), (
+            mock.patch.object(
+                agent.plugin_rpc, 'firewall_deleted')) as (mock_fw_deleted), (
+            mock.patch.object(
+                driver, fw_const.FIREWALL_CREATE_EVENT.lower())) as (
+                                                        mock_create_fw), (
+            mock.patch.object(
+                driver, fw_const.FIREWALL_UPDATE_EVENT.lower())) as (
+                                                        mock_update_fw), (
+            mock.patch.object(
+                driver, fw_const.FIREWALL_DELETE_EVENT.lower())) as (
+                                                        mock_delete_fw), (
+            mock.patch.object(
+                agent, '_get_driver', return_value=driver)):
+
+            firewall = self.fo._fake_firewall_obj()
+            if not rule_list_info:
+                firewall.update({'firewall_rule_list': False})
+                self.ev.data.get('firewall').update(
+                                            {'firewall_rule_list': False})
+            else:
+                self.ev.data.get('firewall').update(
+                                            {'firewall_rule_list': True})
+
+            agent_info = self.ev.data['context']['agent_info']
+            agent.handle_event(self.ev)
+            context = self.fo.neutron_context
+
+            if 'service_info' in self.fo.context:
+                self.fo.context.pop('service_info')
+            if not rule_list_info:
+                if self.ev.id == fw_const.FIREWALL_CREATE_EVENT:
+                    mock_set_fw_status.assert_called_with(
+                            agent_info,
+                            firewall['id'], const.STATUS_ACTIVE, firewall)
+                elif self.ev.id == fw_const.FIREWALL_UPDATE_EVENT:
+                    mock_set_fw_status.assert_called_with(
+                            agent_info,
+                            const.STATUS_ACTIVE, firewall)
+                elif self.ev.id == fw_const.FIREWALL_DELETE_EVENT:
+                    mock_fw_deleted.assert_called_with(
+                            agent_info, firewall['id'], firewall)
+            else:
+                if self.ev.id == fw_const.FIREWALL_CREATE_EVENT:
+                    mock_create_fw.assert_called_with(
+                            context,
+                            firewall, self.fo.host)
+                elif self.ev.id == fw_const.FIREWALL_UPDATE_EVENT:
+                    mock_update_fw.assert_called_with(
+                            context,
+                            firewall, self.fo.host)
+                elif self.ev.id == fw_const.FIREWALL_DELETE_EVENT:
+                    mock_delete_fw.assert_called_with(
+                            context,
+                            firewall, self.fo.host)
+
+    def test_create_firewall_with_rule_list_info_true(self):
+        """ Implements test case for create firewall method
+        of firewall agent's event handler with firewall rules.
+
+        Returns: none
+
+        """
+
+        self.ev.id = fw_const.FIREWALL_CREATE_EVENT
+        self._test_handle_event()
+
+    def test_update_firewall_with_rule_list_info_true(self):
+        """ Implements test case for update firewall method
+        of firewall agent's event handler with firewall rules.
+
+        Returns: none
+
+        """
+
+        self.ev.id = fw_const.FIREWALL_UPDATE_EVENT
+        self._test_handle_event()
+
+    def test_delete_firewall_with_rule_list_info_true(self):
+        """ Implements test case for delete firewall method
+        of firewall agent's event handler with firewall rules.
+
+        Returns: none
+
+        """
+
+        self.ev.id = fw_const.FIREWALL_DELETE_EVENT
+        self._test_handle_event()
+
+    def test_create_firewall_with_rule_list_info_false(self):
+        """ Implements test case for create firewall method
+        of firewall agent's event handler without firewall rules.
+
+        Returns: none
+
+        """
+
+        self.ev.id = fw_const.FIREWALL_CREATE_EVENT
+        self._test_handle_event(False)
+
+    def test_update_firewall_with_rule_list_info_false(self):
+        """ Implements test case for update firewall method
+        of firewall agent's event handler without firewall rules.
+
+        Returns: none
+
+        """
+
+        self.ev.id = fw_const.FIREWALL_UPDATE_EVENT
+        self._test_handle_event(False)
+
+    def test_delete_firewall_with_rule_list_info_false(self):
+        """ Implements test case for delete firewall method
+        of firewall agent's event handler without firewall rules.
+
+        Returns: none
+
+        """
+
+        self.ev.id = fw_const.FIREWALL_DELETE_EVENT
+        self._test_handle_event(False)
