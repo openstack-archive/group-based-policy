5ac57c027738aec521ded018b1d14f70abd1de79
dpaks <in.live.in@live.in>
2016-06-24 17:43:44 +0530

    NFP - Configurator
    NFP configurator runs over the cloud inside a vm launched
    on service management network.
    
    a) Implements NFP service configuration APIs described in NFP blue
       print. Listed below,
    	a.1) create_network_function_device_config
    	a.2) delete_network_function_device_config
    	a.3) create_network_function_config
    	a.4) delete_network_function_config
    	a.5) get_notifications
    b) Supports framework for service specific drivers. Service drivers
       translates generic config APIs into service specific configuration
       APIs. Currently configurator stack includes following drivers,
    	b.1) vyos FW
    	b.2) vyos VPN
    	b.3) haproxy LB
    c) Supports health monitor API. Asynchronously polls for service to be
       UP and notifies under the cloud.
    d) Supports asynchronous notifications for,
    	d.1) Configuration API result, SUCCESS/FAILURE
    	d.2) Initial Health check status of a service
    	d.3) Heat request
    
    Change-Id: I63ae91648ccfec1ff28c584594c74e7bc36efe36
    Implements: blueprint gbp-network-services-framework

---
+++ gbpservice/contrib/nfp/configurator/lib/fw_constants.py
@@ -0,0 +1,18 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+DRIVERS_DIR = 'gbpservice.contrib.nfp.configurator.drivers.firewall'
+SERVICE_TYPE = 'firewall'
+
+FIREWALL_CREATE_EVENT = 'CREATE_FIREWALL'
+FIREWALL_UPDATE_EVENT = 'UPDATE_FIREWALL'
+FIREWALL_DELETE_EVENT = 'DELETE_FIREWALL'
