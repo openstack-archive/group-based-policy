23f2ab684bff2562ac90b3b69e1eb80b54051e45
Rajendra Machani <rajendra.machani@oneconvergence.com>
2016-06-29 15:18:20 +0530

    NFP - Devstack and Diskimage Create Scripts
    This changeset contains the following
    (1) Changes to the NFP devstack to support configurator
    (2) Changes to the NFP diskimage build script to build configurator disk image
    
    Change-Id: I1717e07b82073945752549c254ed8a39ce7257ff
    Implements: blueprint gbp-network-services-framework
    Co-Authored-By: Deepak S <in.live.in@live.in>

--- gbpservice/tests/contrib/diskimage-create/disk_image_create.py
+++ gbpservice/tests/contrib/diskimage-create/disk_image_create.py
@@ -10,9 +10,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-#! /usr/bin/python
-
-import datetime
 import os
 from oslo_serialization import jsonutils
 import subprocess
@@ -31,23 +28,54 @@ def parse_json(j_file):
     return
 
 
-def dib():
+def set_nfp_git_branch(nfp_branch_name, configurator_dir):
+    Dockerfile_path = configurator_dir + '/Dockerfile'
+    cmd = "sudo sed -i \"s/GIT-BRANCH-NAME/%s/g\" %s" % (
+               nfp_branch_name.replace('/', '\/'), Dockerfile_path)
+    os.system(cmd)
+
+
+def create_configurator_docker(nfp_branch_name):
+    configurator_dir = "%s/../../../contrib/nfp/configurator" % cur_dir
+    docker_images = "%s/output/docker_images/" % cur_dir
+    if not os.path.exists(docker_images):
+        os.makedirs(docker_images)
+
+    # create a docker image
+    os.chdir(configurator_dir)
+    set_nfp_git_branch(nfp_branch_name, configurator_dir)
+    docker_args = ['docker', 'build', '-t', 'configurator-docker', '.']
+    ret = subprocess.call(docker_args)
+    if(ret):
+        print("Failed to build docker image [configurator-docker]")
+        return -1
+
+    os.chdir(docker_images)
+    del(docker_args)
+    # save the docker image
+    docker_args = ['docker', 'save', '-o', 'configurator-docker',
+                   'configurator-docker']
+    ret = subprocess.call(docker_args)
+    if(ret):
+        print("Failed to save docker image [configurator-docker]")
+        return -1
+    # set environment variable, needed by 'extra-data.d'
+    os.environ['DOCKER_IMAGES_PATH'] = docker_images
+
+    return 0
+
+
+def dib(nfp_branch_name):
     dib = conf['dib']
-    elems = cur_dir + '/elements/'
+    elems = "%s/elements/" % cur_dir
 
     # set the elements path in environment variable
     os.environ['ELEMENTS_PATH'] = elems
     # set the Ubuntu Release for the build in environment variable
     os.environ['DIB_RELEASE'] = conf['ubuntu_release']['release']
 
-    image_name = 'nfp_reference_service'
     # basic elements
-    dib_args = ['disk-image-create', 'base', 'vm', 'ubuntu', 'devuser',
-                'dhcp-all-interfaces']
-
-    # create user
-    os.environ['DIB_DEV_USER_USERNAME'] = 'ubuntu'
-    os.environ['DIB_DEV_USER_SHELL'] = '/bin/bash'
+    dib_args = ['disk-image-create', 'base', 'vm', 'ubuntu']
 
     # configures elements
     for element in dib['elements']:
@@ -55,15 +83,28 @@ def dib():
         # root login enabled, set password environment varaible
         if element == 'root-passwd':
             os.environ['DIB_PASSWORD'] = dib['root_password']
-        if element == 'nfp-reference-configurator':
-            # set environment variable, needed by 'extra-data.d'
-            service_dir = cur_dir + '/../nfp_service/'
+        elif element == 'devuser':
+            os.environ['DIB_DEV_USER_USERNAME'] = 'ubuntu'
+            os.environ['DIB_DEV_USER_SHELL'] = '/bin/bash'
+        elif element == 'nfp-reference-configurator':
+            image_name = 'nfp_reference_service'
+            service_dir = "%s/../nfp_service/" % cur_dir
             pecan_dir = os.path.abspath(os.path.join(cur_dir,
                                                      '../../../nfp'))
             service_dir = os.path.realpath(service_dir)
             pecan_dir = os.path.realpath(pecan_dir)
             os.environ['PECAN_GIT_PATH'] = pecan_dir
             os.environ['SERVICE_GIT_PATH'] = service_dir
+            if 'devuser' in dib['elements']:
+                os.environ['SSH_RSS_KEY'] = (
+                    "%s/output/%s" % (cur_dir, image_name))
+                os.environ['DIB_DEV_USER_AUTHORIZED_KEYS'] = (
+                    "%s.pub" % os.environ['SSH_RSS_KEY'])
+        elif element == 'configurator':
+            image_name = 'configurator'
+            create_configurator_docker(nfp_branch_name)
+            # for bigger size images
+            dib_args.append('--no-tmpfs')
 
     # offline mode, assuming the image cache (tar) already exists
     dib_args.append('--offline')
@@ -73,18 +114,8 @@ def dib():
 
     dib_args.append('--image-size')
     dib_args.append(str(dib['image_size_in_GB']))
-    timestamp = datetime.datetime.now().strftime('%I%M%p-%d-%m-%Y')
-    image_name = image_name + '_' + timestamp
     dib_args.append('-o')
     dib_args.append(str(image_name))
-
-    # set environment variable, needed by 'extra-data.d'
-    os.environ['NFP_IMAGE_NAME'] = image_name
-    if 'nfp-reference-configurator' in dib['elements']:
-        os.environ['SSH_RSS_KEY'] = (
-            "%s/output/%s" % (cur_dir, image_name))
-        os.environ['DIB_DEV_USER_AUTHORIZED_KEYS'] = (
-            "%s.pub" % os.environ['SSH_RSS_KEY'])
 
     os.chdir(cur_dir)
     out_dir = 'output'
@@ -95,18 +126,20 @@ def dib():
 
     ret = subprocess.call(dib_args)
     if not ret:
-        image_path = cur_dir + '/output/' + image_name + '.qcow2'
+        image_path = "%s/output/%s.qcow2" % (cur_dir, image_name)
         print("Image location: %s" % image_path)
-        with open("/tmp/nfp_image_path", "w") as f:
+        with open("%s/output/last_built_image_path" % cur_dir, "w") as f:
             f.write(image_path)
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("ERROR: Invalid Usage")
-        print("Usage:\n\t%s <json config file>" % sys.argv[0])
+        print("Usage:\n\t%s <json config file> [NFP_BRANCH_NAME]"
+              % sys.argv[0])
         print("\twhere: <json config file> contains all the configuration")
+        print("\tand NFP_BRANCH_NAME is the string, and is optional.")
         exit()
 
     # save PWD
@@ -120,5 +153,11 @@ if __name__ == "__main__":
     parse_json(sys.argv[1])
     elements = conf['dib']['elements']
 
+    nfp_branch_name = sys.argv[2] if len(sys.argv) == 3 else None
+
+    if 'configurator' in elements and nfp_branch_name is None:
+        print("ERROR: You have to pass NFP_BRANCH_NAME.")
+        exit()
+
     # run Disk Image Builder to create VM image
-    dib()
+    dib(nfp_branch_name)
