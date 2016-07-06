77a4ad7d95b9af3f80192422c0ef89d3d6a3c288
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2016-07-06 11:51:27 -0700

    Splitting NFP integration tests into new job
    Adding the necessary hooks to setup and run the NFP integration
    tests.
    
    A complementary patch will be submitted in the OpenStack project-config
    to define a new job, but this patch will have to be merged for that
    patch to merge.
    
    Once the above two patches are merged, a follow up patch will be submitted
    to remove running the NFP integration tests from the current integration
    job.
    
    Change-Id: I73c1c76b2bb8bc20fa42b342b01e2ce61da420d3
    (cherry picked from commit a002cc58ec9fb4ffecc6f773798e19015000b6d1)
    (cherry picked from commit b155a0baba2d6cd5e99cccc7f5bec73102d0ff0f)

---
+++ gbpservice/tests/contrib/post_test_nfp_hook.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source functions-gbp
+
+set -x
+
+trap prepare_logs ERR
+
+rm -rf $TOP_DIR/exercises/*
+cp $TOP_DIR/exercises-nfp/* $TOP_DIR/exercises/
+# Run exercise scripts
+$TOP_DIR/exercise.sh
+exercises_exit_code=$?
+
+source $TOP_DIR/lib/nfp
+delete_nfp_gbp_resources $TOP_DIR
+
+# Check if exercises left any resources undeleted
+check_residual_resources neutron service
+check_residual_resources admin admin
+check_residual_resources admin demo
+check_residual_resources demo demo
+
+# Prepare the log files for Jenkins to upload
+prepare_logs
+
+exit $(($exercises_exit_code))
