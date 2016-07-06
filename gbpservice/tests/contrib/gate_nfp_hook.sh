c0a2884ddde3b85a27be814e9fbe8fb56a72290b
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2016-07-06 11:51:27 -0700

    Splitting NFP integration tests into new job
    Adding the necessary hooks to setup and run the NFP integration
    tests.
    
    A complementary patch will be submitted in the OpenStack project-config
    to define a new job, but this patch will have to be merged for that
    patch to merge.
    
    Once the above two patches are merged, a follow up patch will be submitted
    to remove running the NFP integration tests from the current integration
    job.
    
    Change-Id: I73c1c76b2bb8bc20fa42b342b01e2ce61da420d3
    (cherry picked from commit a002cc58ec9fb4ffecc6f773798e19015000b6d1)
    (cherry picked from commit b155a0baba2d6cd5e99cccc7f5bec73102d0ff0f)

---
+++ gbpservice/tests/contrib/gate_nfp_hook.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CONTRIB_DIR="$BASE/new/group-based-policy/gbpservice/tests/contrib"
+cp $CONTRIB_DIR/functions-gbp .
+source functions-gbp
+
+set -x
+
+trap prepare_logs ERR
+
+prepare_gbp_devstack
+prepare_nfp_devstack
+$TOP_DIR/stack.sh
+
+# Use devstack functions to install mysql and psql servers
+source $TOP_DIR/stackrc
+source $TOP_DIR/lib/database
+disable_service postgresql
+enable_service mysql
+initialize_database_backends
+install_database
+
+# Set up the 'openstack_citest' user and database in each backend
+tmp_dir=`mktemp -d`
+
+cat << EOF > $tmp_dir/mysql.sql
+CREATE DATABASE openstack_citest;
+CREATE USER 'openstack_citest'@'localhost' IDENTIFIED BY 'openstack_citest';
+CREATE USER 'openstack_citest' IDENTIFIED BY 'openstack_citest';
+GRANT ALL PRIVILEGES ON *.* TO 'openstack_citest'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'openstack_citest';
+FLUSH PRIVILEGES;
+EOF
+/usr/bin/mysql -u root < $tmp_dir/mysql.sql
