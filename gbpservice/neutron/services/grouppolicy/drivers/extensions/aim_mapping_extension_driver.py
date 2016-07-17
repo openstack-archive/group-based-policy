d0cb712c37f617551248d2e5207b99c789fa8da8
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2016-07-16 22:11:02 -0700

    AIM Policy Driver - Part 2 - Extension Driver
    This adds a AIM specific extension to reflect the APIC DN for
    a GBP resource that maps to an AIM resource. This patch implements
    this only for the PTG resource.
    
    Change-Id: I30f5e5e63b3b172eb79c8a9934eb662928d13f6c

---
+++ gbpservice/neutron/services/grouppolicy/drivers/extensions/aim_mapping_extension_driver.py
@@ -0,0 +1,49 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron._i18n import _LI
+from neutron import manager as n_manager
+from oslo_log import log as logging
+
+from gbpservice.neutron.extensions import aim_driver_ext
+from gbpservice.neutron.services.grouppolicy import (
+    group_policy_driver_api as api)
+
+LOG = logging.getLogger(__name__)
+
+
+class AIMExtensionDriver(api.ExtensionDriver):
+    _supported_extension_alias = aim_driver_ext.AIM_DRIVER_EXT
+    _extension_dict = aim_driver_ext.EXTENDED_ATTRIBUTES_2_0
+
+    def __init__(self):
+        LOG.info(_LI("AIM Extension __init__"))
+        self._policy_driver = None
+
+    @property
+    def _pd(self):
+        if not self._policy_driver:
+            gbp_plugin = (n_manager.NeutronManager.get_service_plugins()
+                          .get("GROUP_POLICY"))
+            policy_mgr = gbp_plugin.policy_driver_manager
+            self._policy_driver = policy_mgr.policy_drivers['aim_mapping'].obj
+        return self._policy_driver
+
+    def initialize(self):
+        pass
+
+    @property
+    def extension_alias(self):
+        return self._supported_extension_alias
+
+    def extend_policy_target_group_dict(self, session, result):
+        self._pd.extend_policy_target_group_dict(session, result)
