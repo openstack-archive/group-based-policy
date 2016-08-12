ae3e8bf0b91b4cda55fbff587763880eefb02199
Ivar Lazzaro <ivarlazzaro@gmail.com>
2016-08-12 15:00:00 -0700

    [wip][aim] Set APIC domains on EPG creation
    NOTE: This patch is based on the apic-domains branch of AIM. Move
    it back to master once the pull request is merged
    
    Change-Id: I45fa45c37e83502df21a877d2ceab8fb3f6c5836

--- gbpservice/neutron/plugins/ml2plus/drivers/apic_aim/mechanism_driver.py
+++ gbpservice/neutron/plugins/ml2plus/drivers/apic_aim/mechanism_driver.py
@@ -15,6 +15,7 @@
 
 from aim import aim_manager
 from aim.api import resource as aim_resource
+from aim.common import utils
 from aim import config as aim_cfg
 from aim import context as aim_context
 from aim import utils as aim_utils
@@ -138,10 +139,16 @@ class ApicMechanismDriver(api_plus.MechanismDriver):
                                        enable_routing=False,
                                        limit_ip_learn_to_subnets=True)
         self.aim.create(aim_ctx, bd)
+        vmms = [x.name for x in self.aim.find(aim_ctx, aim_resource.VMMDomain)
+                if x.type == utils.OPENSTACK_VMM_TYPE]
+        phys = [x.name for x in
+                self.aim.find(aim_ctx, aim_resource.PhysicalDomain)]
         epg = aim_resource.EndpointGroup(tenant_name=tenant_aname,
                                          app_profile_name=self.ap_name,
                                          name=aname, display_name=dname,
-                                         bd_name=aname)
+                                         bd_name=aname,
+                                         openstack_vmm_domain_names=vmms,
+                                         physical_domain_names=phys)
         self.aim.create(aim_ctx, epg)
 
     def update_network_precommit(self, context):
