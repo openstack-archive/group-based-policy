d0a80300170129c9abf549e7aa0988e116fa2fe2
Ashutosh Mishra <mca.ashu4@gmail.com>
2016-08-24 18:53:25 +0530

    NFP - Added LBaaSV2 support
    This changeset contains following changes -
    1) NFP LBaaSV2 agent
    2) NFP LBaaSV2 haproxy service driver along with octavia
       lib files
    3) NFP LBaaSV2 plugin driver
    
    Change-Id: I3fdf63a023ad5241f6325151c9be3b814be7f3b8

---
+++ gbpservice/contrib/nfp/configurator/drivers/loadbalancer/v2/haproxy/octavia_lib/common/constants.py
@@ -0,0 +1,313 @@
+# Copyright 2014 Rackspace
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+LB_ALGORITHM_ROUND_ROBIN = 'ROUND_ROBIN'
+LB_ALGORITHM_LEAST_CONNECTIONS = 'LEAST_CONNECTIONS'
+LB_ALGORITHM_SOURCE_IP = 'SOURCE_IP'
+SUPPORTED_LB_ALGORITHMS = (LB_ALGORITHM_LEAST_CONNECTIONS,
+                           LB_ALGORITHM_ROUND_ROBIN,
+                           LB_ALGORITHM_SOURCE_IP)
+
+SESSION_PERSISTENCE_SOURCE_IP = 'SOURCE_IP'
+SESSION_PERSISTENCE_HTTP_COOKIE = 'HTTP_COOKIE'
+SESSION_PERSISTENCE_APP_COOKIE = 'APP_COOKIE'
+SUPPORTED_SP_TYPES = (SESSION_PERSISTENCE_SOURCE_IP,
+                      SESSION_PERSISTENCE_HTTP_COOKIE,
+                      SESSION_PERSISTENCE_APP_COOKIE)
+
+HEALTH_MONITOR_PING = 'PING'
+HEALTH_MONITOR_TCP = 'TCP'
+HEALTH_MONITOR_HTTP = 'HTTP'
+HEALTH_MONITOR_HTTPS = 'HTTPS'
+SUPPORTED_HEALTH_MONITOR_TYPES = (HEALTH_MONITOR_HTTP, HEALTH_MONITOR_HTTPS,
+                                  HEALTH_MONITOR_PING, HEALTH_MONITOR_TCP)
+
+UPDATE_STATS = 'UPDATE_STATS'
+UPDATE_HEALTH = 'UPDATE_HEALTH'
+
+PROTOCOL_TCP = 'TCP'
+PROTOCOL_HTTP = 'HTTP'
+PROTOCOL_HTTPS = 'HTTPS'
+PROTOCOL_TERMINATED_HTTPS = 'TERMINATED_HTTPS'
+SUPPORTED_PROTOCOLS = (PROTOCOL_TCP, PROTOCOL_HTTPS, PROTOCOL_HTTP,
+                       PROTOCOL_TERMINATED_HTTPS)
+
+# Note: The database Amphora table has a foreign key constraint against
+#       the provisioning_status table
+# Amphora has been allocated to a load balancer
+AMPHORA_ALLOCATED = 'ALLOCATED'
+# Amphora is being built
+AMPHORA_BOOTING = 'BOOTING'
+# Amphora is ready to be allocated to a load balancer
+AMPHORA_READY = 'READY'
+
+ACTIVE = 'ACTIVE'
+PENDING_DELETE = 'PENDING_DELETE'
+PENDING_UPDATE = 'PENDING_UPDATE'
+PENDING_CREATE = 'PENDING_CREATE'
+DELETED = 'DELETED'
+ERROR = 'ERROR'
+SUPPORTED_PROVISIONING_STATUSES = (ACTIVE, AMPHORA_ALLOCATED,
+                                   AMPHORA_BOOTING, AMPHORA_READY,
+                                   PENDING_DELETE, PENDING_CREATE,
+                                   PENDING_UPDATE, DELETED, ERROR)
+MUTABLE_STATUSES = (ACTIVE,)
+
+SUPPORTED_AMPHORA_STATUSES = (AMPHORA_ALLOCATED, AMPHORA_BOOTING,
+                              AMPHORA_READY, DELETED, PENDING_DELETE)
+
+ONLINE = 'ONLINE'
+OFFLINE = 'OFFLINE'
+DEGRADED = 'DEGRADED'
+ERROR = 'ERROR'
+NO_MONITOR = 'NO_MONITOR'
+OPERATING_STATUS = 'operating_status'
+SUPPORTED_OPERATING_STATUSES = (ONLINE, OFFLINE, DEGRADED, ERROR, NO_MONITOR)
+
+AMPHORA_VM = 'VM'
+SUPPORTED_AMPHORA_TYPES = (AMPHORA_VM,)
+
+# L7 constants
+L7RULE_TYPE_HOST_NAME = 'HOST_NAME'
+L7RULE_TYPE_PATH = 'PATH'
+L7RULE_TYPE_FILE_TYPE = 'FILE_TYPE'
+L7RULE_TYPE_HEADER = 'HEADER'
+L7RULE_TYPE_COOKIE = 'COOKIE'
+SUPPORTED_L7RULE_TYPES = (L7RULE_TYPE_HOST_NAME, L7RULE_TYPE_PATH,
+                          L7RULE_TYPE_FILE_TYPE, L7RULE_TYPE_HEADER,
+                          L7RULE_TYPE_COOKIE)
+
+L7RULE_COMPARE_TYPE_REGEX = 'REGEX'
+L7RULE_COMPARE_TYPE_STARTS_WITH = 'STARTS_WITH'
+L7RULE_COMPARE_TYPE_ENDS_WITH = 'ENDS_WITH'
+L7RULE_COMPARE_TYPE_CONTAINS = 'CONTAINS'
+L7RULE_COMPARE_TYPE_EQUAL_TO = 'EQUAL_TO'
+SUPPORTED_L7RULE_COMPARE_TYPES = (L7RULE_COMPARE_TYPE_REGEX,
+                                  L7RULE_COMPARE_TYPE_STARTS_WITH,
+                                  L7RULE_COMPARE_TYPE_ENDS_WITH,
+                                  L7RULE_COMPARE_TYPE_CONTAINS,
+                                  L7RULE_COMPARE_TYPE_EQUAL_TO)
+
+L7POLICY_ACTION_REJECT = 'REJECT'
+L7POLICY_ACTION_REDIRECT_TO_URL = 'REDIRECT_TO_URL'
+L7POLICY_ACTION_REDIRECT_TO_POOL = 'REDIRECT_TO_POOL'
+SUPPORTED_L7POLICY_ACTIONS = (L7POLICY_ACTION_REJECT,
+                              L7POLICY_ACTION_REDIRECT_TO_URL,
+                              L7POLICY_ACTION_REDIRECT_TO_POOL)
+
+# Largest a 32-bit integer can be, which is a limitation
+# here if you're using MySQL, as most probably are. This just needs
+# to be larger than any existing rule position numbers which will
+# definitely be the case with 2147483647
+MAX_POLICY_POSITION = 2147483647
+
+# Testing showed haproxy config failed to parse after more than
+# 53 rules per policy
+MAX_L7RULES_PER_L7POLICY = 50
+
+# See RFCs 2616, 2965, 6265, 7230: Should match characters valid in a
+# http header or cookie name.
+HTTP_HEADER_NAME_REGEX = r'\A[a-zA-Z0-9!#$%&\'*+-.^_`|~]+\Z'
+
+# See RFCs 2616, 2965, 6265: Should match characters valid in a cookie value.
+HTTP_COOKIE_VALUE_REGEX = r'\A[a-zA-Z0-9!#$%&\'()*+-./:<=>?@[\]^_`{|}~]+\Z'
+
+# See RFC 7230: Should match characters valid in a header value.
+HTTP_HEADER_VALUE_REGEX = (r'\A[a-zA-Z0-9'
+                           r'!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~\\]+\Z')
+
+# Also in RFC 7230: Should match characters valid in a header value
+# when quoted with double quotes.
+HTTP_QUOTED_HEADER_VALUE_REGEX = (r'\A"[a-zA-Z0-9 \t'
+                                  r'!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~\\]*"\Z')
+
+# Task/Flow constants
+AMPHORA = 'amphora'
+FAILED_AMPHORA = 'failed_amphora'
+FAILOVER_AMPHORA = 'failover_amphora'
+AMPHORAE = 'amphorae'
+AMPHORA_ID = 'amphora_id'
+FAILOVER_AMPHORA_ID = 'failover_amphora_id'
+DELTA = 'delta'
+DELTAS = 'deltas'
+HEALTH_MON = 'health_mon'
+LISTENER = 'listener'
+LISTENERS = 'listeners'
+LOADBALANCER = 'loadbalancer'
+LOADBALANCER_ID = 'loadbalancer_id'
+SERVER_GROUP_ID = 'server_group_id'
+ANTI_AFFINITY = 'anti-affinity'
+MEMBER = 'member'
+MEMBER_ID = 'member_id'
+COMPUTE_ID = 'compute_id'
+COMPUTE_OBJ = 'compute_obj'
+AMP_DATA = 'amp_data'
+AMPS_DATA = 'amps_data'
+NICS = 'nics'
+VIP = 'vip'
+POOL = 'pool'
+POOL_ID = 'pool_id'
+L7POLICY = 'l7policy'
+L7RULE = 'l7rule'
+OBJECT = 'object'
+SERVER_PEM = 'server_pem'
+UPDATE_DICT = 'update_dict'
+VIP_NETWORK = 'vip_network'
+AMPHORAE_NETWORK_CONFIG = 'amphorae_network_config'
+ADDED_PORTS = 'added_ports'
+PORTS = 'ports'
+MEMBER_PORTS = 'member_ports'
+LOADBALANCER_TOPOLOGY = 'topology'
+
+CERT_ROTATE_AMPHORA_FLOW = 'octavia-cert-rotate-amphora-flow'
+CREATE_AMPHORA_FLOW = 'octavia-create-amphora-flow'
+CREATE_AMPHORA_FOR_LB_FLOW = 'octavia-create-amp-for-lb-flow'
+CREATE_HEALTH_MONITOR_FLOW = 'octavia-create-health-monitor-flow'
+CREATE_LISTENER_FLOW = 'octavia-create-listener_flow'
+PRE_CREATE_LOADBALANCER_FLOW = 'octavia-pre-create-loadbalancer-flow'
+CREATE_SERVER_GROUP_FLOW = 'octavia-create-server-group-flow'
+UPDATE_LB_SERVERGROUPID_FLOW = 'octavia-update-lb-server-group-id-flow'
+CREATE_LISTENERS_FLOW = 'octavia-create-all-listeners-flow'
+CREATE_LOADBALANCER_FLOW = 'octavia-create-loadbalancer-flow'
+CREATE_LOADBALANCER_GRAPH_FLOW = 'octavia-create-loadbalancer-graph-flow'
+CREATE_MEMBER_FLOW = 'octavia-create-member-flow'
+CREATE_POOL_FLOW = 'octavia-create-pool-flow'
+CREATE_L7POLICY_FLOW = 'octavia-create-l7policy-flow'
+CREATE_L7RULE_FLOW = 'octavia-create-l7rule-flow'
+DELETE_AMPHORA_FLOW = 'octavia-delete-amphora-flow'
+DELETE_HEALTH_MONITOR_FLOW = 'octavia-delete-health-monitor-flow'
+DELETE_LISTENER_FLOW = 'octavia-delete-listener_flow'
+DELETE_LOADBALANCER_FLOW = 'octavia-delete-loadbalancer-flow'
+DELETE_MEMBER_FLOW = 'octavia-delete-member-flow'
+DELETE_POOL_FLOW = 'octavia-delete-pool-flow'
+DELETE_L7POLICY_FLOW = 'octavia-delete-l7policy-flow'
+DELETE_L7RULE_FLOW = 'octavia-delete-l7policy-flow'
+FAILOVER_AMPHORA_FLOW = 'octavia-failover-amphora-flow'
+LOADBALANCER_NETWORKING_SUBFLOW = 'octavia-new-loadbalancer-net-subflow'
+UPDATE_HEALTH_MONITOR_FLOW = 'octavia-update-health-monitor-flow'
+UPDATE_LISTENER_FLOW = 'octavia-update-listener-flow'
+UPDATE_LOADBALANCER_FLOW = 'octavia-update-loadbalancer-flow'
+UPDATE_MEMBER_FLOW = 'octavia-update-member-flow'
+UPDATE_POOL_FLOW = 'octavia-update-pool-flow'
+UPDATE_L7POLICY_FLOW = 'octavia-update-l7policy-flow'
+UPDATE_L7RULE_FLOW = 'octavia-update-l7rule-flow'
+WAIT_FOR_AMPHORA = 'wait-for-amphora'
+
+FAILOVER_AMPHORA_FLOW = 'octavia-failover-amphora-flow'
+
+POST_MAP_AMP_TO_LB_SUBFLOW = 'octavia-post-map-amp-to-lb-subflow'
+CREATE_AMP_FOR_LB_SUBFLOW = 'octavia-create-amp-for-lb-subflow'
+GET_AMPHORA_FOR_LB_SUBFLOW = 'octavia-get-amphora-for-lb-subflow'
+POST_LB_AMP_ASSOCIATION_SUBFLOW = (
+    'octavia-post-loadbalancer-amp_association-subflow')
+
+MAP_LOADBALANCER_TO_AMPHORA = 'octavia-mapload-balancer-to-amphora'
+RELOAD_AMPHORA = 'octavia-reload-amphora'
+CREATE_AMPHORA_INDB = 'octavia-create-amphora-indb'
+GENERATE_SERVER_PEM = 'octavia-generate-serverpem'
+UPDATE_CERT_EXPIRATION = 'octavia-update-cert-expiration'
+CERT_COMPUTE_CREATE = 'octavia-cert-compute-create'
+COMPUTE_CREATE = 'octavia-compute-create'
+UPDATE_AMPHORA_COMPUTEID = 'octavia-update-amphora-computeid'
+MARK_AMPHORA_BOOTING_INDB = 'octavia-mark-amphora-booting-indb'
+WAIT_FOR_AMPHORA = 'octavia-wait_for_amphora'
+COMPUTE_WAIT = 'octavia-compute-wait'
+UPDATE_AMPHORA_INFO = 'octavia-update-amphora-info'
+AMPHORA_FINALIZE = 'octavia-amphora-finalize'
+MARK_AMPHORA_ALLOCATED_INDB = 'octavia-mark-amphora-allocated-indb'
+RELOADLOAD_BALANCER = 'octavia-reloadload-balancer'
+MARK_LB_ACTIVE_INDB = 'octavia-mark-lb-active-indb'
+MARK_AMP_MASTER_INDB = 'octavia-mark-amp-master-indb'
+MARK_AMP_BACKUP_INDB = 'octavia-mark-amp-backup-indb'
+MARK_AMP_STANDALONE_INDB = 'octavia-mark-amp-standalone-indb'
+GET_VRRP_SUBFLOW = 'octavia-get-vrrp-subflow'
+AMP_VRRP_UPDATE = 'octavia-amphora-vrrp-update'
+AMP_VRRP_START = 'octavia-amphora-vrrp-start'
+AMP_VRRP_STOP = 'octavia-amphora-vrrp-stop'
+AMP_UPDATE_VRRP_INTF = 'octavia-amphora-update-vrrp-intf'
+CREATE_VRRP_GROUP_FOR_LB = 'octavia-create-vrrp-group-for-lb'
+CREATE_VRRP_SECURITY_RULES = 'octavia-create-vrrp-security-rules'
+
+GENERATE_SERVER_PEM_TASK = 'GenerateServerPEMTask'
+
+# Task Names
+RELOAD_LB_AFTER_AMP_ASSOC = 'reload-lb-after-amp-assoc'
+RELOAD_LB_AFTER_AMP_ASSOC_FULL_GRAPH = 'reload-lb-after-amp-assoc-full-graph'
+RELOAD_LB_AFTER_PLUG_VIP = 'reload-lb-after-plug-vip'
+
+NOVA_1 = '1.1'
+NOVA_2 = '2'
+NOVA_3 = '3'
+NOVA_VERSIONS = (NOVA_1, NOVA_2, NOVA_3)
+
+RPC_NAMESPACE_CONTROLLER_AGENT = 'controller'
+
+
+# Active standalone roles and topology
+TOPOLOGY_SINGLE = 'SINGLE'
+TOPOLOGY_ACTIVE_STANDBY = 'ACTIVE_STANDBY'
+ROLE_MASTER = 'MASTER'
+ROLE_BACKUP = 'BACKUP'
+ROLE_STANDALONE = 'STANDALONE'
+
+SUPPORTED_LB_TOPOLOGIES = (TOPOLOGY_ACTIVE_STANDBY, TOPOLOGY_SINGLE)
+SUPPORTED_AMPHORA_ROLES = (ROLE_BACKUP, ROLE_MASTER, ROLE_STANDALONE)
+
+TOPOLOGY_STATUS_OK = 'OK'
+
+ROLE_MASTER_PRIORITY = 100
+ROLE_BACKUP_PRIORITY = 90
+
+VRRP_AUTH_DEFAULT = 'PASS'
+VRRP_AUTH_AH = 'AH'
+SUPPORTED_VRRP_AUTH = (VRRP_AUTH_DEFAULT, VRRP_AUTH_AH)
+
+KEEPALIVED_CMD = '/usr/sbin/keepalived '
+# The DEFAULT_VRRP_ID value needs to be variable for multi tenant support
+# per amphora in the future
+DEFAULT_VRRP_ID = 1
+VRRP_PROTOCOL_NUM = 112
+AUTH_HEADER_PROTOCOL_NUMBER = 51
+
+
+TEMPLATES = '/templates'
+AGENT_API_TEMPLATES = '/templates'
+
+AGENT_CONF_TEMPLATE = 'amphora_agent_conf.template'
+USER_DATA_CONFIG_DRIVE_TEMPLATE = 'user_data_config_drive.template'
+
+OPEN = 'OPEN'
+FULL = 'FULL'
+
+# OPEN = HAProxy listener status nbconn < maxconn
+# FULL = HAProxy listener status not nbconn < maxconn
+HAPROXY_LISTENER_STATUSES = (OPEN, FULL)
+
+UP = 'UP'
+DOWN = 'DOWN'
+
+# UP = HAProxy backend has working or no servers
+# DOWN = HAProxy backend has no working servers
+HAPROXY_BACKEND_STATUSES = (UP, DOWN)
+
+
+NO_CHECK = 'no check'
+
+HAPROXY_MEMBER_STATUSES = (UP, DOWN, NO_CHECK)
+
+API_VERSION = '0.5'
+
+HAPROXY_BASE_PEER_PORT = 1025
+KEEPALIVED_CONF = 'keepalived.conf.j2'
+CHECK_SCRIPT_CONF = 'keepalived_check_script.conf.j2'
---
+++ gbpservice/contrib/nfp/configurator/drivers/loadbalancer/v2/haproxy/octavia_lib/common/jinja/haproxy/jinja_cfg.py
@@ -0,0 +1,336 @@
+#    Copyright (c) 2015 Rackspace
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import re
+
+import jinja2
+import six
+
+from oslo_config import cfg
+from gbpservice.contrib.nfp.configurator.drivers.loadbalancer.v2.haproxy.octavia_lib.\
+    common import constants
+from gbpservice.contrib.nfp.configurator.drivers.loadbalancer.v2.haproxy.octavia_lib.\
+    common import utils as octavia_utils
+
+PROTOCOL_MAP = {
+    constants.PROTOCOL_TCP: 'tcp',
+    constants.PROTOCOL_HTTP: 'http',
+    constants.PROTOCOL_HTTPS: 'tcp',
+    constants.PROTOCOL_TERMINATED_HTTPS: 'http'
+}
+
+BALANCE_MAP = {
+    constants.LB_ALGORITHM_ROUND_ROBIN: 'roundrobin',
+    constants.LB_ALGORITHM_LEAST_CONNECTIONS: 'leastconn',
+    constants.LB_ALGORITHM_SOURCE_IP: 'source'
+}
+
+ACTIVE_PENDING_STATUSES = constants.SUPPORTED_PROVISIONING_STATUSES + (
+    constants.DEGRADED,)
+
+BASE_PATH = '/var/lib/octavia'
+BASE_CRT_DIR = BASE_PATH + '/certs'
+
+HAPROXY_TEMPLATE = os.path.abspath(
+    os.path.join(os.path.dirname(__file__),
+                 'templates/haproxy.cfg.j2'))
+
+CONF = cfg.CONF
+# CONF.import_group('haproxy_amphora', 'octavia.common.config')
+
+JINJA_ENV = None
+
+
+class JinjaTemplater(object):
+
+    def __init__(self,
+                 base_amp_path=None,
+                 base_crt_dir=None,
+                 haproxy_template=None,
+                 log_http=None,
+                 log_server=None,
+                 timeout_client=None,
+                 timeout_server=None,
+                 timeout_connect=None):
+        """HaProxy configuration generation
+
+        :param base_amp_path: Base path for amphora data
+        :param base_crt_dir: Base directory for certificate storage
+        :param haproxy_template: Absolute path to Jinja template
+        :param log_http: Haproxy HTTP logging path
+        :param log_server: Haproxy Server logging path
+        :param timeout_client: Timeout client
+        :param timeout_server: Timeout server
+        :param timeout_connect: Timeout connect
+        """
+
+        self.base_amp_path = base_amp_path if base_amp_path else BASE_PATH
+        self.base_crt_dir = base_crt_dir if base_crt_dir else BASE_CRT_DIR
+        self.haproxy_template = (haproxy_template if haproxy_template
+                                 else HAPROXY_TEMPLATE)
+        self.log_http = log_http
+        self.log_server = log_server
+        self.timeout_client = timeout_client
+        self.timeout_server = timeout_server
+        self.timeout_connect = timeout_connect
+
+    def build_config(self, listener, tls_cert,
+                     socket_path=None,
+                     user_group='nogroup'):
+        """Convert a logical configuration to the HAProxy version
+
+        :param listener: The listener configuration
+        :param tls_cert: The TLS certificates for the listener
+        :param socket_path: The socket path for Haproxy process
+        :param user_group: The user group
+        :return: Rendered configuration
+        """
+        return self.render_loadbalancer_obj(listener,
+                                            tls_cert=tls_cert,
+                                            user_group=user_group,
+                                            socket_path=socket_path)
+
+    def _get_template(self):
+        """Returns the specified Jinja configuration template."""
+        global JINJA_ENV
+        if not JINJA_ENV:
+            template_loader = jinja2.FileSystemLoader(
+                searchpath=os.path.dirname(self.haproxy_template))
+            JINJA_ENV = jinja2.Environment(
+                loader=template_loader,
+                trim_blocks=True,
+                lstrip_blocks=True)
+        JINJA_ENV.filters['hash_amp_id'] = octavia_utils.base64_sha1_string
+        return JINJA_ENV.get_template(os.path.basename(self.haproxy_template))
+
+    def render_loadbalancer_obj(self, listener,
+                                tls_cert=None,
+                                user_group='nogroup',
+                                socket_path=None):
+        """Renders a templated configuration from a load balancer object
+
+        :param listener: The listener configuration
+        :param tls_cert: The TLS certificates for the listener
+        :param socket_path: The socket path for Haproxy process
+        :param user_group: The user group
+        :return: Rendered configuration
+        """
+        loadbalancer = self._transform_loadbalancer(
+            listener.load_balancer,
+            listener,
+            tls_cert)
+        if not socket_path:
+            socket_path = '%s/%s.sock' % (self.base_amp_path, listener.id)
+        return self._get_template().render(
+            {'loadbalancer': loadbalancer,
+             'user_group': user_group,
+             'stats_sock': socket_path,
+             'log_http': self.log_http,
+             'log_server': self.log_server,
+             'timeout_client': self.timeout_client,
+             'timeout_server': self.timeout_server,
+             'timeout_connect': self.timeout_connect},
+            constants=constants)
+
+    def _transform_loadbalancer(self, loadbalancer, listener, tls_cert):
+        """Transforms a load balancer into an object that will
+
+           be processed by the templating system
+        """
+        listener = self._transform_listener(listener, tls_cert)
+        return {
+            'name': loadbalancer.name,
+            'vip_address': loadbalancer.vip.ip_address,
+            'listener': listener,
+            'topology': loadbalancer.topology,
+            'enabled': loadbalancer.enabled
+        }
+
+    def _transform_listener(self, listener, tls_cert):
+        """Transforms a listener into an object that will
+
+            be processed by the templating system
+        """
+        ret_value = {
+            'id': listener.id,
+            'protocol_port': listener.protocol_port,
+            'protocol_mode': PROTOCOL_MAP[listener.protocol],
+            'protocol': listener.protocol,
+            'peer_port': listener.peer_port,
+            'topology': listener.load_balancer.topology,
+            'amphorae': listener.load_balancer.amphorae,
+            'enabled': listener.enabled
+        }
+        if listener.connection_limit and listener.connection_limit > -1:
+            ret_value['connection_limit'] = listener.connection_limit
+        if listener.tls_certificate_id:
+            ret_value['default_tls_path'] = '%s.pem' % (
+                os.path.join(self.base_crt_dir,
+                             listener.id,
+                             tls_cert.primary_cn))
+        if listener.sni_containers:
+            ret_value['crt_dir'] = os.path.join(self.base_crt_dir, listener.id)
+        if listener.default_pool:
+            ret_value['default_pool'] = self._transform_pool(
+                listener.default_pool)
+        pools = [self._transform_pool(x) for x in listener.pools]
+        ret_value['pools'] = pools
+        l7policies = [self._transform_l7policy(x) for x in listener.l7policies]
+        ret_value['l7policies'] = l7policies
+        return ret_value
+
+    def _transform_pool(self, pool):
+        """Transforms a pool into an object that will
+
+            be processed by the templating system
+        """
+        ret_value = {
+            'id': pool.id,
+            'protocol': PROTOCOL_MAP[pool.protocol],
+            'lb_algorithm': BALANCE_MAP.get(pool.lb_algorithm, 'roundrobin'),
+            'members': [],
+            'health_monitor': '',
+            'session_persistence': '',
+            'enabled': pool.enabled,
+            'operating_status': pool.operating_status,
+            'stick_size': CONF.haproxy_amphora.haproxy_stick_size
+        }
+        members = [self._transform_member(x) for x in pool.members]
+        ret_value['members'] = members
+        if pool.health_monitor:
+            ret_value['health_monitor'] = self._transform_health_monitor(
+                pool.health_monitor)
+        if pool.session_persistence:
+            ret_value[
+                'session_persistence'] = self._transform_session_persistence(
+                pool.session_persistence)
+        return ret_value
+
+    @staticmethod
+    def _transform_session_persistence(persistence):
+        """Transforms session persistence into an object that will
+
+            be processed by the templating system
+        """
+        return {
+            'type': persistence.type,
+            'cookie_name': persistence.cookie_name
+        }
+
+    @staticmethod
+    def _transform_member(member):
+        """Transforms a member into an object that will
+
+            be processed by the templating system
+        """
+        return {
+            'id': member.id,
+            'address': member.ip_address,
+            'protocol_port': member.protocol_port,
+            'weight': member.weight,
+            'enabled': member.enabled,
+            'subnet_id': member.subnet_id,
+            'operating_status': member.operating_status
+        }
+
+    def _transform_health_monitor(self, monitor):
+        """Transforms a health monitor into an object that will
+
+            be processed by the templating system
+        """
+        codes = None
+        if monitor.expected_codes:
+            codes = '|'.join(self._expand_expected_codes(
+                monitor.expected_codes))
+        return {
+            'id': monitor.id,
+            'type': monitor.type,
+            'delay': monitor.delay,
+            'timeout': monitor.timeout,
+            'fall_threshold': monitor.fall_threshold,
+            'rise_threshold': monitor.rise_threshold,
+            'http_method': monitor.http_method,
+            'url_path': monitor.url_path,
+            'expected_codes': codes,
+            'enabled': monitor.enabled,
+        }
+
+    def _transform_l7policy(self, l7policy):
+        """Transforms an L7 policy into an object that will
+
+            be processed by the templating system
+        """
+        ret_value = {
+            'id': l7policy.id,
+            'action': l7policy.action,
+            'redirect_url': l7policy.redirect_url,
+            'enabled': l7policy.enabled
+        }
+        if l7policy.redirect_pool:
+            ret_value['redirect_pool'] = self._transform_pool(
+                l7policy.redirect_pool)
+        else:
+            ret_value['redirect_pool'] = None
+        l7rules = [self._transform_l7rule(x) for x in l7policy.l7rules]
+        ret_value['l7rules'] = l7rules
+        return ret_value
+
+    def _transform_l7rule(self, l7rule):
+        """Transforms an L7 rule into an object that will
+
+            be processed by the templating system
+        """
+        return {
+            'id': l7rule.id,
+            'type': l7rule.type,
+            'compare_type': l7rule.compare_type,
+            'key': l7rule.key,
+            'value': self._escape_haproxy_config_string(l7rule.value),
+            'invert': l7rule.invert
+        }
+
+    @staticmethod
+    def _escape_haproxy_config_string(value):
+        """Escapes certain characters in a given string such that
+
+            haproxy will parse the string as a single value
+        """
+        # Escape backslashes first
+        value = re.sub(r'\\', r'\\\\', value)
+        # Spaces next
+        value = re.sub(' ', '\\ ', value)
+        return value
+
+    @staticmethod
+    def _expand_expected_codes(codes):
+        """Expand the expected code string in set of codes.
+
+        200-204 -> 200, 201, 202, 204
+        200, 203 -> 200, 203
+        """
+
+        retval = set()
+        for code in codes.replace(',', ' ').split(' '):
+            code = code.strip()
+
+            if not code:
+                continue
+            elif '-' in code:
+                low, hi = code.split('-')[:2]
+                retval.update(
+                    str(i) for i in six.moves.xrange(int(low), int(hi) + 1))
+            else:
+                retval.add(code)
+        return retval
