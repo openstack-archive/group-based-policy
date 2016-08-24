881b26f9230fc9f0e7eb7fe0414a31fe66c4ff28
Ashutosh Mishra <mca.ashu4@gmail.com>
2016-08-24 18:49:35 +0530

    NFP - Controller Enhancements
    This changeset contains following changes -
    1) Load service drivers from different paths -
       Driver loading logic in configurator is changed.
       Drivers can be loaded from different paths now.
       Drivers path is specified in configurator.ini file
    
    2) Addressed the issue of configurator losing notifications
       from it's queue when restarted -
       Notifications from Configurator to UTC components
       are sent on rabbitmq using oslo messaging rpc cast()
       and when UTC component requests for notifications
       advanced controller pulls them from rabbitmq using
       pika RPC library.
    
    3) Update service chain node driver support for Loadbalancer
       and Firewall service -
       a) Loadbalancer - HM and pool resources can be updated
       b) Firewall - New firewall rules can added,
                     existing rules can be deleted or updated
    
    4) Redirecting advanced controller logs to nfp_pecan.log file
    
    Change-Id: Id0c95e1ad1c6c6757781a8734465eab8eae80436

---
+++ gbpservice/contrib/nfp/configurator/modules/__init__.py
@@ -0,0 +1,34 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_config import cfg as oslo_config
+
+nfp_configurator_extra_opts = [
+       oslo_config.StrOpt('log_forward_ip_address',
+                          default='', help='Log collector host IP address'),
+       oslo_config.IntOpt('log_forward_port',
+                          default='514', help='Log collector port number'),
+       oslo_config.StrOpt('log_level',
+                          default='debug',
+                          help='Log level info/error/debug/warning')]
+
+oslo_config.CONF.register_opts(nfp_configurator_extra_opts, "configurator")
+
+
+nfp_configurator_config_drivers_opts = [
+    oslo_config.ListOpt(
+        'drivers',
+        default=['gbpservice.contrib.nfp.configurator.drivers'],
+        help='List of config driver directories')]
+
+oslo_config.CONF.register_opts(nfp_configurator_config_drivers_opts,
+                               "CONFIG_DRIVERS")
