068537001e6d5da61669647ca669a65a4ed82a11
Ashutosh Mishra <mca.ashu4@gmail.com>
2016-08-24 18:53:25 +0530

    NFP - Added LBaaSV2 support
    This changeset contains following changes -
    1) NFP LBaaSV2 agent
    2) NFP LBaaSV2 haproxy service driver along with octavia
       lib files
    3) NFP LBaaSV2 plugin driver
    
    Change-Id: I3fdf63a023ad5241f6325151c9be3b814be7f3b8
    (cherry picked from commit b85524cc7708d3f7afb045e1752c4c9137edaaa8)

---
+++ gbpservice/contrib/nfp/configurator/drivers/loadbalancer/v2/haproxy/config.py
@@ -0,0 +1,131 @@
+# Copyright 2011 VMware, Inc., 2014 A10 Networks
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Routines for configuring Octavia Haproxy driver
+"""
+
+from oslo_config import cfg
+
+from gbpservice.nfp.core import log as nfp_logging
+
+LOG = nfp_logging.getLogger(__name__)
+
+haproxy_amphora_opts = [
+    cfg.StrOpt('base_path',
+               default='/var/lib/octavia',
+               help=_('Base directory for amphora files.')),
+    cfg.StrOpt('base_cert_dir',
+               default='/var/lib/octavia/certs',
+               help=_('Base directory for cert storage.')),
+    cfg.StrOpt('haproxy_template', help=_('Custom haproxy template.')),
+    cfg.IntOpt('connection_max_retries',
+               default=10,
+               help=_('Retry threshold for connecting to amphorae.')),
+    cfg.IntOpt('connection_retry_interval',
+               default=5,
+               help=_('Retry timeout between connection attempts in '
+                      'seconds.')),
+    cfg.StrOpt('haproxy_stick_size', default='10k',
+               help=_('Size of the HAProxy stick table. Accepts k, m, g '
+                      'suffixes.  Example: 10k')),
+
+    # REST server
+    cfg.IPOpt('bind_host', default='0.0.0.0',
+              help=_("The host IP to bind to")),
+    cfg.PortOpt('bind_port', default=9443,
+                help=_("The port to bind to")),
+    cfg.StrOpt('haproxy_cmd', default='/usr/sbin/haproxy',
+               help=_("The full path to haproxy")),
+    cfg.IntOpt('respawn_count', default=2,
+               help=_("The respawn count for haproxy's upstart script")),
+    cfg.IntOpt('respawn_interval', default=2,
+               help=_("The respawn interval for haproxy's upstart script")),
+    cfg.FloatOpt('rest_request_conn_timeout', default=10,
+                 help=_("The time in seconds to wait for a REST API "
+                        "to connect.")),
+    cfg.FloatOpt('rest_request_read_timeout', default=60,
+                 help=_("The time in seconds to wait for a REST API "
+                        "response.")),
+    # REST client
+    cfg.StrOpt('client_cert', default='/etc/octavia/certs/client.pem',
+               help=_("The client certificate to talk to the agent")),
+    cfg.StrOpt('server_ca', default='/etc/octavia/certs/server_ca.pem',
+               help=_("The ca which signed the server certificates")),
+    cfg.BoolOpt('use_upstart', default=True,
+                help=_("If False, use sysvinit.")),
+]
+
+certificate_opts = [
+    cfg.StrOpt('cert_manager',
+               default='local_cert_manager',
+               help='Name of the cert manager to use'),
+    cfg.StrOpt('cert_generator',
+               default='local_cert_generator',
+               help='Name of the cert generator to use'),
+]
+
+# Options only used by the amphora agent
+amphora_agent_opts = [
+    cfg.StrOpt('agent_server_ca', default='/etc/octavia/certs/client_ca.pem',
+               help=_("The ca which signed the client certificates")),
+    cfg.StrOpt('agent_server_cert', default='/etc/octavia/certs/server.pem',
+               help=_("The server certificate for the agent.py server "
+                      "to use")),
+    cfg.StrOpt('agent_server_network_dir',
+               default='/etc/network/interfaces.d/',
+               help=_("The directory where new network interfaces "
+                      "are located")),
+    cfg.StrOpt('agent_server_network_file',
+               help=_("The file where the network interfaces are located. "
+                      "Specifying this will override any value set for "
+                      "agent_server_network_dir.")),
+    # Do not specify in octavia.conf, loaded at runtime
+    cfg.StrOpt('amphora_id', help=_("The amphora ID.")),
+]
+
+keepalived_vrrp_opts = [
+    cfg.IntOpt('vrrp_advert_int',
+               default=1,
+               help=_('Amphora role and priority advertisement interval '
+                      'in seconds.')),
+    cfg.IntOpt('vrrp_check_interval',
+               default=5,
+               help=_('VRRP health check script run interval in seconds.')),
+    cfg.IntOpt('vrrp_fail_count',
+               default=2,
+               help=_('Number of successive failure before transition to a '
+                      'fail state.')),
+    cfg.IntOpt('vrrp_success_count',
+               default=2,
+               help=_('Number of successive failure before transition to a '
+                      'success state.')),
+    cfg.IntOpt('vrrp_garp_refresh_interval',
+               default=5,
+               help=_('Time in seconds between gratuitous ARP announcements '
+                      'from the MASTER.')),
+    cfg.IntOpt('vrrp_garp_refresh_count',
+               default=2,
+               help=_('Number of gratuitous ARP announcements to make on '
+                      'each refresh interval.'))
+
+]
+
+# Register the configuration options
+cfg.CONF.register_opts(amphora_agent_opts, group='amphora_agent')
+cfg.CONF.register_opts(certificate_opts, group='certificates')
+cfg.CONF.register_opts(haproxy_amphora_opts, group='haproxy_amphora')
+cfg.CONF.register_opts(keepalived_vrrp_opts, group='keepalived_vrrp')
+CONF = cfg.CONF
