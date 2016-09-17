845858fb547a182809dbaa92bffe876210ed89ae
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2016-09-17 09:53:05 -0700

    [WIP][apic-mapping] Segmentation label ext for PTs
    This defines a new extension: apic_mapping_extension, for the apic_mapping
    policy driver. An extension attribute: segmentation_labels that extends the
    Policy Target definition is being introduced in this extension.
    
    A corresponding extension driver: apic_mapping_extension is being added that
    processes this extension.
    
    The segementation_labels attribute is a list of strings. Each string can be
    upto 1024 characters long. These labels are not interpreted by GBP but are
    instead passed downstream by the apic_mapping driver. It is assumed that these
    are defined outside of OpenStack and the backend system can appropriately
    interpret them.
    
    Change-Id: I360bf9f7f1d4bdca76d4f16b7535a6416f430830

---
+++ gbpservice/neutron/db/grouppolicy/extensions/apic_mapping_driver_ext_db.py
@@ -0,0 +1,49 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron.db import model_base
+import sqlalchemy as sa
+
+
+class ApicMappingExtensionDB(model_base.BASEV2):
+    __tablename__ = 'gp_apic_mapping_segmentation_lablels'
+    policy_target_id = sa.Column(
+        sa.String(36), sa.ForeignKey('gp_policy_targets.id',
+                                     ondelete="CASCADE"), primary_key=True)
+    segmentation_label = sa.Column(sa.String(1024), primary_key=True)
+
+
+class ApicMappingExtensionDBMixin(object):
+
+    def get_policy_target_segmentation_labels(self, session, policy_target_id):
+        rows = (session.query(ApicMappingExtensionDB).filter_by(
+                policy_target_id=policy_target_id).all())
+        return rows
+
+    def get_policy_target_segmentation_label(self, session, policy_target_id,
+                                             segmentation_label):
+        row = (session.query(ApicMappingExtensionDB).filter_by(
+            policy_target_id=policy_target_id,
+            segmentation_label=segmentation_label).one())
+        return row
+
+    def add_policy_target_segmentation_label(self, session, policy_target_id,
+                                             segmentation_label):
+        row = ApicMappingExtensionDB(policy_target_id=policy_target_id,
+                                     segmentation_label=segmentation_label)
+        session.add(row)
+
+    def delete_policy_target_segmentation_label(
+        self, session, policy_target_id, segmentation_label):
+        row = self.get_policy_target_segmentation_label(
+            session, policy_target_id, segmentation_label)
+        session.delete(row)
