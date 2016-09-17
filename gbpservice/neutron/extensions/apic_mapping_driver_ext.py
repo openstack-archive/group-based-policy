94a622d619ad29626a1668294195f1545aa5995a
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2016-09-17 09:53:05 -0700

    [apic-mapping] Segmentation label extension for PT
    This patch defines a new extension: apic_mapping_extension, for the
    apic_mapping policy driver. An extension attribute:
    segmentation_labels, that extends the Policy Target definition, is
    being introduced in this extension.
    
    A corresponding extension driver: apic_mapping_extension, that processes
    this extension, is also being added.
    
    The segementation_labels attribute is a list of strings. Each string can
    be upto 1024 characters long. These labels are not interpreted by GBP
    but are instead passed downstream by the apic_mapping driver. It is
    assumed that these are defined outside of OpenStack and the backend
    system can appropriately interpret them.
    
    The get_gbp_details() RPC call implemented by the apic_mapping driver
    will return the segmentation_labels in its body.
    
    A CLI option: --segmentation-labels will be provided for the
    policy_target create and update operations. This CLI option will accept
    a comma separated string as the option value.
    
    Change-Id: I360bf9f7f1d4bdca76d4f16b7535a6416f430830

---
+++ gbpservice/neutron/extensions/apic_mapping_driver_ext.py
@@ -0,0 +1,59 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron.api import extensions
+from neutron.api.v2 import attributes as attr
+
+from gbpservice.neutron.extensions import group_policy as gp
+
+
+APIC_MAPPING_PD_EXT = 'apic_mapping_pd_ext'
+
+EXTENDED_ATTRIBUTES_2_0 = {
+    gp.POLICY_TARGETS: {
+        'segmentation_labels': {
+            'allow_post': True, 'allow_put': True, 'default': None,
+            'validate': {'type:list_of_unique_strings': None},
+            'convert_to': attr.convert_none_to_empty_list,
+            'is_visible': True},
+    },
+}
+
+
+class Apic_mapping_driver_ext(extensions.ExtensionDescriptor):
+
+    @classmethod
+    def get_name(cls):
+        return "APIC Mapping Policy Driver Extension"
+
+    @classmethod
+    def get_alias(cls):
+        return APIC_MAPPING_PD_EXT
+
+    @classmethod
+    def get_description(cls):
+        return _("Add APIC mapping policy driver specific extensions.")
+
+    @classmethod
+    def get_namespace(cls):
+        return ("http://docs.openstack.org/ext/neutron/grouppolicy/"
+                "apic_mapping_pd/api/v1.0")
+
+    @classmethod
+    def get_updated(cls):
+        return "2016-08-03T10:00:00-00:00"
+
+    def get_extended_resources(self, version):
+        if version == "2.0":
+            return EXTENDED_ATTRIBUTES_2_0
+        else:
+            return {}
