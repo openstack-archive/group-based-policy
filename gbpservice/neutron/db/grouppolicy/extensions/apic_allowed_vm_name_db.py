0c7b755d41b3f26052ff4abc14b9bfd12c4ccad7
Kent Wu <wu@noironetworks.com>
2016-10-11 17:16:32 -0700

    [apic-mapping] Allowed VM Name extension for L3 Policy
    This patch defines a new extension: cisco_apic_gbp_allowed_vm_name,
    for the apic policy drivers. An extension attribute:
    allowed_vm_names, that extends the L3 Policy definition, is
    being introduced in this extension.
    
    A corresponding extension driver: apic_allowed_vm_name, that processes
    this extension, is also being added. This extension driver should be
    configured for this extension to be available. The driver name should be
    added to the existing list of extension drivers under:
    [group_policy]
    extension_drivers=<existing_ext_drivers>,apic_allowed_vm_name
    
    The allowed_vm_names attribute is a list of regexes. Each regex can
    be up to 255 characters long.
    
    While during the port-binding phase, we will also enforce the regex
    checking against the VM name from Nova. Only those VM names matching
    one of those regexes will be allowed.
    
    A CLI option: --allowed_vm_names will be provided for the
    L3 Policy create and update operations. This CLI option will accept
    a comma separated string as the option value.
    
    Change-Id: I4602919df9a0458eb255b93399c70f64dfeeb863

---
+++ gbpservice/neutron/db/grouppolicy/extensions/apic_allowed_vm_name_db.py
@@ -0,0 +1,50 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron.db import model_base
+import sqlalchemy as sa
+
+
+class ApicAllowedVMNameDB(model_base.BASEV2):
+    __tablename__ = 'gp_apic_mapping_allowed_vm_names'
+    l3_policy_id = sa.Column(sa.String(36),
+                             sa.ForeignKey('gp_l3_policies.id',
+                                           ondelete="CASCADE"),
+                             primary_key=True)
+    allowed_vm_name = sa.Column(sa.String(255), primary_key=True)
+
+
+class ApicAllowedVMNameDBMixin(object):
+
+    def get_l3_policy_allowed_vm_names(self, session, l3_policy_id):
+        rows = (session.query(ApicAllowedVMNameDB).filter_by(
+                l3_policy_id=l3_policy_id).all())
+        return rows
+
+    def get_l3_policy_allowed_vm_name(self, session, l3_policy_id,
+                                      allowed_vm_name):
+        row = (session.query(ApicAllowedVMNameDB).filter_by(
+            l3_policy_id=l3_policy_id,
+            allowed_vm_name=allowed_vm_name).one())
+        return row
+
+    def add_l3_policy_allowed_vm_name(self, session, l3_policy_id,
+                                      allowed_vm_name):
+        row = ApicAllowedVMNameDB(l3_policy_id=l3_policy_id,
+                                  allowed_vm_name=allowed_vm_name)
+        session.add(row)
+
+    def delete_l3_policy_allowed_vm_name(self, session, l3_policy_id,
+                                         allowed_vm_name):
+        row = self.get_l3_policy_allowed_vm_name(
+            session, l3_policy_id, allowed_vm_name)
+        session.delete(row)
