1cf96108fe4ad7ff14e2bd56042141afd6776af6
Kent Wu <wu@noironetworks.com>
2016-10-11 17:16:32 -0700

    [apic-mapping] Allowed VM Name extension for L3 Policy
    This patch defines a new extension: cisco_apic_gbp_allowed_vm_name,
    for the apic policy drivers. An extension attribute:
    allowed_vm_names, that extends the L3 Policy definition, is
    being introduced in this extension.
    
    A corresponding extension driver: apic_allowed_vm_name, that processes
    this extension, is also being added. This extension driver should be
    configured for this extension to be available. The driver name should be
    added to the existing list of extension drivers under:
    [group_policy]
    extension_drivers=<existing_ext_drivers>,apic_allowed_vm_name
    
    The allowed_vm_names attribute is a list of regexes. Each regex can
    be up to 255 characters long.
    
    While during the port-binding phase, we will also enforce the regex
    checking against the VM name from Nova. Only those VM names matching
    one of those regexes will be allowed.
    
    A CLI option: --allowed_vm_names will be provided for the
    L3 Policy create and update operations. This CLI option will accept
    a comma separated string as the option value.
    
    Change-Id: I4602919df9a0458eb255b93399c70f64dfeeb863
    (cherry picked from commit 29cd855015a0ada31673d6d33fd1715f0c9f3d3a)
    (cherry picked from commit 1b84edb5ec27379b54e63d586e4081b12c006a77)

---
+++ gbpservice/neutron/tests/unit/services/grouppolicy/test_apic_allowed_vm_name.py
@@ -0,0 +1,80 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron.db import api as db_api
+
+from gbpservice.neutron.db.grouppolicy.extensions import (
+    apic_allowed_vm_name_db as db)
+from gbpservice.neutron.tests.unit.services.grouppolicy import (
+    test_extension_driver_api as test_ext_base)
+
+
+class ExtensionDriverTestCase(test_ext_base.ExtensionDriverTestBase):
+    _extension_drivers = ['apic_allowed_vm_name']
+    _extension_path = None
+
+    def test_l3p_lifecycle(self):
+        l3p = self.create_l3_policy(name='myl3')['l3_policy']
+        l3p = self.show_l3_policy(
+            l3p['id'], expected_res_status=200)['l3_policy']
+        self.assertEqual([], l3p['allowed_vm_names'])
+        self.delete_l3_policy(l3p['id'], tenant_id=l3p['tenant_id'],
+                              expected_res_status=204)
+
+        allowed_vm_names = []
+        l3p = self.create_l3_policy(
+            name='myl3',
+            allowed_vm_names=allowed_vm_names)['l3_policy']
+        l3p = self.show_l3_policy(
+            l3p['id'], expected_res_status=200)['l3_policy']
+        self.assertItemsEqual([], l3p['allowed_vm_names'])
+        self.delete_l3_policy(l3p['id'], tenant_id=l3p['tenant_id'],
+                              expected_res_status=204)
+
+        allowed_vm_names = ['safe_vm*', '^secure_vm*']
+        l3p = self.create_l3_policy(
+            name='myl3',
+            allowed_vm_names=allowed_vm_names)['l3_policy']
+        l3p = self.show_l3_policy(
+            l3p['id'], expected_res_status=200)['l3_policy']
+        self.assertItemsEqual(allowed_vm_names, l3p['allowed_vm_names'])
+
+        allowed_vm_names = ['good_vm*', '^ok_vm*', 'safe_vm*']
+        l3p = self.update_l3_policy(
+            l3p['id'], allowed_vm_names=allowed_vm_names,
+            expected_res_status=200)['l3_policy']
+        l3p = self.show_l3_policy(
+            l3p['id'], expected_res_status=200)['l3_policy']
+        self.assertItemsEqual(allowed_vm_names, l3p['allowed_vm_names'])
+
+        allowed_vm_names = []
+        l3p = self.update_l3_policy(
+            l3p['id'], allowed_vm_names=allowed_vm_names,
+            expected_res_status=200)['l3_policy']
+        l3p = self.show_l3_policy(
+            l3p['id'], expected_res_status=200)['l3_policy']
+        self.assertItemsEqual(allowed_vm_names, l3p['allowed_vm_names'])
+
+        allowed_vm_names = ['^ok_vm*']
+        l3p = self.update_l3_policy(
+            l3p['id'], allowed_vm_names=allowed_vm_names,
+            expected_res_status=200)['l3_policy']
+        l3p = self.show_l3_policy(
+            l3p['id'], expected_res_status=200)['l3_policy']
+        self.assertItemsEqual(allowed_vm_names, l3p['allowed_vm_names'])
+
+        self.delete_l3_policy(l3p['id'], tenant_id=l3p['tenant_id'],
+                              expected_res_status=204)
+        session = db_api.get_session()
+        rows = (session.query(db.ApicAllowedVMNameDB).filter_by(
+                l3_policy_id=l3p['id']).all())
+        self.assertEqual([], rows)
