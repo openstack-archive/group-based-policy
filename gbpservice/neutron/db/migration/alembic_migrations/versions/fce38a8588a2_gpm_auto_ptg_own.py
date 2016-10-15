d0c52b03f8a7315d960e2f90411ad79d66f13ffd
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2016-10-14 22:08:42 -0700

    [aim-mapping] Auto PTG per L2P
    This change automatically creates a PTG per L2P. This PTG is created as a
    reverse map of the "default" EPG that is being created per L2P per Neutron
    network. We will henceforth refer to this PTG as "auto" PTG.
    
    In order to maintain the reverse-mapping integrity between the default EPG and
    the auto PTG, an entry is created in the apic name-mapping DB that maps the ID
    of the auto PTG to the "apic-name" of the default EPG.
    
    The initial name of the auto PTG is derived from the ID of the L2P to ease
    debugging and troubleshooting, and takes the form: "auto-ptg-<l2p_id>". This
    name is mutable (just like any other PTG). The aim_mapping driver does not
    have any specical meaning for this name, and does not care about after it
    implicitly sets it at the time of the auto PTG creation.
    
    The auto PTG cannot be deleted by the end user and doing so will result in
    an error.
    
    The shared status of the auto PTG is made consistent with the shared status
    of the L2P (once set, it cannot be changed).
    
    The auto PTG is deleted when the corresponding L2P is deleted (attempted in
    the pre-commit phase).
    
    To prevent forward mapping of the auto PTG to a new EPG, all above
    operations are invoked on the GBP DB mixin (parent of the GBP plugin). This
    ensures that the aim_mapping policy driver is not invoked for the create and
    delete auto PTG operations during L2P creation and deletion.
    
    The creation of the auto PTG is controlled by a configuration and is disabled
    by default thus allowing this new feature to be turned ON only where needed.
    All existing deployments should not see any change in behavior as long
    as they choose not to turn ON this feature. This configuration is as follows:
    
    [aim_mapping]
    create_auto_ptg=<True or False>
    
    Change-Id: I76977449419fed7984efb534ab88612c88cd3d54

---
+++ gbpservice/neutron/db/migration/alembic_migrations/versions/fce38a8588a2_gpm_auto_ptg_own.py
@@ -0,0 +1,42 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""Track implicit ownership of Auto PTGs (gpm_auto_ptg_own)
+
+Revision ID: fce38a8588a2
+Revises: a707faecf518
+Create Date: 2016-10-17 16:33:38.199797
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'fce38a8588a2'
+down_revision = 'a707faecf518'
+
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+
+    op.create_table(
+        'gpm_owned_policy_target_groups',
+        sa.Column('policy_target_group_id', sa.String(length=36),
+                  nullable=False),
+        sa.ForeignKeyConstraint(['policy_target_group_id'],
+                                ['gp_policy_target_groups.id'],
+                                name='ipd_auto_ptg_owned_fk',
+                                ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('policy_target_group_id')
+    )
