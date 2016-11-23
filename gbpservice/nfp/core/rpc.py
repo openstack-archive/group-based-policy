246bc72190eb90fb663a73c4a6c910bc433db0ef
YogeshRajmane <yogesh.rajmane@oneconvergence.com>
2016-11-23 21:58:40 +0530

    NFP - Enhancement to support module priority (WIP)
    Enhancement of NFP process model to support registering same
    event or RPCs by multiple modules and allocate event or RPC
    calls by selecting module from list of modules on basis of
    priority
    
    Change-Id: I148add0837505ed38a72942a1e0f33a1374853ed
    Closes-Bug: 1644283

--- gbpservice/nfp/core/rpc.py
+++ gbpservice/nfp/core/rpc.py
@@ -10,8 +10,13 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import operator
+import oslo_messaging
+
 from oslo_config import cfg as oslo_config
 
+from oslo_messaging.rpc import dispatcher as om_rpc_dispatcher
+from oslo_messaging import server as msg_server
 from oslo_service import loopingcall as oslo_looping_call
 from oslo_service import periodic_task as oslo_periodic_task
 
@@ -26,6 +31,20 @@ LOG = nfp_logging.getLogger(__name__)
 
 n_rpc.init(oslo_config.CONF)
 
+
+def get_rpc_server(transport, target, endpoints,
+                   executor='blocking', serializer=None):
+    dispatcher = RPCDispatcher(target, endpoints, serializer)
+    return msg_server.MessageHandlingServer(transport, dispatcher, executor)
+
+# monkey patching
+# get_rpc_server() function of oslo_messaging creates rpc server
+# with its own dispatcher but we want to create rpc server with our
+# modified dispatcher,hence there is need to override
+# get_rpc_server() function
+oslo_messaging.get_rpc_server = get_rpc_server
+
+
 """Wrapper class for Neutron RpcAgent definition.
 
     NFP modules will use this class for the agent definition.
@@ -38,11 +57,16 @@ class RpcAgent(n_rpc.Service):
 
     def __init__(
             self, sc, host=None,
-            topic=None, manager=None, report_state=None):
+            topic=None, manager=None, report_state=None, priority=0):
         # report_state =
         #   {<agent_state_keys>, 'plugin_topic': '', 'report_interval': ''}
-        super(RpcAgent, self).__init__(host=host, topic=topic, manager=manager)
-
+        super(RpcAgent, self).__init__(
+            host=host, topic=topic, manager=manager)
+        self.sc = sc
+        self.host = host
+        self.topic = topic
+        self.manager = manager
+        self.priority = priority
         # Check if the agent needs to report state
         if report_state:
             self._report_state = ReportState(report_state)
@@ -116,3 +140,75 @@ class ReportStateTask(oslo_periodic_task.PeriodicTasks):
     def report_state(self, context):
         # trigger the state reporting
         self._controller.report_state()
+
+
+"""RPC method dispatcher
+
+   This class handles the dispatch of a method when rpc server
+   receives message.
+"""
+
+
+class RPCDispatcher(om_rpc_dispatcher.RPCDispatcher):
+
+    def __init__(self, target, endpoints, serializer):
+        super(RPCDispatcher, self).__init__(target, endpoints, serializer)
+
+    def _do_dispatch(self, endpoint, method, ctxt, args):
+        rpc_callback = getattr(endpoint, '_rpc_callback')
+        endpoint = rpc_callback(method)
+        if not endpoint:
+            raise om_rpc_dispatcher.NoSuchMethod(method)
+        return super(RPCDispatcher, self)._do_dispatch(
+            endpoint, method, ctxt, args)
+
+    def _dispatch(self, ctxt, message):
+        """Dispatch an RPC message to the appropriate endpoint method.
+
+        :param ctxt: the request context
+        :type ctxt: dict
+        :param message: the message payload
+        :type message: dict
+        """
+        rpc_manager = self.endpoints[0]
+        rpc_manager.target.version = message.get('version', '1.0')
+        method = message.get('method')
+        setattr(rpc_manager, method, '')
+        result = super(RPCDispatcher, self)._dispatch(ctxt, message)
+        return result
+
+"""Core's RPC Manager
+
+"""
+
+
+class RpcManager(object):
+    # value of version of the target instance will be updated with
+    # rpc client's version in dispatcher when client make rpc call/cast
+    target = oslo_messaging.Target(version='1.0')
+
+    def __init__(self, sc, conf, agents):
+        self.sc = sc
+        self.conf = conf
+        self.agents = agents
+
+    def _rpc_callback(self, method):
+        """Determine the rpc manager of which, method to be dispatched
+
+        :param method: rpc method name to be dispatched
+        :return manager: rpc manager having the highest priority, method
+         and the same version
+        """
+        agents = sorted(
+            self.agents, key=operator.attrgetter('priority'), reverse=True)
+        # client rpc version
+        version = self.target.version
+        for agent in agents:
+            if hasattr(agent.manager, 'target'):
+                target = agent.manager.target
+            else:
+                target = oslo_messaging.Target()
+            if (hasattr(agent.manager, method) and
+                om_rpc_dispatcher.RPCDispatcher._is_compatible(
+                    target, version)):
+                return agent.manager
