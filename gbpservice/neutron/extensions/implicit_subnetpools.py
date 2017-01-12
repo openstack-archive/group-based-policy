e90f20be700a9199be209d6ecdcd29813a338ab6
Ivar Lazzaro <ivarlazzaro@gmail.com>
2017-01-12 00:09:19 -0800

    Introduce last resort subnetpool extension
    In order to preserve legacy scripts that create Neutron resources such
    as Networks or Subnets, we need a way to specify default address pools
    that can be assigned to subnets upon creation.
    
    Something similar already exists in Neutron: an attribute "is_default"
    that can be set on a subnetpool so that it can be used by subnets that
    don't specify a subnetpool themselves. This however has a bunch of
    limitations such as:
    
    - For reasons mentioned by Armando in [0], the intent of using the
      default subnetpool has to be explicit at subnet creation;
    - When a subnetpool doesn't exist for a Subnet requesting the default
      one, an exception is raised instead of leaving the subnet without
      any subnetpool;
    - Only one default subnetpool can exist in the whole system regardless
      of its visibility (shared, non shared).
    
    To accomodate our use cases, we are introducing a new extension for
    the subnetpool resource called "last_resort_subnetpool" and a new
    attribute for the subnetpool API "is_last_resort". When a subnetpool is
    marked as last resort the following semantics apply:
    
    - Only one last resort subnetpool can exist *per tenant* (if not shared)
    - Only one shared last resort subnetpool can exist in the whole system.
      This doesn't affect private last resort subnetpools
    - Whenever a subnet is created, the current subnetpool retrival
      mechanism is executed. If that returns no result, a last resort
      subnetpool will be searched in the Subnet tenant. If that fails too,
      we fall back to a global last resort subnetpool. In case all of this
      fails, the subnet will be created with no subnetpool as a legacy
      resource.
    Change-Id: Iead6ad458cdcd8ca7c79f5df553336d6147e4c62

---
+++ gbpservice/neutron/extensions/implicit_subnetpools.py
@@ -0,0 +1,55 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron.api import extensions
+from neutron.api.v2 import attributes
+from neutron.common import constants
+
+
+EXTENDED_ATTRIBUTES_2_0 = {
+    attributes.SUBNETPOOLS: {
+        'is_implicit': {'allow_post': True, 'allow_put': True,
+                        'default': False,
+                        'convert_to': attributes.convert_to_boolean,
+                        'is_visible': True, 'required_by_policy': True,
+                        'enforce_policy': True},
+    },
+}
+
+
+class Implicit_subnetpools(extensions.ExtensionDescriptor):
+    """Extension class supporting default subnetpools."""
+
+    @classmethod
+    def get_name(cls):
+        return "Implicit Subnetpools"
+
+    @classmethod
+    def get_alias(cls):
+        return "implicit-subnetpools"
+
+    @classmethod
+    def get_description(cls):
+        return "Provides ability to mark a subnetpool as implicit"
+
+    @classmethod
+    def get_updated(cls):
+        return "2017-01-11T18:00:00-00:00"
+
+    def get_required_extensions(self):
+        return [constants.SUBNET_ALLOCATION_EXT_ALIAS]
+
+    def get_extended_resources(self, version):
+        if version == "2.0":
+            return EXTENDED_ATTRIBUTES_2_0
+        else:
+            return {}
