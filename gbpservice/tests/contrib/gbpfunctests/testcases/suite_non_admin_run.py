34ac0178b563cf623dc1b243b03919ffeb51bff7
Anusree <anusree.a04@gmail.com>
2017-01-20 03:40:59 +0530

    Remove white space between print ()
    There was a white space between print and ()
    
    Change-Id: Id3db3d3d08e03dc722ee7de31880c062727a25c6

--- gbpservice/tests/contrib/gbpfunctests/testcases/suite_non_admin_run.py
+++ gbpservice/tests/contrib/gbpfunctests/testcases/suite_non_admin_run.py
@@ -43,29 +43,29 @@ def main():
     try:
         flag = sys.argv[1]
     except Exception:
-        print ('%s' % (usage))
+        print('%s' % (usage))
         sys.exit(1)
     fname = run_func_neg()
     num_lines = sum(1 for line in open(fname))
-    print ("\nNumber of Functional Test Scripts to execute = %s" % (num_lines))
+    print("\nNumber of Functional Test Scripts to execute = %s" % (num_lines))
     with open(fname) as f:
         for i, l in enumerate(f, 1):
-            print ("Functional Test Script to execute now == %s" % l)
+            print("Functional Test Script to execute now == %s" % l)
             # Assumption: test-scripts are executable from any location
             # Reading the line from text file, also reads trailing \n, hence we
             # need to strip
             cmd = 'python %s %s' % (l.strip(), flag)
-            print (cmd)
+            print(cmd)
             # out=getoutput(cmd)
             subprocess.call(cmd, shell=True)
     f = open("test_results.txt")
     contents = f.read()
     f.close()
-    print (contents)
-    print ("\n\nTotal Number of TestCases Executed= %s" % (
+    print(contents)
+    print("\n\nTotal Number of TestCases Executed= %s" % (
         contents.count("TESTCASE_GBP_")))
-    print ("\n\nNumber of TestCases Passed= %s" % (contents.count("PASSED")))
-    print ("\n\nNumber of TestCases Failed= %s" % (contents.count("FAILED")))
+    print("\n\nNumber of TestCases Passed= %s" % (contents.count("PASSED")))
+    print("\n\nNumber of TestCases Failed= %s" % (contents.count("FAILED")))
     if contents.count("FAILED") > 0:
         sys.exit(1)
     else:
