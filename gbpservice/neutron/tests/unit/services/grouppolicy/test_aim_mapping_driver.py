openstack-archive/group-based-policy

commit bcadb5a54f0680f4c94462dbb8d89c811cdc61f5

[AIM] Send port notifications on router changes
When a router that has external connectivity is
connected/disconnected from a subnet, the ports
on that subnet may gain/lose external connecitivity
via SNAT. To reflect this on the data path, port
update notifications need to be sent out so
that the opflex agent may refetch information
about the port.
The same applies for external-gateway changes
of a router, except that the set of ports affected
is all the ports in all the subnets that are
connected to the router.

All these notifications are not sent immediately
but delayed until the current database transaction
completes.

Change-Id: I346159ed4217f6ff3442d5c3a75e54ea7841ee29
Signed-off-by: Amit Bose <amitbose@gmail.com>

gbpservice/network/neutronv2/local_api.py
@@ -34,13 +34,13 @@ from gbpservice.neutron.services.grouppolicy.common import exceptions as exc
 LOG = logging.getLogger(__name__)
 
 
-def _get_outer_transaction(transaction):
+def get_outer_transaction(transaction):
     if not transaction:
         return
     if not transaction._parent:
         return transaction
     else:
-        return _get_outer_transaction(transaction._parent)
+        return get_outer_transaction(transaction._parent)
 
 
 BATCH_NOTIFICATIONS = False
@@ -155,7 +155,7 @@ class LocalAPI(object):
     def _process_notifications(self, context, action, resource, obj,
                                orig_obj=None):
         if BATCH_NOTIFICATIONS:
-            outer_transaction = (_get_outer_transaction(
+            outer_transaction = (get_outer_transaction(
                 context._session.transaction))
         else:
             outer_transaction = None

gbpservice/neutron/extensions/patch.py
@@ -216,7 +216,7 @@ def get_session(autocommit=True, expire_on_commit=False, use_slave=False):
         global LOCAL_API_NOTIFICATION_QUEUE
         if transaction and not transaction._parent and (
             not transaction.is_active and not transaction.nested):
-            if transaction in LOCAL_API_NOTIFICATION_QUEUE:
+            if transaction in (LOCAL_API_NOTIFICATION_QUEUE or []):
                 # push the queued notifications only when the
                 # outermost transaction completes
                 PUSH_NOTIFICATIONS_METHOD(transaction)

gbpservice/neutron/plugins/ml2plus/drivers/apic_aim/mechanism_driver.py
@@ -49,6 +49,7 @@ import oslo_messaging
 
 from apic_ml2.neutron.plugins.ml2.drivers.cisco.apic import (rpc as
     apic_topo_rpc)
+from gbpservice.network.neutronv2 import local_api
 from gbpservice.neutron.extensions import cisco_apic
 from gbpservice.neutron.extensions import cisco_apic_l3 as a_l3
 from gbpservice.neutron.plugins.ml2plus import driver_api as api_plus
@@ -59,7 +60,7 @@ from gbpservice.neutron.plugins.ml2plus.drivers.apic_aim import extension_db
 
 LOG = log.getLogger(__name__)
 DEVICE_OWNER_SNAT_PORT = 'apic:snat-pool'
-
+local_api.BATCH_NOTIFICATIONS = True
 
 # REVISIT(rkukura): Consider making these APIC name constants
 # configurable, although changing them would break an existing
@@ -680,12 +681,26 @@ class ApicMechanismDriver(api_plus.MechanismDriver):
 
             if old_net == new_net:
                 old_net = None
+                affected_port_ids = []
+            else:
+                # SNAT information of ports on the subnet that interface
+                # with the router will change because router's gateway
+                # changed.
+                sub_ids = self._get_router_interface_subnets(session,
+                                                             current['id'])
+                affected_port_ids = self._get_non_router_ports_in_subnets(
+                    session, sub_ids)
+
             old_net = self.plugin.get_network(context,
                                               old_net) if old_net else None
             new_net = self.plugin.get_network(context,
                                               new_net) if new_net else None
             self._manage_external_connectivity(context,
                                                current, old_net, new_net)
+
+            # Send a port update so that SNAT info may be recalculated for
+            # affected ports in the interfaced subnets.
+            self._notify_port_update_bulk(context, affected_port_ids)
 
         # REVISIT(rkukura): Update extension attributes?
 
@@ -911,13 +926,22 @@ class ApicMechanismDriver(api_plus.MechanismDriver):
             epg = self.aim.update(aim_ctx, epg,
                                   provided_contract_names=contracts)
 
-        # If this is first interface-port, then that will determine
-        # the VRF for this router. Setup exteral-connectivity for VRF
-        # if external-gateway is set.
-        if (router.gw_port_id and not router_intf_count):
-            net = self.plugin.get_network(context,
-                                          router.gw_port.network_id)
-            self._manage_external_connectivity(context, router, None, net, vrf)
+        if router.gw_port_id:
+            # If this is first interface-port, then that will determine
+            # the VRF for this router. Setup exteral-connectivity for VRF
+            # if external-gateway is set.
+            if not router_intf_count:
+                net = self.plugin.get_network(context,
+                                              router.gw_port.network_id)
+                self._manage_external_connectivity(context, router, None, net,
+                                                   vrf)
+            # SNAT information of ports on the subnet will change because
+            # of router interface addition. Send a port update so that it may
+            # be recalculated.
+            port_ids = self._get_non_router_ports_in_subnets(
+                session,
+                [subnet['id'] for subnet in subnets])
+            self._notify_port_update_bulk(context, port_ids)
 
     def remove_router_interface(self, context, router_id, port_db, subnets):
         LOG.debug("APIC AIM MD removing subnets %(subnets)s from router "
@@ -977,18 +1001,25 @@ class ApicMechanismDriver(api_plus.MechanismDriver):
         if not router_ids:
             self._dissassociate_network_from_vrf(aim_ctx, network_db, vrf)
 
-        # If this was the last interface-port, then we no longer know
-        # the VRF for this router. So update external-conectivity to
-        # exclude this router.
-        if (router_db.gw_port_id and
-            not self._get_router_intf_count(session, router_db)):
-            net = self.plugin.get_network(context,
-                                          router_db.gw_port.network_id)
-            self._manage_external_connectivity(
-                context, router_db, net, None, vrf)
+        if router_db.gw_port_id:
+            # If this was the last interface-port, then we no longer know
+            # the VRF for this router. So update external-conectivity to
+            # exclude this router.
+            if not self._get_router_intf_count(session, router_db):
+                net = self.plugin.get_network(context,
+                                              router_db.gw_port.network_id)
+                self._manage_external_connectivity(
+                    context, router_db, net, None, vrf)
 
-            self._delete_snat_ip_ports_if_reqd(context, net['id'],
-                                               router_id)
+                self._delete_snat_ip_ports_if_reqd(context, net['id'],
+                                                   router_id)
+            # SNAT information of ports on the subnet will change because
+            # of router interface removal. Send a port update so that it may
+            # be recalculated.
+            port_ids = self._get_non_router_ports_in_subnets(
+                session,
+                [subnet['id'] for subnet in subnets])
+            self._notify_port_update_bulk(context, port_ids)
 
     def bind_port(self, context):
         current = context.current
@@ -1777,8 +1808,16 @@ class ApicMechanismDriver(api_plus.MechanismDriver):
     def _notify_port_update(self, plugin_context, port_id):
         port = self.plugin.get_port(plugin_context, port_id)
         if self._is_port_bound(port):
-            LOG.debug("APIC notify port %s", port['id'])
-            self.notifier.port_update(plugin_context, port)
+            LOG.debug("Enqueing notify for port %s", port['id'])
+            txn = local_api.get_outer_transaction(
+                plugin_context.session.transaction)
+            local_api.send_or_queue_notification(txn, self.notifier,
+                                                 'port_update',
+                                                 [plugin_context, port])
+
+    def _notify_port_update_bulk(self, plugin_context, port_ids):
+        for p_id in port_ids:
+            self._notify_port_update(plugin_context, p_id)
 
     def get_or_allocate_snat_ip(self, plugin_context, host_or_vrf,
                                 ext_network):
@@ -2020,3 +2059,23 @@ class ApicMechanismDriver(api_plus.MechanismDriver):
                 result.append((net, db._make_segment_dict(seg)))
                 net_ids.add(seg.network_id)
         return result
+
+    def _get_router_interface_subnets(self, session, router_id):
+        subnet_ids = (session.query(models_v2.IPAllocation.subnet_id)
+                      .join(l3_db.RouterPort,
+                            models_v2.IPAllocation.port_id ==
+                            l3_db.RouterPort.port_id)
+                      .filter(l3_db.RouterPort.router_id == router_id)
+                      .distinct())
+        return [s[0] for s in subnet_ids]
+
+    def _get_non_router_ports_in_subnets(self, session, subnet_ids):
+        if not subnet_ids:
+            return []
+        port_ids = (session.query(models_v2.IPAllocation.port_id)
+                    .join(models_v2.Port)
+                    .filter(models_v2.IPAllocation.subnet_id.in_(subnet_ids))
+                    .filter(models_v2.Port.device_owner !=
+                            n_constants.DEVICE_OWNER_ROUTER_INTF)
+                    .all())
+        return [p[0] for p in port_ids]

commit 12f953e5d939e6817b3a3c2fb5fc88ecf6fd24f9

Making notification-queue init thread-safe
Create notification queue per session and associate with
the session object. This helps us to get away from having
to create a global notification queue.

Also protecting notification queue initialization in patched
get_session code by adding a thread lock.

This at least helps to address the issue seen in commit:
https://github.com/openstack/group-based-policy/commit/bcadb5a54f0680f4c94462dbb8d89c811cdc61f5

which caused certain tests to fail. An additional check was
added in that commit to see if the LOCAL_API_NOTIFICATION_QUEUE
variable was uninitalized but this should have never been the case.
This led to the suspicion of unsafe thread behavior.

Change-Id: Iddd9c7b9f8975d37ccd00d28776b982b90f51324

gbpservice/network/neutronv2/local_api.py
@@ -44,23 +44,23 @@ def get_outer_transaction(transaction):
 
 
 BATCH_NOTIFICATIONS = False
-NOTIFICATION_QUEUE = {}
 NOTIFIER_REF = 'notifier_object_reference'
 NOTIFIER_METHOD = 'notifier_method_name'
 NOTIFICATION_ARGS = 'notification_args'
 
 
-def _queue_notification(transaction_key, notifier_obj, notifier_method, args):
+def _queue_notification(session, transaction_key, notifier_obj,
+                        notifier_method, args):
     entry = {NOTIFIER_REF: notifier_obj, NOTIFIER_METHOD: notifier_method,
              NOTIFICATION_ARGS: args}
-    if transaction_key not in NOTIFICATION_QUEUE:
-        NOTIFICATION_QUEUE[transaction_key] = [entry]
+    if transaction_key not in session.notification_queue:
+        session.notification_queue[transaction_key] = [entry]
     else:
-        NOTIFICATION_QUEUE[transaction_key].append(entry)
+        session.notification_queue[transaction_key].append(entry)
 
 
-def send_or_queue_notification(transaction_key, notifier_obj, notifier_method,
-                               args):
+def send_or_queue_notification(session, transaction_key, notifier_obj,
+                               notifier_method, args):
     if not transaction_key or 'subnet.delete.end' in args:
         # We make an exception for the dhcp agent notification
         # for subnet delete since the implementation for sending
@@ -71,19 +71,20 @@ def send_or_queue_notification(transaction_key, notifier_obj, notifier_method,
         getattr(notifier_obj, notifier_method)(*args)
         return
 
-    _queue_notification(transaction_key, notifier_obj, notifier_method, args)
+    _queue_notification(session, transaction_key, notifier_obj,
+                        notifier_method, args)
 
 
-def post_notifications_from_queue(transaction_key):
-    queue = NOTIFICATION_QUEUE[transaction_key]
+def post_notifications_from_queue(session, transaction_key):
+    queue = session.notification_queue[transaction_key]
     for entry in queue:
         getattr(entry[NOTIFIER_REF],
                 entry[NOTIFIER_METHOD])(*entry[NOTIFICATION_ARGS])
-    del NOTIFICATION_QUEUE[transaction_key]
+    del session.notification_queue[transaction_key]
 
 
-def discard_notifications_after_rollback(transaction_key):
-    NOTIFICATION_QUEUE.pop(transaction_key, None)
+def discard_notifications_after_rollback(session):
+    session.notification_queue.pop(session.transaction, None)
 
 
 class dummy_context_mgr(object):
@@ -163,14 +164,14 @@ class LocalAPI(object):
             args = [action, orig_obj, {resource: obj}]
         else:
             args = [action, {}, {resource: obj}]
-        send_or_queue_notification(
+        send_or_queue_notification(context._session,
             outer_transaction, self._nova_notifier,
             'send_network_change', args)
         # REVISIT(rkukura): Do create.end notification?
         if cfg.CONF.dhcp_agent_notification:
             action_state = ".%s.end" % action.split('_')[0]
             args = [context, {resource: obj}, resource + action_state]
-            send_or_queue_notification(
+            send_or_queue_notification(context._session,
                 outer_transaction, self._dhcp_agent_notifier,
                 'notify', args)
 

gbpservice/neutron/extensions/patch.py
@@ -22,6 +22,7 @@ from neutron.db import securitygroups_db
 from neutron.plugins.ml2 import db as ml2_db
 from oslo_log import log
 from sqlalchemy import event
+import threading
 
 LOG = log.getLogger(__name__)
 
@@ -184,46 +185,48 @@ def get_port_from_device_mac(context, device_mac):
 
 ml2_db.get_port_from_device_mac = get_port_from_device_mac
 
-LOCAL_API_NOTIFICATION_QUEUE = None
 PUSH_NOTIFICATIONS_METHOD = None
 DISCARD_NOTIFICATIONS_METHOD = None
+LOCAL_API_NOTIFICATION_QUEUE_LOCK = threading.RLock()
 
 
 def get_session(autocommit=True, expire_on_commit=False, use_slave=False):
     # The folowing are declared as global so that they can
     # used in the inner functions that follow.
-    global LOCAL_API_NOTIFICATION_QUEUE
     global PUSH_NOTIFICATIONS_METHOD
     global DISCARD_NOTIFICATIONS_METHOD
+    global LOCAL_API_NOTIFICATION_QUEUE_LOCK
     # This conditional logic is to ensure that local_api
     # is imported only once.
-    if 'local_api' not in locals():
+    LOCAL_API_NOTIFICATION_QUEUE_LOCK.acquire()
+    try:
         from gbpservice.network.neutronv2 import local_api
-        LOCAL_API_NOTIFICATION_QUEUE = local_api.NOTIFICATION_QUEUE
         PUSH_NOTIFICATIONS_METHOD = (
             local_api.post_notifications_from_queue)
         DISCARD_NOTIFICATIONS_METHOD = (
             local_api.discard_notifications_after_rollback)
-    # The following two lines are copied from the original
-    # implementation of db_api.get_session() and should be updated
-    # if the original implementation changes.
-    facade = db_api._create_facade_lazily()
-    new_session = facade.get_session(autocommit=autocommit,
-                                     expire_on_commit=expire_on_commit,
-                                     use_slave=use_slave)
+        # The following two lines are copied from the original
+        # implementation of db_api.get_session() and should be updated
+        # if the original implementation changes.
+        facade = db_api._create_facade_lazily()
+        new_session = facade.get_session(autocommit=autocommit,
+                                         expire_on_commit=expire_on_commit,
+                                         use_slave=use_slave)
+        new_session.notification_queue = {}
+    finally:
+        LOCAL_API_NOTIFICATION_QUEUE_LOCK.release()
 
     def gbp_after_transaction(session, transaction):
-        global LOCAL_API_NOTIFICATION_QUEUE
         if transaction and not transaction._parent and (
             not transaction.is_active and not transaction.nested):
-            if transaction in (LOCAL_API_NOTIFICATION_QUEUE or []):
+            if transaction in session.notification_queue:
                 # push the queued notifications only when the
                 # outermost transaction completes
-                PUSH_NOTIFICATIONS_METHOD(transaction)
+                PUSH_NOTIFICATIONS_METHOD(session, transaction)
 
     def gbp_after_rollback(session):
         # We discard all queued notifiactions if the transaction fails.
-        DISCARD_NOTIFICATIONS_METHOD(session.transaction)
+        DISCARD_NOTIFICATIONS_METHOD(session)
 
     if local_api.BATCH_NOTIFICATIONS:
         event.listen(new_session, "after_transaction_end",

gbpservice/neutron/plugins/ml2plus/drivers/apic_aim/mechanism_driver.py
@@ -1984,7 +1984,8 @@ class ApicMechanismDriver(api_plus.MechanismDriver):
             LOG.debug("Enqueing notify for port %s", port['id'])
             txn = local_api.get_outer_transaction(
                 plugin_context.session.transaction)
-            local_api.send_or_queue_notification(txn, self.notifier,
+            local_api.send_or_queue_notification(plugin_context.session,
+                                                 txn, self.notifier,
                                                  'port_update',
                                                  [plugin_context, port])
 

gbpservice/neutron/tests/unit/services/grouppolicy/test_aim_mapping_driver.py
@@ -2710,6 +2710,7 @@ class NotificationTest(AIMBaseTestCase):
         self.mac_prefix = '12:34:56:78:5d:'
         self.queue_notification_call_count = 0
         self.max_notification_queue_length = 0
+        self.notification_queue = None
         self.post_notifications_from_queue_call_count = 0
         self.orig_generate_uuid = uuidutils.generate_uuid
         self.orig_is_uuid_like = uuidutils.is_uuid_like
@@ -2740,33 +2741,57 @@ class NotificationTest(AIMBaseTestCase):
             sc_plugin=sc_plugin, **kwargs)
         self.orig_generate_mac = self._plugin._generate_mac
         self._plugin._generate_mac = _generate_mac
+
         self.orig_queue_notification = local_api._queue_notification
 
-        # The two functions are patched below to instrument how
+        # The functions are patched below to instrument how
         # many times the functions are called and also to track
         # the queue length.
-        def _queue_notification(
+        def _queue_notification(session,
             transaction_key, notifier_obj, notifier_method, args):
             self.queue_notification_call_count += 1
-            self.orig_queue_notification(
+            self.orig_queue_notification(session,
                 transaction_key, notifier_obj, notifier_method, args)
-            if local_api.NOTIFICATION_QUEUE:
-                key = local_api.NOTIFICATION_QUEUE.keys()[0]
-                length = len(local_api.NOTIFICATION_QUEUE[key])
+            if session.notification_queue:
+                key = session.notification_queue.keys()[0]
+                length = len(session.notification_queue[key])
                 if length > self.max_notification_queue_length:
                     self.max_notification_queue_length = length
+            self.notification_queue = session.notification_queue
 
         local_api._queue_notification = _queue_notification
+
+        self.orig_send_or_queue_notification = (
+            local_api.send_or_queue_notification)
+
+        def send_or_queue_notification(
+            session, transaction_key, notifier_obj, notifier_method, args):
+            self.orig_send_or_queue_notification(session,
+                transaction_key, notifier_obj, notifier_method, args)
+            self.notification_queue = session.notification_queue
+
+        local_api.send_or_queue_notification = send_or_queue_notification
 
         self.orig_post_notifications_from_queue = (
             local_api.post_notifications_from_queue)
 
-        def post_notifications_from_queue(transaction_key):
+        def post_notifications_from_queue(session, transaction_key):
             self.post_notifications_from_queue_call_count += 1
-            self.orig_post_notifications_from_queue(transaction_key)
+            self.orig_post_notifications_from_queue(session, transaction_key)
+            self.notification_queue = session.notification_queue
 
         local_api.post_notifications_from_queue = (
             post_notifications_from_queue)
+
+        self.orig_discard_notifications_after_rollback = (
+            local_api.discard_notifications_after_rollback)
+
+        def discard_notifications_after_rollback(session):
+            self.orig_discard_notifications_after_rollback(session)
+            self.notification_queue = session.notification_queue
+
+        local_api.discard_notifications_after_rollback = (
+            discard_notifications_after_rollback)
 
     def tearDown(self):
         super(NotificationTest, self).tearDown()
@@ -2775,8 +2800,12 @@ class NotificationTest(AIMBaseTestCase):
         uuidutils.is_uuid_like = self.orig_is_uuid_like
         local_api.BATCH_NOTIFICATIONS = False
         local_api._queue_notification = self.orig_queue_notification
+        local_api.send_or_queue_notification = (
+            self.orig_send_or_queue_notification)
         local_api.post_notifications_from_queue = (
             self.orig_post_notifications_from_queue)
+        local_api.discard_notifications_after_rollback = (
+            self.orig_discard_notifications_after_rollback)
 
     def _expected_dhcp_agent_call_list(self):
         # This testing strategy assumes the sequence of notifications
@@ -2894,7 +2923,7 @@ class NotificationTest(AIMBaseTestCase):
                     'security-groups', sg_id).get_response(self.ext_api)
             notifier.assert_has_calls(expected_calls(), any_order=False)
             # test that no notifications have been left out
-            self.assertEqual({}, local_api.NOTIFICATION_QUEUE)
+            self.assertEqual({}, self.notification_queue)
 
     def _disable_checks(self, no_batch_event, with_batch_event):
         # this is a temporarty workaround to avoid having to repeatedly
@@ -2992,21 +3021,12 @@ class NotificationTest(AIMBaseTestCase):
                 self.assertEqual([], dhcp_notifier.call_args_list)
                 self.assertEqual([], nova_notifier.call_args_list)
                 # test that notification queue has been flushed
-                self.assertEqual({}, local_api.NOTIFICATION_QUEUE)
+                self.assertEqual({}, self.notification_queue)
                 # test that the push notifications func itself was not called
                 self.assertEqual(
                     0, self.post_notifications_from_queue_call_count)
         # restore mock
         self.dummy.create_policy_target_group_precommit = orig_func
-
-    def test_notifier_queue_populated(self):
-        local_api.BATCH_NOTIFICATIONS = True
-        with mock.patch.object(local_api, 'post_notifications_from_queue'):
-            self.create_policy_target_group(name="ptg1")
-            self.assertEqual(1, len(local_api.NOTIFICATION_QUEUE))
-            key = local_api.NOTIFICATION_QUEUE.keys()[0]
-            self.assertLess(0, len(local_api.NOTIFICATION_QUEUE[key]))
-        local_api.NOTIFICATION_QUEUE = {}
 
 
 class TestExternalSegment(AIMBaseTestCase):