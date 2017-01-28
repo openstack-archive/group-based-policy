7461b2c9d7f7635b678157628730b28c43d744bf
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2017-01-27 16:24:44 -0800

    [WIP] Newton sync
    Change-Id: I887ee6cfca8199710cf5c653b5f57dff86bb035a

--- gbpservice/neutron/plugins/ml2plus/patch_neutron.py
+++ gbpservice/neutron/plugins/ml2plus/patch_neutron.py
@@ -13,8 +13,22 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from neutron.api import extensions
+from oslo_db.sqlalchemy import enginefacade
 
+
+@property
+def noop_writer(self):
+    """Override TransactionContextManager cloning"""
+    return self
+
+
+enginefacade._TransactionContextManager.writer = noop_writer
+
+
+from neutron.api import extensions
+from neutron.callbacks import registry
+
+from gbpservice.network.neutronv2 import local_api
 from gbpservice.neutron.plugins.ml2plus import extension_overrides
 
 # Monkeypatch Neutron to allow overriding its own extension
@@ -29,4 +43,45 @@ def get_extensions_path(service_plugins=None):
     path = _real_get_extensions_path(service_plugins)
     return extension_overrides.__path__[0] + ':' + path
 
+
 extensions.get_extensions_path = get_extensions_path
+
+
+import sys
+
+from neutron.common import exceptions as nexcp
+from neutron import context as n_context
+
+
+class NoSession(nexcp.BadRequest):
+    message = _("No DB session in scope ")
+
+
+def get_current_session():
+    i = 1
+    not_found = True
+    try:
+        while not_found:
+            for val in sys._getframe(i).f_locals.itervalues():
+                if isinstance(val, n_context.Context):
+                    ctx = val
+                    not_found = False
+                    break
+            i = i + 1
+        return ctx.session
+    except Exception:
+        raise NoSession()
+
+
+def notify(resource, event, trigger, **kwargs):
+    if 'context' in kwargs:
+        session = kwargs['context'].session
+    else:
+        session = get_current_session()
+
+    txn = local_api.get_outer_transaction(session.transaction)
+    local_api.send_or_queue_registry_notification(
+        session, txn, resource, event, trigger, **kwargs)
+
+
+registry.notify = notify
