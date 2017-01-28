0b1ee12aaf667558381929ca7780f689dd2d424c
Igor Duarte Cardoso <igor.duarte.cardoso@intel.com>
2017-01-28 00:05:17 +0000

    Quality of Service support via NSPs
    QoS support for PTs inside PTG, as 2 new NSP param types.
    
    The new NSP param types, qos_maxrate and qos_burstrate, map to the
    Neutron QoS Policy resource, which then gets associated to a QoS
    bandwith limit rule with a certain maximum rate and/or burst rate (in
    Kbps and Kb respectively) set.
    
    Change-Id: I4a15daf5e0edd76d2d436eac6fdfb6b9f64992b2

--- gbpservice/neutron/tests/unit/db/grouppolicy/test_group_policy_db.py
+++ gbpservice/neutron/tests/unit/db/grouppolicy/test_group_policy_db.py
@@ -17,6 +17,7 @@ import webob.exc
 
 import mock
 from neutron.api import extensions
+from neutron.api.rpc.callbacks.producer import registry
 from neutron import context
 from neutron.db import api as db_api
 from neutron import manager
@@ -350,6 +351,7 @@ class GroupPolicyDbTestCase(GroupPolicyDBTestBase,
 
         extensions.append_api_extensions_path(
             gbpservice.neutron.extensions.__path__)
+        print("IGOR %s", service_plugins)
         super(GroupPolicyDbTestCase, self).setUp(
             plugin=core_plugin, ext_mgr=ext_mgr,
             service_plugins=service_plugins
@@ -372,6 +374,7 @@ class GroupPolicyDbTestCase(GroupPolicyDBTestBase,
 
     def tearDown(self):
         self._unset_notification_mocks()
+        registry.clear()
         super(GroupPolicyDbTestCase, self).tearDown()
 
 
