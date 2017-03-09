7239b4897d285dd3a69262ae0d038a744ddc7a90
Mahesh Kurund <kurundmahesh@gmail.com>
2017-03-09 15:34:09 +0530

    NFP changes to support async model
    Following are the NFP side changes to support async operations.
    1. Added get_status api in nfp_node_driver to update operational
       state of servicechain node.
    2. Using queue notifications to send rpc to orchestrator.
    3. Extended ncp_node_instance_network_function_mappings table to
       have status and status_details of an network_function.
    
    Change-Id: I5375066fb640d53c6bc5f0a7cf65902faa221519
    Closes-Bug: 1671077

--- gbpservice/tests/contrib/devstack/exercises-nfp/fw.sh
+++ gbpservice/tests/contrib/devstack/exercises-nfp/fw.sh
@@ -20,6 +20,7 @@ EXERCISE_DIR=$(cd $(dirname "$0") && pwd)
 TOP_DIR=$(cd $EXERCISE_DIR/..; pwd)
 
 source $TOP_DIR/openrc neutron service
+source $TOP_DIR/exercises/nfp_lib.sh
 
 create_gbp_resources() {
     gbp servicechain-node-create --service-profile base_mode_fw --template-file $TOP_DIR/nfp-templates/fw_template.yml FWNODE
@@ -36,6 +37,7 @@ create_gbp_resources() {
     gbp policy-rule-set-create --policy-rules "fw-web-redirect-rule fw-web-allow-rule-tcp fw-web-allow-rule-udp fw-web-allow-rule-icmp" fw-webredirect-ruleset
     gbp group-create fw-consumer --consumed-policy-rule-sets "fw-webredirect-ruleset=None"
     gbp group-create fw-provider --provided-policy-rule-sets "fw-webredirect-ruleset=None"
+    check_group_status fw-provider 600
 }
 
 delete_gbp_resources() {
@@ -121,6 +123,7 @@ update_gbp_resources() {
     fi
     
     gbp group-update fw-provider --provided-policy-rule-sets "fw-webredirect-ruleset=None"
+    check_group_status fw-provider 600
     ServiceChainInstanceCount=`gbp sci-list -f value | grep fw-provider | wc -l`
     if [ "$ServiceChainInstanceCount" -eq "1" ]; then
         echo "Chain created"
