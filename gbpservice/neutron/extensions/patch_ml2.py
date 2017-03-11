96c25838c9d8f2478e07e9167377ad4d20ed2c4e
Robert Kukura <kukura@noironetworks.com>
2017-03-10 20:38:30 -0500

    Sync ML2 monkey-patch with mitaka
    The ML2 plugin's delete_network and delete_subnet functions are
    monkey-patched in GBP to bail out of potentially infinite loops after
    100 attempts. This monkey patch is updated to be based on Neutron's
    stable/mitaka rather than stable/liberty versions of these functions.
    
    Note that when GBP is updated for stable/newton, it should be possible
    remove the monkey-patching of these functions. Calls to
    session.expire_all() have been added, along with other changes, to
    address the potentially infinite looping (see
    https://bugs.launchpad.net/neutron/+bug/1623990).
    
    Change-Id: I30a117a6a6f5e0adc254b5ae88db811620c35422

--- gbpservice/neutron/extensions/patch_ml2.py
+++ gbpservice/neutron/extensions/patch_ml2.py
@@ -10,12 +10,15 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from neutron._i18n import _
+from neutron._i18n import _LE
+from neutron._i18n import _LI
+from neutron._i18n import _LW
 from neutron.api.v2 import attributes
 from neutron.common import exceptions as exc
 from neutron.common import ipv6_utils
 from neutron.db import db_base_plugin_v2
 from neutron.db import models_v2
-from neutron import i18n
 from neutron.plugins.ml2.common import exceptions as ml2_exc
 from neutron.plugins.ml2 import driver_context
 from neutron.plugins.ml2 import plugin
@@ -35,7 +38,7 @@ class InfiniteLoopError(exc.NeutronException):
 # REVISIT(rkukura): Partially address bug 1510327 (GBP: Deleting
 # groups leads to subnet-delete in infinite loop) by limiting the
 # retry loop and doing additional info-level logging. This is based on
-# the stable/liberty version of neutron.plugins.ml2.plugin.Ml2Plugin.
+# the stable/mitaka version of neutron.plugins.ml2.plugin.Ml2Plugin.
 def delete_network(self, context, id):
     # REVISIT(rkukura) The super(Ml2Plugin, self).delete_network()
     # function is not used because it auto-deletes ports and
@@ -49,7 +52,7 @@ def delete_network(self, context, id):
     attempt = 0
     while True:
         attempt += 1
-        LOG.info(i18n._LI("Attempt %(attempt)s to delete network %(net)s"),
+        LOG.info(_LI("Attempt %(attempt)s to delete network %(net)s"),
                  {'attempt': attempt, 'net': id})
         if attempt > 100:
             raise InfiniteLoopError()
@@ -111,14 +114,14 @@ def delete_network(self, context, id):
             with excutils.save_and_reraise_exception() as ctxt:
                 if isinstance(e.inner_exception, sql_exc.IntegrityError):
                     ctxt.reraise = False
-                    LOG.warning(i18n._LW("A concurrent port creation has "
-                        "occurred"))
+                    LOG.warning(_LW("A concurrent port creation has "
+                                    "occurred"))
                     continue
-        LOG.info(i18n._LI("Auto-deleting ports %(ports)s for network %(net)s"),
+        LOG.info(_LI("Auto-deleting ports %(ports)s for network %(net)s"),
                  {'ports': ports, 'net': id})
         self._delete_ports(context, port_ids)
-        LOG.info(i18n._LI("Auto-deleting subnets %(subnets)s for network "
-                          "%(net)s"), {'subnets': subnets, 'net': id})
+        LOG.info(_LI("Auto-deleting subnets %(subnets)s for network %(net)s"),
+                 {'subnets': subnets, 'net': id})
         self._delete_subnets(context, subnet_ids)
 
     try:
@@ -127,15 +130,15 @@ def delete_network(self, context, id):
         # TODO(apech) - One or more mechanism driver failed to
         # delete the network.  Ideally we'd notify the caller of
         # the fact that an error occurred.
-        LOG.error(i18n._LE("mechanism_manager.delete_network_postcommit"
-            " failed"))
+        LOG.error(_LE("mechanism_manager.delete_network_postcommit"
+                      " failed"))
     self.notifier.network_delete(context, id)
 
 plugin.Ml2Plugin.delete_network = delete_network
 
 
 # REVISIT(rkukura): Related to bug 1510327, also limit the retry loop
-# here. This is based on the stable/liberty version of
+# here. This is based on the stable/mitaka version of
 # neutron.plugins.ml2.plugin.Ml2Plugin.
 def delete_subnet(self, context, id):
     # REVISIT(rkukura) The super(Ml2Plugin, self).delete_subnet()
@@ -147,10 +150,11 @@ def delete_subnet(self, context, id):
 
     LOG.debug("Deleting subnet %s", id)
     session = context.session
+    deallocated = set()
     attempt = 0
     while True:
         attempt += 1
-        LOG.info(i18n._LI("Attempt %(attempt)s to delete subnet %(subnet)s"),
+        LOG.info(_LI("Attempt %(attempt)s to delete subnet %(subnet)s"),
                  {'attempt': attempt, 'subnet': id})
         if attempt > 100:
             raise InfiniteLoopError()
@@ -171,43 +175,52 @@ def delete_subnet(self, context, id):
                 qry_allocated = (
                     qry_allocated.filter(models_v2.Port.device_owner.
                     in_(db_base_plugin_v2.AUTO_DELETE_PORT_OWNERS)))
-            allocated = qry_allocated.all()
-            # Delete all the IPAllocation that can be auto-deleted
-            if allocated:
-                for x in allocated:
-                    session.delete(x)
+            allocated = set(qry_allocated.all())
             LOG.debug("Ports to auto-deallocate: %s", allocated)
-            # Check if there are more IP allocations, unless
-            # is_auto_address_subnet is True. In that case the check is
-            # unnecessary. This additional check not only would be wasteful
-            # for this class of subnet, but is also error-prone since when
-            # the isolation level is set to READ COMMITTED allocations made
-            # concurrently will be returned by this query
             if not is_auto_addr_subnet:
-                alloc = self._subnet_check_ip_allocations(context, id)
-                if alloc:
-                    user_alloc = self._subnet_get_user_allocation(
-                        context, id)
-                    if user_alloc:
-                        LOG.info(i18n._LI("Found port (%(port_id)s, %(ip)s) "
-                            "having IP allocation on subnet "
-                            "%(subnet)s, cannot delete"),
-                            {'ip': user_alloc.ip_address,
-                             'port_id': user_alloc.port_id,
-                             'subnet': id})
-                        raise exc.SubnetInUse(subnet_id=id)
-                    else:
-                        # allocation found and it was DHCP port
-                        # that appeared after autodelete ports were
-                        # removed - need to restart whole operation
-                        raise os_db_exception.RetryRequest(
-                            exc.SubnetInUse(subnet_id=id))
+                user_alloc = self._subnet_get_user_allocation(
+                    context, id)
+                if user_alloc:
+                    LOG.info(_LI("Found port (%(port_id)s, %(ip)s) "
+                                 "having IP allocation on subnet "
+                                 "%(subnet)s, cannot delete"),
+                             {'ip': user_alloc.ip_address,
+                              'port_id': user_alloc.port_id,
+                              'subnet': id})
+                    raise exc.SubnetInUse(subnet_id=id)
 
             db_base_plugin_v2._check_subnet_not_used(context, id)
 
-            # If allocated is None, then all the IPAllocation were
-            # correctly deleted during the previous pass.
-            if not allocated:
+            # SLAAC allocations currently can not be removed using
+            # update_port workflow, and will persist in 'allocated'.
+            # So for now just make sure update_port is called once for
+            # them so MechanismDrivers is aware of the change.
+            # This way SLAAC allocation is deleted by FK on subnet deletion
+            # TODO(pbondar): rework update_port workflow to allow deletion
+            # of SLAAC allocation via update_port.
+            to_deallocate = allocated - deallocated
+
+            # If to_deallocate is blank, then all known IPAllocations
+            # (except SLAAC allocations) were correctly deleted
+            # during the previous pass.
+            # Check if there are more IP allocations, unless
+            # is_auto_address_subnet is True. If transaction isolation
+            # level is set to READ COMMITTED allocations made
+            # concurrently will be returned by this query and transaction
+            # will be restarted. It works for REPEATABLE READ isolation
+            # level too because this query is executed only once during
+            # transaction, and if concurrent allocations are detected
+            # transaction gets restarted. Executing this query second time
+            # in transaction would result in not seeing allocations
+            # committed by concurrent transactions.
+            if not to_deallocate:
+                if (not is_auto_addr_subnet and
+                        self._subnet_check_ip_allocations(context, id)):
+                    # allocation found and it was DHCP port
+                    # that appeared after autodelete ports were
+                    # removed - need to restart whole operation
+                    raise os_db_exception.RetryRequest(
+                        exc.SubnetInUse(subnet_id=id))
                 network = self.get_network(context, subnet['network_id'])
                 mech_context = driver_context.SubnetContext(self, context,
                                                             subnet,
@@ -225,23 +238,33 @@ def delete_subnet(self, context, id):
                 LOG.debug("Committing transaction")
                 break
 
-        for a in allocated:
+        for a in to_deallocate:
+            deallocated.add(a)
             if a.port:
                 # calling update_port() for each allocation to remove the
                 # IP from the port and call the MechanismDrivers
-                data = {attributes.PORT:
-                        {'fixed_ips': [{'subnet_id': ip.subnet_id,
-                                        'ip_address': ip.ip_address}
-                                       for ip in a.port.fixed_ips
-                                       if ip.subnet_id != id]}}
+                fixed_ips = [{'subnet_id': ip.subnet_id,
+                              'ip_address': ip.ip_address}
+                             for ip in a.port.fixed_ips
+                             if ip.subnet_id != id]
+                # By default auto-addressed ips are not removed from port
+                # on port update, so mark subnet with 'delete_subnet' flag
+                # to force ip deallocation on port update.
+                if is_auto_addr_subnet:
+                    fixed_ips.append({'subnet_id': id,
+                                      'delete_subnet': True})
+                data = {attributes.PORT: {'fixed_ips': fixed_ips}}
                 try:
-                    self.update_port(context, a.port_id, data)
+                    # NOTE Don't inline port_id; needed for PortNotFound.
+                    port_id = a.port_id
+                    self.update_port(context, port_id, data)
                 except exc.PortNotFound:
-                    LOG.debug("Port %s deleted concurrently", a.port_id)
+                    # NOTE Attempting to access a.port_id here is an error.
+                    LOG.debug("Port %s deleted concurrently", port_id)
                 except Exception:
                     with excutils.save_and_reraise_exception():
-                        LOG.exception(i18n._LE("Exception deleting fixed_ip "
-                            "from port %s"), a.port_id)
+                        LOG.exception(_LE("Exception deleting fixed_ip "
+                                          "from port %s"), port_id)
 
     try:
         self.mechanism_manager.delete_subnet_postcommit(mech_context)
@@ -249,7 +272,6 @@ def delete_subnet(self, context, id):
         # TODO(apech) - One or more mechanism driver failed to
         # delete the subnet.  Ideally we'd notify the caller of
         # the fact that an error occurred.
-        LOG.error(i18n._LE(
-            "mechanism_manager.delete_subnet_postcommit failed"))
+        LOG.error(_LE("mechanism_manager.delete_subnet_postcommit failed"))
 
 plugin.Ml2Plugin.delete_subnet = delete_subnet
