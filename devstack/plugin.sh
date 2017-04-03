9273d83c003e019a755d33eaeeed6c6038b89d6e
Rajendra Machani <rajendra.machani@oneconvergence.com>
2017-04-03 11:33:52 +0530

    Fix to read correct options of keystone_authtoken
    Reading the correct option names from keystone_authtoken group for authentication.
    
    Change-Id: If3d05592b67f9e75f34f14864e564084c991b9e7
    Closes-Bug: 1678732

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -28,9 +28,6 @@ function gbp_configure_neutron {
 
 function nfp_configure_neutron {
     NEUTRON_ML2_CONF="/etc/neutron/plugins/ml2/ml2_conf.ini"
-    iniset $NEUTRON_CONF keystone_authtoken admin_tenant_name "service"
-    iniset $NEUTRON_CONF keystone_authtoken admin_user "neutron"
-    iniset $NEUTRON_CONF keystone_authtoken admin_password $ADMIN_PASSWORD
     iniset $NEUTRON_CONF node_composition_plugin node_plumber "admin_owned_resources_apic_plumber"
     iniset $NEUTRON_CONF node_composition_plugin node_drivers "nfp_node_driver"
     iniset $NEUTRON_CONF admin_owned_resources_apic_tscp plumbing_resource_owner_user "neutron"
