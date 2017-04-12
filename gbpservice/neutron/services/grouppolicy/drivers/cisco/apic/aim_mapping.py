587512c122a4437cb4304c1cac43367dbbcb8bf0
Kent Wu <wu@noironetworks.com>
2017-04-11 21:17:33 -0700

    [AIM] Late binding for the VMM domain and phys-dom
    WIP
    
    Change-Id: Ie0882117b75ada3c2f32770adc7bc147a61dfd14

--- gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/aim_mapping.py
+++ gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/aim_mapping.py
@@ -590,11 +590,6 @@ class AIMMappingDriver(nrd.CommonNeutronBase, aim_rpc.AIMMappingRPCMixin):
             policy_enforcement_pref=
             self._get_policy_enforcement_pref(context.current))
 
-        session = context._plugin_context.session
-        aim_ctx = aim_context.AimContext(session)
-        vmms, phys = self.aim_mech_driver.get_aim_domains(aim_ctx)
-        aim_epg.openstack_vmm_domain_names = vmms
-        aim_epg.physical_domain_names = phys
         # AIM EPG will be persisted in the following call
         self._add_implicit_svc_contracts_to_epg(context, l2p_db, aim_epg)
 
