b488c28b4a96170c0a20b53c6819f8cf569679e7
mak-454 <ahmed.khan@oneconvergence.com>
2017-04-18 17:52:52 +0530

    NFP - Failure handling in Orchestrator
    Added following support :
    1) Context managers,
       1.1) To be used with python 'with' statement.
       1.2) support 'retry', 'ignore' and 'lock' functions.
             -> retry : retry a func for n counts
             -> ignore: Ignore certain expected exceptions.
             -> lock: Lock a db transaction
       1.3) NFP module need not handle all possible exceptions
            as try-except branches.
    
    2) Single class Exception Handling :
       All the exceptions from module will be caught by nfp/core
       and the registered exception handler will be invoked with
       all the relevant details (event, data, context, exception..)
    
    3) Used 'context manager' retry function with client methods,
       Neutronclient, Novaclient etc.. especially for GET methods.
       E.x, GET_TOKEN is retried 'n' times to overcome any
       temporary failures with keystone.
    
    Change-Id: I4dd520e1dc83db20b757e875c84782ca2ab5430e
    Partial-Bug: 1668198

--- gbpservice/neutron/services/servicechain/plugins/ncp/node_drivers/nfp_node_driver.py
+++ gbpservice/neutron/services/servicechain/plugins/ncp/node_drivers/nfp_node_driver.py
@@ -12,7 +12,7 @@
 # limitations under the License.
 
 import eventlet
-from eventlet import greenpool
+import sys
 import threading
 import time
 
@@ -283,10 +283,9 @@ class NFPContext(object):
 
     @staticmethod
     def _initialise_attr(sc_instance_id):
-        context = {'thread_pool': greenpool.GreenPool(10),
-                   'active_threads': [],
-                   'sc_node_count': 0,
+        context = {'sc_node_count': 0,
                    'sc_gateway_type_nodes': [],
+                   'network_functions': [],
                    'update': False}
         if nfp_context_store.context:
             nfp_context_store.context.update({sc_instance_id: context})
@@ -440,62 +439,110 @@ class NFPNodeDriver(driver_base.NodeDriverBase):
             self.SUPPORTED_SERVICE_TYPES):
             raise InvalidServiceType()
 
-    def _wait(self, thread, context):
-        try:
-            result = thread.wait()
-            return result
-        except Exception as e:
-            NFPContext.clear_nfp_context(context.instance['id'])
-            raise e
-
     def create(self, context):
         try:
             context._plugin_context = self._get_resource_owner_context(
                 context._plugin_context)
             network_function_id = self._create_network_function(context)
+        except Exception:
+            # NFPContext.clear_nfp_context(context.instance['id'])
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            message = "Traceback: %s" % (exc_value)
+            LOG.error(message)
+            network_function_id = ''
+
+        finally:
             self._set_node_instance_network_function_map(
                 context.plugin_session, context.current_node['id'],
                 context.instance['id'], network_function_id)
-        except Exception as e:
-            NFPContext.clear_nfp_context(context.instance['id'])
-            raise e
 
-        self._wait_for_node_operation_completion(context,
-                                                 network_function_id,
-                                                 nfp_constants.CREATE)
+        self._wait_for_node_operation_completion(
+            context, network_function_id,
+            nfp_constants.CREATE)
+
 
     def _wait_for_node_operation_completion(self, context,
-                                            network_function_id,
-                                            operation):
-        # Check for NF status in a separate thread
-        LOG.debug("Spawning thread for nf ACTIVE poll operation: %s" % (
-            operation))
+                                            network_function_id, operation):
         nfp_context = NFPContext.get_nfp_context(context.instance['id'])
-        if operation == nfp_constants.DELETE:
-            gth = nfp_context['thread_pool'].spawn(
-                self._wait_for_network_function_delete_completion,
-                context, network_function_id)
-        else:
-            gth = nfp_context['thread_pool'].spawn(
-                self._wait_for_network_function_operation_completion,
-                context, network_function_id, operation=operation)
-
-        nfp_context['active_threads'].append(gth)
-
-        LOG.debug("Active Threads count (%d), sc_node_count (%d)" % (
-            len(nfp_context['active_threads']), nfp_context['sc_node_count']))
-
         nfp_context['sc_node_count'] -= 1
+        nfp_context['network_functions'].append(network_function_id)
 
-        # At last wait for the threads to complete, success/failure/timeout
-        if nfp_context['sc_node_count'] == 0:
-            nfp_context['thread_pool'].waitall()
-            # Get the results
-            for gth in nfp_context['active_threads']:
-                self._wait(gth, context)
-            NFPContext.clear_nfp_context(context.instance['id'])
-        else:
+        if nfp_context['sc_node_count'] != 0:
             NFPContext.store_nfp_context(context.instance['id'], **nfp_context)
+        else:
+            network_functions = nfp_context['network_functions']
+            LOG.info(_LI("NETWORT FUNCTIONS: %r" % network_functions))
+            nf_elapsed_time_map = {}
+            nf_status_map = {}
+            if operation == nfp_constants.DELETE:
+                timeout = cfg.CONF.nfp_node_driver.service_delete_timeout
+            else:
+                timeout = cfg.CONF.nfp_node_driver.service_create_timeout
+            for nf in network_functions:
+                nf_elapsed_time_map[nf] = 0
+                LOG.info(_LI("STARTED POLLING for %(operation)s network "
+                             "function for NF:%(network_function_id)s"),
+                         {'operation': operation,
+                          'network_function_id': nf})
+            complete_msg = ("COMPLETED POLLING for %s network function for NF:"
+                            % operation)
+            while network_functions:
+                for nf in network_functions[:]:
+                    elapsed = nf_elapsed_time_map[nf]
+                    if elapsed >= timeout:
+                        network_functions.remove(nf)
+                        nf_status_map[nf] = 'TIMEDOUT'
+                        msg = complete_msg + "%s, status: TIMEDOUT" % nf
+                        LOG.info(_LI(msg))
+                    else:
+                        b_time = time.time()
+                        status = self._poll_for_network_function(context, nf,
+                                                                 operation)
+                        e_time = time.time()
+                        if 'PENDING' in status:
+                            nf_elapsed_time_map[nf] += (e_time - b_time)
+                        else:
+                            nf_status_map[nf] = status
+                            network_functions.remove(nf)
+                            msg = complete_msg + "%s, status:%s" % (nf, status)
+                            LOG.info(_LI(msg))
+                eventlet.sleep(15)
+                for nf in network_functions:
+                    nf_elapsed_time_map[nf] += 15
+
+            NFPContext.clear_nfp_context(context.instance['id'])
+
+            all_success = True
+            for network_function_id, status in nf_status_map.iteritems():
+                LOG.info(
+                    _LI("Got %(operation)s network function result for NF:"
+                        "%(network_function_id)s with status:%(status)s"),
+                    {'network_function_id': network_function_id,
+                     'operation': operation,
+                     'status': status})
+                if status == nfp_constants.ERROR or status == 'TIMEDOUT':
+                    all_success = False
+            if not all_success:
+                if operation == nfp_constants.DELETE:
+                    raise NodeInstanceDeleteFailed()
+                else:
+                    raise NodeInstanceCreateFailed()
+
+    def _poll_for_network_function(self, context,
+                                   network_function_id, operation):
+
+        try:
+            network_function = self.nfp_notifier.get_network_function(
+                context.plugin_context, network_function_id)
+            if not network_function:
+                if operation == nfp_constants.DELETE:
+                    return "DELETED"
+                return nfp_constants.ERROR
+            return network_function['status']
+        except Exception as e:
+            LOG.error(_LE("Failed to retrieve network function(nf-%s) - %r"
+                      % (network_function_id, e)))
+            return nfp_constants.ERROR
 
     def update(self, context):
         NFPContext.clear_nfp_context(context.instance['id'])
@@ -537,31 +584,29 @@ class NFPNodeDriver(driver_base.NodeDriverBase):
             context.plugin_session,
             context.current_node['id'],
             context.instance['id'])
-
-        if not network_function_map:
-            NFPContext.store_nfp_context(
-                context.instance['id'],
-                sc_gateway_type_nodes=[],
-                sc_node_count=nfp_context['sc_node_count'] - 1)
-            return
-
-        network_function_id = network_function_map.network_function_id
-        try:
-            self.nfp_notifier.delete_network_function(
-                context=context.plugin_context,
-                network_function_id=network_function_id)
-        except Exception as e:
-            NFPContext.clear_nfp_context(context.instance['id'])
-            LOG.exception(_LE("Delete Network service Failed"))
+        network_function_id = None
+        if network_function_map:
             self._delete_node_instance_network_function_map(
                 context.plugin_session,
                 context.current_node['id'],
                 context.instance['id'])
-            raise e
+            network_function_id = network_function_map.network_function_id
+
+        if network_function_id:
+            try:
+                self.nfp_notifier.delete_network_function(
+                    context=context.plugin_context,
+                    network_function_id=(
+                        network_function_map.network_function_id))
+            except Exception:
+                # NFPContext.clear_nfp_context(context.instance['id'])
+                LOG.exception(_LE("Delete Network service Failed"))
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                message = "Traceback: %s" % (exc_value)
+                LOG.error(message)
 
         self._update_ptg(context)
-        self._wait_for_node_operation_completion(context,
-                                                 network_function_id,
+        self._wait_for_node_operation_completion(context, network_function_id,
                                                  nfp_constants.DELETE)
 
     def update_policy_target_added(self, context, policy_target):
@@ -683,49 +728,17 @@ class NFPNodeDriver(driver_base.NodeDriverBase):
                                 {'provided_policy_rule_sets':
                                     dict((x, '') for x in prs)}})
 
-    def _wait_for_network_function_delete_completion(self, context,
-                                                     network_function_id):
-        time_waited = 0
-        network_function = None
-        curr_time = start_time = int(time.time())
-        timeout = cfg.CONF.nfp_node_driver.service_delete_timeout
-
-        while curr_time - start_time < timeout:
-            curr_time = int(time.time())
-            network_function = self.nfp_notifier.get_network_function(
-                context.plugin_context, network_function_id)
-            if network_function:
-                LOG.debug("Got %s nf result for NF: %s with status:%s,"
-                          "time waited: %s" % (network_function_id, 'delete',
-                          time_waited, network_function['status']))
-            if not network_function:
-                break
-            eventlet.sleep(5)
-            time_waited = time_waited + 5
-
-        LOG.debug("Deleting sci nf mapping")
-        self._delete_node_instance_network_function_map(
-            context.plugin_session,
-            context.current_node['id'],
-            context.instance['id'])
-        LOG.debug("sci nf mapping got deleted. NF got deldted.")
-
-        if network_function:
-            LOG.error(_LE("Delete network function %(network_function)s "
-                          "failed"),
-                      {'network_function': network_function_id})
-            raise NodeInstanceDeleteFailed()
-
     def _wait_for_network_function_operation_completion(self, context,
                                                         network_function_id,
                                                         operation):
+        if not network_function_id:
+            raise NodeInstanceCreateFailed()
+
         time_waited = 0
         network_function = None
         timeout = cfg.CONF.nfp_node_driver.service_create_timeout
-        curr_time = start_time = int(time.time())
 
-        while curr_time - start_time < timeout:
-            curr_time = int(time.time())
+        while time_waited < timeout:
             network_function = self.nfp_notifier.get_network_function(
                 context.plugin_context, network_function_id)
             LOG.debug("Got %s nf result for NF: %s with status:%s,"
@@ -1094,6 +1107,16 @@ class NFPNodeDriver(driver_base.NodeDriverBase):
             vip_ip = config_param_values.get('vip_ip')
             if not vip_ip:
                 raise VipNspNotSetonProvider()
+
+            if service_targets:
+                for provider_port in service_targets['provider_ports']:
+                    provider_port['allowed_address_pairs'] = [
+                        {'ip_address': vip_ip}]
+                    port = {
+                        'port': provider_port
+                    }
+                    context.core_plugin.update_port(
+                        context.plugin_context, provider_port['id'], port)
 
         provider = {
             'pt': service_targets.get('provider_pt_objs', []),
--- gbpservice/nfp/orchestrator/coal/networking/nfp_network_driver_base.py
+++ gbpservice/nfp/orchestrator/coal/networking/nfp_network_driver_base.py
@@ -16,6 +16,7 @@ class NFPNetworkDriverBase(object):
 
     Handles ports, operations on them
     """
+
     def __init__(self):
         pass
 
