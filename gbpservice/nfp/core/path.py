f60e72119c6a6bc66d46ea8e8fe9c897cd2a7801
mak-454 <ahmed.khan@oneconvergence.com>
2017-04-18 17:13:54 +0530

    NFP - Failure handling in core
    Added following support :
    1) Watchdog support for,
       1.1) Sequenced events.
       1.2) APIs sent to controller.
       1.3) Works delivered to threads in worker process.
    
    2) Path support,
       2.1) NFP modules can now identify their events with a Path.
            CREATE/UPDATE/DELETE
       2.2) Core offloads handling of following conditions on path,
            -> DELETE events while CREATE is going on.
            -> Discard delayed responses from controller on a path.
            -> UPDATE while an UPDATE is going on.
    
    3) Support for Event Context,
       3.1) Inherently passed along with each event.
       3.2) Modules can choose to override.
       3.3) Maintained as a python GT context so that all methods
            executing in that thread get access without being
            explicitly passed.
    
    Change-Id: I9feeeb77590b8c8844f5adfb09a04c8836874db7
    Partial-Bug: 1668198

---
+++ gbpservice/nfp/core/path.py
@@ -0,0 +1,169 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from gbpservice.nfp.core import log as nfp_logging
+
+import collections
+
+deque = collections.deque
+
+LOG = nfp_logging.getLogger(__name__)
+
+
+class Supress(object):
+
+    def __init__(self, ignore_list=None):
+        self._ignore = ignore_list or []
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, e_type, e_value, traceback):
+        if e_type in self._ignore:
+            return True
+        for exception in self._ignore:
+            if isinstance(e_type, exception):
+                return True
+
+
+class Path(object):
+
+    def __init__(self, name):
+        self._waitq = deque()
+        self.name = name
+        self.count = 0
+        self.invalid = False
+
+    def queue(self, event):
+        self._waitq.append(event)
+
+    def pop(self):
+        events = []
+
+        with Supress([IndexError]):
+            events.append(self._waitq.popleft())
+        return events
+
+    def done(self):
+        self._waitq.clear()
+
+# {'key': {'current':Path, 'waiting':Path}
+paths = {}
+
+
+def run():
+    for key, path in paths.iteritems():
+        if path['current'].count == 0:
+            path['current'].done()
+            if path['waiting'].name != 'INVALID':
+                path['current'] = path['waiting']
+                path['current'].invalid = False
+                path['waiting'] = Path('INVALID')
+
+    events = []
+    # Get any queued events in the current path
+    for key, path in paths.iteritems():
+        events += path['current'].pop()
+    return events
+
+
+def event_complete(event):
+    name = event.desc.path_type
+    key = event.desc.path_key
+
+    if not name:
+        return
+    name = name.upper()
+    with Supress([KeyError]):
+        path = paths[key]
+        if path['current'].name != name:
+            return
+        path['current'].count -= 1
+
+
+def schedule_event(event):
+    name = event.desc.path_type
+    key = event.desc.path_key
+
+    if not name:
+        return 'schedule'
+
+    name = name.upper()
+
+    try:
+        path = paths[key]
+        if path['current'].name == name:
+            if path['current'].invalid:
+                return 'discard'
+            path['current'].count += 1
+            return 'schedule'
+
+        if path['waiting'].name == name:
+            path['waiting'].queue(event)
+            return 'wait'
+
+        if path['current'].name != name:
+            return 'discard'
+    except Exception:
+        return 'schedule'
+    return 'schedule'
+
+
+def path_complete(path_type, key):
+    try:
+        path = paths[key]
+        if path['current'].name == path_type.upper() and (
+                path['waiting'].name == 'INVALID'):
+            paths.pop(key)
+    except KeyError:
+        message = "Path completion - %s path does not exist with key %s" % (
+            path_type, key)
+        LOG.debug(message)
+
+
+def create_path(key):
+        # Create cannot progress if there is already a path
+        # with the same key in any state
+    try:
+        path = paths[key]
+        assert False, "Path (%s) with key (%s) is already in progress" % (
+            path['current'].name, key)
+    except KeyError:
+        # Create new path
+        paths[key] = {'current': Path('CREATE'), 'waiting': Path('INVALID')}
+
+
+def delete_path(key):
+    try:
+        path = paths[key]
+        if path['current'].name != 'DELETE':
+            path['waiting'] = Path('DELETE')
+            path['current'].invalid = True
+        else:
+            assert False, ("Delete Path (%s) with key (%s)"
+                           "is already in progress" % (
+                               path['current'].name, key))
+    except KeyError:
+        paths[key] = {'current': Path('DELETE'), 'waiting': Path('INVALID')}
+
+
+def update_path(key):
+    # Update cannot progress if there is DELETE already in progress
+    # or DELETE already waiting.
+    try:
+        path = paths[key]
+        assert False, "Path (%s) with key (%s) is in progress" % (
+            path['current'].name, key)
+    except KeyError:
+        # Create new path
+        paths[key] = {'current': Path('UPDATE'), 'waiting': Path('INVALID')}
