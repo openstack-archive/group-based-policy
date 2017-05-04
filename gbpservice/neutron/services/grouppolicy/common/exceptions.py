cef6c2b4dadc29c85cde9d77382ce95d58f5a568
ashu_mishra <mca.ashu4@gmail.com>
2017-05-04 16:42:20 +0530

    Handle delete Async issue
    This patch resolves the issue, by enforcing chain deletion first(i.e. use need
    to clear provided prs from the group) before ptg deletion.
    Since chain deletion still goes as asynchronous, If user tries to delete
    ptg again, aim driver set the status description with proper message to retry
    after some time.
    
    Resolves the issue where, with converge plugin delete is failing while
    deleting grouppolicy resources. Due to asynchronous model, chain deletion
    goes in backend, while aim driver delete resources like subnet, pts.
    Observed that subnet deletion failed, since chain(vm) created by node driver
    still holding the ports on that subnet.
    
    Change-Id: Ic47bcbb569860bc765334eff81bc54506a3fb020

--- gbpservice/neutron/services/grouppolicy/common/exceptions.py
+++ gbpservice/neutron/services/grouppolicy/common/exceptions.py
@@ -329,3 +329,9 @@ class IdenticalExternalRoute(GroupPolicyBadRequest):
     message = _("External segments %(es1)s and %(es2)s cannot have "
                 "identical external route CIDR %(cidr)s if associated "
                 "with a common L3 policy.")
+
+
+class DeleteOfPTGProvidingPRSNotSupported(GroupPolicyBadRequest):
+    message = _("Deletion of Policy target group which is providing"
+                "redirect PRS is not supported. First detach the redirect"
+                "PRS by updating PTG")
