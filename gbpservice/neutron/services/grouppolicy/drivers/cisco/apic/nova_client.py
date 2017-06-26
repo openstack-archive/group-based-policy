f3352b325d2c65ff3d61f7ce88c2d078eabe041b
Ivar Lazzaro <ivarlazzaro@gmail.com>
2017-06-26 12:37:17 -0700

    Use Neutron's Nova client for retrieving VMs' name
    This will make sure we always use the most recently supported
    authentication methods.
    
    Change-Id: I66a64d11ae09c426ca0114020c8b985f9307515e

--- gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/nova_client.py
+++ gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/nova_client.py
@@ -10,12 +10,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from keystoneauth1 import loading as ks_loading
 from neutron._i18n import _LW
 from neutron.notifiers import nova as n_nova
-from novaclient import client as nclient
 from novaclient import exceptions as nova_exceptions
-from oslo_config import cfg
 from oslo_log import log as logging
 
 LOG = logging.getLogger(__name__)
@@ -27,14 +24,7 @@ client = None
 def _get_client():
     global client
     if client is None:
-        auth = ks_loading.load_auth_from_conf_options(cfg.CONF, 'nova')
-        session = ks_loading.load_session_from_conf_options(
-            cfg.CONF, 'nova', auth=auth)
-
-        client = nclient.Client(
-            n_nova.NOVA_API_VERSION, session=session,
-            region_name=cfg.CONF.nova.region_name,
-            endpoint_type=cfg.CONF.nova.endpoint_type)
+        client = n_nova.Notifier().nclient
     return client
 
 
