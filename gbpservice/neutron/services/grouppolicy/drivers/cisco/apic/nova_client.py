81a90fb82fd17eb094848a6c96a92fc5b024a664
Ivar Lazzaro <ivarlazzaro@gmail.com>
2017-06-26 12:37:17 -0700

    Use Neutron's Nova client for retrieving VMs' name
    This will make sure we always use the most recently supported
    authentication methods.
    
    Change-Id: I66a64d11ae09c426ca0114020c8b985f9307515e

--- gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/nova_client.py
+++ gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/nova_client.py
@@ -10,12 +10,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from keystoneclient import auth as ks_auth
-from keystoneclient import session as ks_session
 from neutron.notifiers import nova as n_nova
-from novaclient import client as nclient
 from novaclient import exceptions as nova_exceptions
-from oslo_config import cfg
 from oslo_log import log as logging
 
 LOG = logging.getLogger(__name__)
@@ -27,30 +23,7 @@ client = None
 def _get_client():
     global client
     if client is None:
-        auth = ks_auth.load_from_conf_options(cfg.CONF, 'nova')
-        endpoint_override = None
-
-        if not auth:
-
-            if cfg.CONF.nova_admin_tenant_id:
-                endpoint_override = "%s/%s" % (cfg.CONF.nova_url,
-                                               cfg.CONF.nova_admin_tenant_id)
-
-            auth = n_nova.DefaultAuthPlugin(
-                auth_url=cfg.CONF.nova_admin_auth_url,
-                username=cfg.CONF.nova_admin_username,
-                password=cfg.CONF.nova_admin_password,
-                tenant_id=cfg.CONF.nova_admin_tenant_id,
-                tenant_name=cfg.CONF.nova_admin_tenant_name,
-                endpoint_override=endpoint_override)
-
-        session = ks_session.Session.load_from_conf_options(
-            cfg.CONF, 'nova', auth=auth)
-        novaclient_cls = nclient.get_client_class(n_nova.NOVA_API_VERSION)
-
-        client = novaclient_cls(
-            session=session,
-            region_name=cfg.CONF.nova.region_name)
+        client = n_nova.Notifier().nclient
     return client
 
 
