b914cb4546893ea35f0848cdfb5ea91923efaccb
Ivar Lazzaro <ivarlazzaro@gmail.com>
2017-07-11 12:04:50 -0700

    Fix keystone client domain id to name
    Change-Id: I7f4bcb5a9f82ff2f634c090b672224360b0613e9

--- gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/apic_mapping.py
+++ gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/apic_mapping.py
@@ -312,8 +312,9 @@ class ApicMappingDriver(api.ResourceMappingDriver,
                 admin_auth = keypassword.Password(
                     auth_url=pass_params[0],
                     username=pass_params[1], password=pass_params[2],
-                    tenant_name=pass_params[3], user_domain_id=pass_params[5],
-                    project_domain_id=pass_params[4])
+                    tenant_name=pass_params[3],
+                    user_domain_name=pass_params[5],
+                    project_domain_name=pass_params[4])
                 session = keysession.Session(auth=admin_auth)
             ApicMappingDriver.manager = apic_manager.APICManager(
                 apic_model.ApicDbModel(), logging, network_config, apic_config,
