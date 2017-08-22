d19e5335e664f5024d4311d98a2e9df7b90d3d2c
Anna Khmelnitsky <akhmelnitsky@vmware.com>
2017-08-21 17:09:10 -0700

    Fix gbp_fip exercise script
    net-list command was missing an argument value
    
    Change-Id: I2631c23e24f864e5d60bb25540f7c443b4226c4a

--- gbpservice/tests/contrib/devstack/exercises/gbp_fip.sh
+++ gbpservice/tests/contrib/devstack/exercises/gbp_fip.sh
@@ -43,7 +43,7 @@ function confirm_server_active {
 }
 
 
-EXT_NET_ID=$(neutron net-list --router:external -c id | grep -v id | awk '{print $2}' )
+EXT_NET_ID=$(neutron net-list --router:external=True -c id | grep -v id | awk '{print $2}' )
 EXT_NET_TO_BE_CLEANED_UP=false
 
 if [ -z "$EXT_NET_ID" ] ; then
@@ -51,7 +51,6 @@ if [ -z "$EXT_NET_ID" ] ; then
     EXT_SUBNET_ID=$(neutron subnet-create --ip_version 4 --gateway 172.16.73.1 --name public-subnet $EXT_NET_ID 172.16.73.0/24 | grep ' id ' | get_field 2)
     EXT_NET_TO_BE_CLEANED_UP=true
 else
-    EXT_NET_ID=$(neutron net-list --router:external -c id | grep -v id | awk '{print $2}' )
     EXT_SUBNET_ID=$(neutron net-show $EXT_NET_ID | grep subnets | awk '{print $4}' )
 fi
 
