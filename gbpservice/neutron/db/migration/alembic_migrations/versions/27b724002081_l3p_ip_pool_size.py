openstack-archive/group-based-policy

commit 1edf15bfb53570fb4fc3a489aab5d5da8dbac3e0

Increase ip_pool to 256 characters
The length of the ip_pool parameter in L3 policies is limited,
and needs to be increased in order to support multiple prefixes,
as well as prefixes from all address families (v4 and v6).

Change-Id: I9dc03ac4275e0f269746011a075bf3b78e0d5233
(cherry picked from commit e3e62e1197d494783eb586510a9ec8d2d4adee5a)
(cherry picked from commit 97bdb259907091fc1171122317f3ee9ac6a86172)

gbpservice/neutron/db/migration/alembic_migrations/versions/27b724002081_l3p_ip_pool_size.py
@@ -1,0 +1,39 @@
+# Copyright 2017 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""l3p ip pool size
+
+Revision ID: 27b724002081
+Revises: cb5b16acbeb0
+Create Date: 2017-07-06 17:34:18.856803
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '27b724002081'
+down_revision = 'cb5b16acbeb0'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.alter_column('gp_l3_policies', 'ip_pool',
+                    existing_type=sa.String(length=64),
+                    type_=sa.String(length=255))
+
+
+def downgrade():
+    pass

gbpservice/neutron/db/migration/alembic_migrations/versions/HEAD
@@ -1,1 +1,1 @@
-cb5b16acbeb0
+27b724002081

gbpservice/neutron/db/grouppolicy/group_policy_db.py
@@ -157,7 +157,7 @@ class L3Policy(model_base.BASEV2, BaseSharedGbpResource):
         'polymorphic_identity': 'base'
     }
     ip_version = sa.Column(sa.Integer, nullable=False)
-    ip_pool = sa.Column(sa.String(64))
+    ip_pool = sa.Column(sa.String(255))
     subnet_prefix_length = sa.Column(sa.Integer)
     l2_policies = orm.relationship(L2Policy, backref='l3_policy')
     external_segments = orm.relationship(

commit 8e5c1d3ae7e2dfe942976c7b32e33b08c2122550

Fix DB migrations
Commit 1edf15bfb53570fb4fc3a489aab5d5da8dbac3e0 broke the DB
migration path during upgrades. This patch moves the migration
in order to ensure that this migration and others aren't lost
when upgrading releases.

Change-Id: I84837640ba85c24dce7151338b252c45a061a8b3

gbpservice/neutron/db/migration/alembic_migrations/versions/27b724002081_l3p_ip_pool_size.py
@@ -16,14 +16,14 @@
 """l3p ip pool size
 
 Revision ID: 27b724002081
-Revises: cb5b16acbeb0
+Revises: cc09261e0fb5
 Create Date: 2017-07-06 17:34:18.856803
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '27b724002081'
-down_revision = 'cb5b16acbeb0'
+down_revision = 'cc09261e0fb5'
 
 from alembic import op
 import sqlalchemy as sa

gbpservice/neutron/db/migration/alembic_migrations/versions/HEAD
@@ -1,1 +1,1 @@
-27b724002081
+cb5b16acbeb0

gbpservice/neutron/db/migration/alembic_migrations/versions/cb5b16acbeb0_alter_project_id_length.py
@@ -14,14 +14,14 @@
 """alter project_id column length in gp_apic_tenant_specific_nat_epg
 
 Revision ID: cb5b16acbeb0
-Revises: cc09261e0fb5
+Revises: 27b724002081
 Create Date: 2017-06-10 00:00:00.000000
 
 """
 
 # revision identifiers, used by Alembic.
 revision = 'cb5b16acbeb0'
-down_revision = 'cc09261e0fb5'
+down_revision = '27b724002081'
 
 from alembic import op
 import sqlalchemy as sa