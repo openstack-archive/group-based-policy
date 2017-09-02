033271daea38830cbdaebcc9cad3931323e53585
Thomas Bachman <tbachman@yahoo.com>
2017-09-02 15:41:09 +0000

    [Do Not Merge]: Test multiscope routing UTs
    This is a test to see if we can capture the reason for the
    multiscope routing UT failures in the stable/ocata gate.
    
    Change-Id: If4c122bb7f4fc9fcf5059ca52bd4d6233fb2df4b

--- gbpservice/neutron/tests/unit/plugins/ml2plus/test_apic_aim.py
+++ gbpservice/neutron/tests/unit/plugins/ml2plus/test_apic_aim.py
@@ -41,8 +41,10 @@ from neutron.tests.unit.extensions import test_l3
 from neutron_lib import constants as n_constants
 from neutron_lib.plugins import directory
 from opflexagent import constants as ofcst
+from oslo_log import log
 import webob.exc
 
+from gbpservice._i18n import _LW
 from gbpservice.neutron.db import implicitsubnetpool_db  # noqa
 from gbpservice.neutron.extensions import cisco_apic_l3 as l3_ext
 from gbpservice.neutron.plugins.ml2plus.drivers.apic_aim import (
@@ -55,6 +57,8 @@ from gbpservice.neutron.plugins.ml2plus.drivers.apic_aim import data_migrations
 from gbpservice.neutron.plugins.ml2plus.drivers.apic_aim import db
 from gbpservice.neutron.plugins.ml2plus.drivers.apic_aim import exceptions
 from gbpservice.neutron.plugins.ml2plus import patch_neutron
+
+LOG = log.getLogger(__name__)
 
 PLUGIN_NAME = 'gbpservice.neutron.plugins.ml2plus.plugin.Ml2PlusPlugin'
 
@@ -1313,6 +1317,7 @@ class TestAimMapping(ApicAimTestCase):
             expected_gw_ips = []
             unexpected_gw_ips = []
             for net, routed_subnets, unrouted_subnets, scope, project in nets:
+                LOG.warning(_LW("Showing network %s"), net['id'])
                 net = self._show('networks', net['id'])['network']
                 self._check_network(
                     net, [router] if routed_subnets else [], scope, project)
