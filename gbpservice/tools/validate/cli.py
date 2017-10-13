70d26a33f1ce4f14cfcb564e003b63d4b69157a4
Robert Kukura <kukura@noironetworks.com>
2017-10-13 15:25:12 -0400

    [AIM] Validation/Repair/Migration Tool
    Adds validation and repair framework that calls into mechanism and
    policy drivers to validate mappings to Neutron resources and to AIM
    resources. The mappings from all standard Neutron resources to AIM are
    currently handled. New unit tests are provided for validation of each
    resource, and validation calls are added to several existing unit
    tests.
    
    A simple command line interface is used perform validation, optionally
    repairing invalid state. This is run using 'gbp-validate [--repair]
    <neutron-server args>'. The same arguments that are passed to specify
    the configuration of neutron-server must be passed to gbp-validate.
    
    Validation of GBP and SFC resources and of SVI networks is not yet
    implemented. Attempting to validate deployments where GBP resources or
    SVI networks exist currently intentionally fails, even when repair is
    enabled. Attempting to validate deployments where SFC is used will
    currently corrupt the mapping to AIM if repair is enabled. Proper
    validation of these resources will be addressed in followup patches.
    
    For isomorphic address scopes with a non-pre-existing VRF, the VRF's
    display name currently depends on the order in which the address
    scopes were created. This will be addressed in a followup patch.
    
    EPG domain association, static paths, and other aspects of port
    binding are not yet validated. This will be addressed in a followup
    patch.
    
    Migration from the old APIC plugin to the unified plugin will require
    associating existing subnets with subnetpools, which will also be
    addressed in a followup patch.
    
    Change-Id: I0c3fe9e2629f76ecca8b3c8a93f9534b2d946e14

---
+++ gbpservice/tools/validate/cli.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2018 Cisco Systems Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import sys
+
+from oslo_config import cfg
+from neutron.common import config
+from neutron import manager
+from neutron_lib.plugins import directory
+
+from gbpservice.neutron.services.grouppolicy import (
+    group_policy_driver_api as api)
+
+# Any policy-driver specific CLI options must be included here, since
+# the CLI options must be registered before the GBP service plugin and
+# the configured policy drivers can be loaded.
+cli_opts = [
+    cfg.BoolOpt('repair', default=False, help='Enable repair of invalid state.')
+]
+
+
+def main():
+    cfg.CONF.register_cli_opts(cli_opts)
+    config.init(sys.argv[1:])
+
+    # REVISIT: Should enable logging but prevent output to stdout.
+    # config.setup_logging()
+
+    if not cfg.CONF.config_file:
+        sys.exit(_("ERROR: Unable to find configuration file via the default"
+                   " search paths (~/.neutron/, ~/, /etc/neutron/, /etc/) and"
+                   " the '--config-file' option!"))
+
+    manager.init()
+
+    gbp_plugin = directory.get_plugin('GROUP_POLICY')
+    if not gbp_plugin:
+        sys.exit("GBP service plugin not configured.")
+
+    result = gbp_plugin.validate_state(cfg.CONF.repair)
+    if result == api.VALIDATION_FAILED:
+        sys.exit("Validation failed")
+    return 0
