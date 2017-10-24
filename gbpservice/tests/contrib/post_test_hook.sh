f850605b9bfb9b65a4d5f4f744ea6b65daf6abf8
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2017-10-24 16:49:25 -0700

    Pike sync
    The following changes have been made to coordinate with the changes
    made in Neutron for Pike:
    
    * Partial use of Neutron context has been completely moved to neutron_lib's
    context.
    
    * The patching of neutron.db.api.get_session() has been replaced with
    patching of sqlalchemy.orm.session to add the notification_queue attribute.
    This significantly reduces the earlier complexity of patching.
    
    * Use of top-level start of transaction in GBP plugins:
    with context.session.begin(subtransactions=True):
    has been migrated to use of:
    with db_api.context_manager.writer.using(context):
    or
    with db_api.context_manager.reader.using(context)
    as relevant.
    
    * Calls to _make_resource_xxx_dict() in GBP plugins have been moved
    to inside the transaction.
    
    * The use of:
    neutron.callbacks.events
    neutron.callbacks.exceptions
    neutron.callbacks.registry
    to
    neutron_lib.callbacks.events
    neutron_lib.callbacks.exceptions
    neutron_lib.callbacks.registry
    
    * The use of:
    neutron.api.v2.attributes.resource_xxx
    neutron.extensions.extension_xxx
    to:
    from neutron_lib.api.definitions.resource_xxx
    from neutron_lib.api.definitions.extension_xxx
    resp.
    
    * The use of:
    neutron.db.db_base_plugin_v2.NeutronDbPluginV2.register_dict_extend_funcs
    to:
    neutron.db._resource_extend.resource_extend
    (the latter is a decorator)
    
    * The use of:
    neutron.db.db_base_plugin_v2.NeutronDbPluginV2.register_model_query_hook()
    to:
    from neutron.db import _model_query as model_query.register_hook()
    
    * The use of:
    neutron.db.segments_db.NetworkSegment
    to:
    neutron.db.models.segment.NetworkSegment
    
    * In the case of Neutron ml2plus plugin (used by APIC/AIM solution),
    the use of get_admin_context() has been patched to return elevated
    version of the current context in use. This helps to preserve the session
    and transaction semantics. Ideally, context.elevated() would have been
    directly used in all these places, however the current context is not
    available in these places, and hence getting the current context and elevating
    it is wrapped in the get_admin_context() patched method.
    
    * In the case of the components used by the APIC/AIM solution (including
    the ml2plus and l3_plugin) the use of:
    with context.session.begin(subtransactions=True):
    to
    with db_api.context_manager.writer.using(context):
    or
    with db_api.context_manager.reader.using(context):
    as relevant.
    
    * Patching of methods from Neutron which is no longer relevant have been
    removed from gbpservice.neutron.extensions.patch module.
    
    * Setting up of UTs has been fixed to load and reset configurations
    appropriately. This helps to eleminate some failures when tests are
    run in non-deterministic orders.
    
    * In tree devstack plugin has been updated (aim repo commit pin needs
    to be reverted).
    
    * Gate jobs have been updated as relevant (including fixes to the exercise
    scripts and job configurations).
    
    The associated repos, namely, client, UI and automation have also been
    updated (the reference to the client's gerrit patch needs to be updated
    once the patch has been merged).
    
    Change-Id: I11dd089effbf40cf104afd720dc40a9911dcf28d

--- gbpservice/tests/contrib/post_test_hook.sh
+++ gbpservice/tests/contrib/post_test_hook.sh
@@ -32,14 +32,14 @@ check_residual_resources demo demo
 echo "Running gbpfunc test suite"
 export PYTHONPATH="$GBP_FUNC_DIR:${PYTHONPATH}"
 cd $GBP_FUNC_DIR/testcases
+# Run tests as non-admin cred
+source_creds $TOP_DIR/openrc demo demo
+python suite_non_admin_run.py upstream
+gbpfunc_non_admin_exit_code=$?
 # Run shared_resource tests as admin cred
 source_creds $TOP_DIR/openrc admin admin
 python suite_admin_run.py
 gbpfunc_admin_exit_code=$?
-# Run rest of the tests as non-admin cred
-source_creds $TOP_DIR/openrc demo demo
-python suite_non_admin_run.py upstream
-gbpfunc_non_admin_exit_code=$?
 
 # Prepare the log files for Jenkins to upload
 prepare_logs
