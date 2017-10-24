ab203d66a4de1d3a7854131afcaad90d5c1b0335
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2017-10-24 16:49:25 -0700

    Preparing for Pike support 2 (neutron_lib context)
    Using the neutron context from neutron_lib (but
    continues using db_api from neutron instead of
    neutron_lib).
    
    Note that Neutron still uses the context from the Neutron
    repo in Ocata, so with this patch, both, Neutron and
    neutron_lib, contexts will be in play depending on
    whether the context is being instantiated by Neutron
    or by GBP.
    
    Also updating the tox hacking ignores to not require
    translation hints for log messages. This should have been
    removed in the previously merged patch which removed the log
    message hints.
    
    Change-Id: I8df4d00a8c0109e04e1f72576a54048332666078

--- gbpservice/neutron/tests/unit/services/servicechain/ncp/test_ncp_plugin.py
+++ gbpservice/neutron/tests/unit/services/servicechain/ncp/test_ncp_plugin.py
@@ -15,9 +15,9 @@ import webob.exc
 
 import mock
 from neutron.common import config  # noqa
-from neutron import context as n_context
 from neutron.db import api as db_api
 from neutron.plugins.common import constants as pconst
+from neutron_lib import context as n_context
 from neutron_lib.db import model_base
 from neutron_lib import exceptions as n_exc
 from neutron_lib.plugins import directory
