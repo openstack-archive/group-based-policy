aa3e0530e5a3991c48094355d7f07dd684d9226d
Thomas Bachman <tbachman@yahoo.com>
2017-10-27 01:43:33 +0000

    Set "Limit IP Learning to Subnet" to True
    The default value of the "Limit IP Learning to Subnet" flag
    is False in AIM when creating BDs. This can lead to problems where
    IPs become unreachable in deployments. This patch explicitly sets
    value of this flag to True.
    
    Change-Id: I8d7970b70912a1ea92f86878481ff5e7bc3321f7

--- gbpservice/neutron/tests/unit/plugins/ml2plus/test_apic_aim.py
+++ gbpservice/neutron/tests/unit/plugins/ml2plus/test_apic_aim.py
@@ -406,9 +406,11 @@ class TestAimMapping(ApicAimTestCase):
         session = db_api.get_session()
         aim_ctx = aim_context.AimContext(session)
         bd = aim_resource.BridgeDomain(tenant_name=tenant_name,
+                                       limit_ip_learn_to_subnets=True,
                                        name=bd_name)
         bd = self.aim_mgr.get(aim_ctx, bd)
         self.assertIsNotNone(bd)
+        self.assertTrue(bd.limit_ip_learn_to_subnets)
         return bd
 
     def _bd_should_not_exist(self, bd_name):
@@ -3900,6 +3902,9 @@ class TestExternalConnectivityBase(object):
             name='EXT-l1')
         ext_bd = aim_resource.BridgeDomain(
             tenant_name=self.t1_aname, name='EXT-l1')
+        aim_ctx = aim_context.AimContext(db_session=db_api.get_session())
+        aim_ext_bd = self.aim_mgr.get(aim_ctx, ext_bd)
+        self.assertTrue(aim_ext_bd.limit_ip_learn_to_subnets)
         ext_vrf = aim_resource.VRF(tenant_name=self.t1_aname, name='EXT-l1')
         self._check_dn(net1, ext_epg, 'EndpointGroup')
         self._check_dn(net1, ext_bd, 'BridgeDomain')
