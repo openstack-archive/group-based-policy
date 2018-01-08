7ebd79af3afc30e8bea8befb64168abc5fd12a98
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2018-01-08 11:51:36 -0800

    Set IPV4_ADDRS_SAFE_TO_USE for gate job config
    This should help with a gate failure where IPV4_ADDRS_SAFE_TO_USE
    is not set and results in job failing with the following error:
    
    'Could not determine host ip address. See local.conf for suggestions on
    setting HOST_IP.'
    
    Change-Id: I38195ef56593a42d2d81c697254125c75b7cfc9d
    (cherry picked from commit 34febe6fb52a5876bd442708f6d8f112af1b013e)

--- gbpservice/tests/contrib/gate_aim_hook.sh
+++ gbpservice/tests/contrib/gate_aim_hook.sh
@@ -21,16 +21,3 @@ disable_service postgresql
 enable_service mysql
 initialize_database_backends
 install_database
-
-# Set up the 'openstack_citest' user and database in each backend
-tmp_dir=`mktemp -d`
-
-cat << EOF > $tmp_dir/mysql.sql
-CREATE DATABASE openstack_citest;
-CREATE USER 'openstack_citest'@'localhost' IDENTIFIED BY 'openstack_citest';
-CREATE USER 'openstack_citest' IDENTIFIED BY 'openstack_citest';
-GRANT ALL PRIVILEGES ON *.* TO 'openstack_citest'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'openstack_citest';
-FLUSH PRIVILEGES;
-EOF
-/usr/bin/mysql -u root < $tmp_dir/mysql.sql
