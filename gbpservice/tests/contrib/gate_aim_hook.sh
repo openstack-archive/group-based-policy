9bb23a1faf145ddf3f3e28ec07c9aab85231531b
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2018-01-08 11:51:36 -0800

    Use devstack-vm-gate script for gate job config
    This should help with a gate failure where IPV4_ADDRS_SAFE_TO_USE
    is not set and results in job failing with the following error:
    
    'Could not determine host ip address. See local.conf for suggestions on
    setting HOST_IP.'
    
    Change-Id: I38195ef56593a42d2d81c697254125c75b7cfc9d

--- gbpservice/tests/contrib/gate_aim_hook.sh
+++ gbpservice/tests/contrib/gate_aim_hook.sh
@@ -8,9 +8,6 @@ set -x
 
 trap prepare_logs ERR
 
-# Make the workspace owned by the stack user
-sudo chown -R $STACK_USER:$STACK_USER $BASE
-
 # temporary fix for bug 1693689
 export IPV4_ADDRS_SAFE_TO_USE=${DEVSTACK_GATE_IPV4_ADDRS_SAFE_TO_USE:-${DEVSTACK_GATE_FIXED_RANGE:-10.1.0.0/20}}
 
@@ -25,15 +22,5 @@ enable_service mysql
 initialize_database_backends
 install_database
 
-# Set up the 'openstack_citest' user and database in each backend
-tmp_dir=`mktemp -d`
-
-cat << EOF > $tmp_dir/mysql.sql
-CREATE DATABASE openstack_citest;
-CREATE USER 'openstack_citest'@'localhost' IDENTIFIED BY 'openstack_citest';
-CREATE USER 'openstack_citest' IDENTIFIED BY 'openstack_citest';
-GRANT ALL PRIVILEGES ON *.* TO 'openstack_citest'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'openstack_citest';
-FLUSH PRIVILEGES;
-EOF
-/usr/bin/mysql -u root < $tmp_dir/mysql.sql
+export DEVSTACK_LOCALCONF=$(cat $LOCAL_CONF)
+$BASE/new/devstack-gate/devstack-vm-gate.sh
