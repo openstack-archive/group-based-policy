acfd53091df289e7275acec1d08d65c71d4087f1
Kent Wu <wu@noironetworks.com>
2018-02-07 16:15:00 -0800

    Query DB to get the tenant ID of the SG
    There is a use case that as an admin user, people can just issue
    port-udpate command to set the SG even if that SG is from another tenant.
    In this use case, the tenant ID of the SG could be from any tenant so we
    have to do a DB query to get that.
    
    Change-Id: I8b52b049928f1a0b6db56749c02a13cab39a2f98
    (cherry picked from commit 053f71e3d60f24b45798625fb6902b90de090ba4)
    (cherry picked from commit 4693f3f2f27308070908f5f7c75b1492df90c9aa)

--- gbpservice/neutron/tests/unit/services/grouppolicy/test_aim_mapping_driver.py
+++ gbpservice/neutron/tests/unit/services/grouppolicy/test_aim_mapping_driver.py
@@ -29,6 +29,7 @@ from neutron.callbacks import registry
 from neutron.common import utils as n_utils
 from neutron import context as nctx
 from neutron.db import api as db_api
+from neutron.db.models import securitygroup as sg_models
 from neutron.extensions import dns
 from neutron import manager
 from neutron.notifiers import nova
@@ -3094,11 +3095,18 @@ class TestPolicyTarget(AIMBaseTestCase):
         self.assertEqual(100, mapping['dhcp_lease_time'])
 
         port = self._plugin.get_port(self._context, pt2['port_id'])
-        port_tenant = self.name_mapper.project(None, port['tenant_id'])
         sg_list = []
-        for sg_id in port['security_groups']:
+
+        ctx = nctx.get_admin_context()
+        port_sgs = (ctx.session.query(sg_models.SecurityGroup.id,
+                                      sg_models.SecurityGroup.tenant_id).
+                    filter(sg_models.SecurityGroup.id.
+                           in_(port['security_groups'])).
+                    all())
+        for sg_id, tenant_id in port_sgs:
+            sg_tenant = self.name_mapper.project(None, tenant_id)
             sg_list.append(
-                {'policy-space': port_tenant,
+                {'policy-space': sg_tenant,
                  'name': sg_id})
         sg_list.append({'policy-space': 'common',
                         'name': self.driver.aim_mech_driver.apic_system_id +
