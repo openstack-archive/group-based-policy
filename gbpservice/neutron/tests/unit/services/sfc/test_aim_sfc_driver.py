ee1e4412657fafc138e7a53e5dc7964c5ef53093
Ivar Lazzaro <ivarlazzaro@gmail.com>
2018-03-16 14:33:50 -0700

    [AIM] Delete PC even if contracts are not present in prov/cons groups
    When deleting a Port Chain, make sure deletion doesn't fail
    even if some expected contrancs are missing from the
    provider or consumer groups
    
    Change-Id: If047370e1924e1863f78cd885c89dd81f758176d
    (cherry picked from commit d266bb01e4a4ca60f52e418ce09f20c32dd1bd90)

--- gbpservice/neutron/tests/unit/services/sfc/test_aim_sfc_driver.py
+++ gbpservice/neutron/tests/unit/services/sfc/test_aim_sfc_driver.py
@@ -1112,6 +1112,17 @@ class TestPortChain(TestAIMServiceFunctionChainingBase):
             port_description='topology/pod-1/paths-103/pathep-[eth3/1]')
         self._verify_pc_mapping(pc)
 
+    def test_delete_no_contract(self):
+        pc = self._create_simple_port_chain()
+        epgs = self.aim_mgr.find(self._aim_context, aim_res.EndpointGroup)
+        extn = self.aim_mgr.find(self._aim_context, aim_res.ExternalNetwork)
+        for res in epgs + extn:
+            self.aim_mgr.update(self._aim_context, res,
+                                consumed_contract_names=[])
+            self.aim_mgr.update(self._aim_context, res,
+                                provided_contract_names=[])
+        self._verify_pc_delete(pc)
+
 
 class TestPortChainSVI(TestPortChain):
 
