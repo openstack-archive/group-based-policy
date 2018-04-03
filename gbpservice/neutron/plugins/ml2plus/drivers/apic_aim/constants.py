b3ae61a089071299fed73ac30db9c662f8ad7217
Ivar Lazzaro <ivarlazzaro@gmail.com>
2018-04-03 16:11:21 -0700

    [AIM] Non SFC modules should not import sfc constants
    This makes it safe to run deployments without installing
    networking-sfc
    
    Change-Id: I90a77143bcdf751b8e9f08d130ab388b981c1b6c

---
+++ gbpservice/neutron/plugins/ml2plus/drivers/apic_aim/constants.py
@@ -0,0 +1,17 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+GBP_FLOW_CLASSIFIER = 'gbp_flowclassifier'
+GBP_PORT = 'gbp_port'
+GBP_NETWORK_VRF = 'gbp_network_vrf'
+GBP_NETWORK_EPG = 'gbp_network_epg'
+GBP_NETWORK_LINK = 'gbp_network_link'
