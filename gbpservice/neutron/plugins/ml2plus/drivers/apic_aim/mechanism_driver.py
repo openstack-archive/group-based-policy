c835b51e299d131144d4937a2b7a14b4c00efe33
Ivar Lazzaro <ivarlazzaro@gmail.com>
2018-05-16 11:22:37 -0700

    [AIM] Make topology RPC version consistent
    Also, remove force send as it's not useful since we changed the
    agent logic from cast to call.
    
    Change-Id: Ic3e608e1aea8a24f8299ee2b8765cce6a55c1fad

--- gbpservice/neutron/plugins/ml2plus/drivers/apic_aim/mechanism_driver.py
+++ gbpservice/neutron/plugins/ml2plus/drivers/apic_aim/mechanism_driver.py
@@ -177,7 +177,7 @@ class ApicMechanismDriver(api_plus.MechanismDriver,
                           db.DbMixin):
 
     class TopologyRpcEndpoint(object):
-        target = oslo_messaging.Target(version='3.0')
+        target = oslo_messaging.Target(version=arpc.VERSION)
 
         def __init__(self, mechanism_driver):
             self.md = mechanism_driver
@@ -2042,8 +2042,7 @@ class ApicMechanismDriver(api_plus.MechanismDriver,
 
     # Topology RPC method handler
     def update_link(self, context, host, interface, mac,
-                    switch, module, port, pod_id='1', port_description='',
-                    force=False):
+                    switch, module, port, pod_id='1', port_description=''):
         LOG.debug('Topology RPC: update_link: %s',
                   ', '.join([str(p) for p in
                              (host, interface, mac, switch, module, port,
@@ -2057,7 +2056,7 @@ class ApicMechanismDriver(api_plus.MechanismDriver,
             hlink = self.aim.get(aim_ctx,
                                  aim_infra.HostLink(host_name=host,
                                                     interface_name=interface))
-            if hlink and hlink.path == port_description and not force:
+            if hlink and hlink.path == port_description:
                 # There was neither a change nor a refresh required.
                 return
             # Create or Update hostlink in AIM
