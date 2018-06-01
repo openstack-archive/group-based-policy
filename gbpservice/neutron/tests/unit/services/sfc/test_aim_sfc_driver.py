55e931415c9ca8db6bf1d3ec0d3bc7477bec375d
Ivar Lazzaro <ivarlazzaro@gmail.com>
2018-06-01 10:43:26 -0700

    [SFC][AIM] Safe access to keys in the port pair groups extension
    Change-Id: I3842998b719fc348c5cc42e33b525a4da994b863

--- gbpservice/neutron/tests/unit/services/sfc/test_aim_sfc_driver.py
+++ gbpservice/neutron/tests/unit/services/sfc/test_aim_sfc_driver.py
@@ -722,6 +722,28 @@ class TestPortPairGroup(TestAIMServiceFunctionChainingBase):
             ppg2['id'], port_pairs=[pp3['id'], pp1['id']],
             expected_res_status=500)
 
+    def test_ppg_update(self):
+        # Correct creation
+        net1 = self._make_network(self.fmt, 'net1', True)
+        self._make_subnet(self.fmt, net1, '192.168.0.1', '192.168.0.0/24')
+        net2 = self._make_network(self.fmt, 'net2', True)
+        self._make_subnet(self.fmt, net2, '192.168.1.1', '192.168.1.0/24')
+
+        # Service 1
+        p11 = self._make_port(self.fmt, net1['network']['id'])['port']
+        self._bind_port_to_host(p11['id'], 'h1')
+        p12 = self._make_port(self.fmt, net2['network']['id'])['port']
+        self._bind_port_to_host(p12['id'], 'h1')
+        pp1 = self.create_port_pair(ingress=p11['id'], egress=p12['id'],
+                                    expected_res_status=201)['port_pair']
+        # This goes through
+        ppg1 = self.create_port_pair_group(
+            port_pairs=[pp1['id']],
+            expected_res_status=201)['port_pair_group']
+        # Same ID update works
+        self.update_port_pair_group(ppg1['id'], port_pairs=[pp1['id']],
+                                    expected_res_status=200)
+
     def test_healthcheck_group(self):
         # Correct creation
         net1 = self._make_network(self.fmt, 'net1', True)
