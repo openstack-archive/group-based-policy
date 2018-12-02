03f743b2af6c4710bf55dc04bc0e5b12a70469bd
Thomas Bachman <tbachman@yahoo.com>
2018-12-02 13:16:52 +0000

    [DO NOT MERGE]: Testing
    Change-Id: If12df13c917b51770fbd59ead5a5a046471bf40b

--- gbpservice/neutron/tests/unit/plugins/ml2plus/__init__.py
+++ gbpservice/neutron/tests/unit/plugins/ml2plus/__init__.py
@@ -16,9 +16,9 @@
 # The following are imported at the beginning to ensure
 # that the patches are applied before any of the
 # modules save a reference to the functions being patched
-from gbpservice.neutron.plugins.ml2plus import patch_neutron  # noqa
-
 from gbpservice.neutron.extensions import patch  # noqa
+
+from gbpservice.neutron.plugins.ml2plus import patch_neutron  # noqa
 
 from oslo_db.sqlalchemy import utils as sa_utils
 
