5953996f483b0bd17a5c384af30b62503286c250
Robert Kukura <kukura@noironetworks.com>
2019-01-24 11:48:03 -0500

    [AIM] New streamlined Opflex RPC handler
    The Opflex request_endpoint_details RPC handler is reimplemented in
    the apic_aim mechanism driver, using a minimal set of baked DB
    queries. These queries return specific individual column data rather
    than entire ORM objects, eliminating the sqlalchemy and DB server
    overhead for relationship processing. Joins are used to minimize the
    number of queries made to the DB server. Rather than using or
    emulating the dictionaries returned by Neutron's get_<resource>()
    methods, the RPC response is built directly from the data returned
    from the queries.
    
    The older RPC handler implementations remain during testing to allow
    performance and scalability comparison. A single implementation will
    eventually be selected and the others removed. Until then, the new RPC
    handler is enabled by setting the enable_new_rpc config variable.
    
    (cherry picked from commit c71c3c4dcd3e216bc7b6ff04357cdc20dbeca327)
    Conflicts:
    	gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/aim_mapping.py
    	gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/aim_mapping_rpc.py
    	gbpservice/neutron/tests/unit/services/grouppolicy/test_aim_mapping_driver.py
    
    Change-Id: I614d5bca3f101ceab06e2fad1a59f5514b438473

--- gbpservice/neutron/plugins/ml2plus/drivers/apic_aim/constants.py
+++ gbpservice/neutron/plugins/ml2plus/drivers/apic_aim/constants.py
@@ -10,8 +10,20 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from neutron_lib import constants as n_constants
+
 GBP_FLOW_CLASSIFIER = 'gbp_flowclassifier'
 GBP_PORT = 'gbp_port'
 GBP_NETWORK_VRF = 'gbp_network_vrf'
 GBP_NETWORK_EPG = 'gbp_network_epg'
 GBP_NETWORK_LINK = 'gbp_network_link'
+
+DEVICE_OWNER_SNAT_PORT = 'apic:snat-pool'
+DEVICE_OWNER_SVI_PORT = 'apic:svi'
+
+IPV4_ANY_CIDR = '0.0.0.0/0'
+IPV4_METADATA_CIDR = '169.254.169.254/16'
+
+PROMISCUOUS_TYPES = [n_constants.DEVICE_OWNER_DHCP,
+                     n_constants.DEVICE_OWNER_LOADBALANCER]
+PROMISCUOUS_SUFFIX = 'promiscuous'
