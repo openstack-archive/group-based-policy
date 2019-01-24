d10fc0b56f51c77456e33519e6da10a50b25ca72
Robert Kukura <kukura@noironetworks.com>
2019-01-24 11:48:03 -0500

    [AIM] New streamlined Opflex RPC handler
    The Opflex request_endpoint_details RPC handler is reimplemented in
    the apic_aim mechanism driver, using a minimal set of baked DB
    queries. These queries return specific individual column data rather
    than entire ORM objects, eliminating the sqlalchemy and DB server
    overhead for relationship processing. Joins are used to minimize the
    number of queries made to the DB server. Rather than using or
    emulating the dictionaries returned by Neutron's get_<resource>()
    methods, the RPC response is built directly from the data returned
    from the queries.
    
    The older RPC handler implementations remain during testing to allow
    performance and scalability comparison. A single implementation will
    eventually be selected and the others removed. Until then, the new RPC
    handler is enabled by setting the enable_new_rpc config variable.
    
    (cherry picked from commit c71c3c4dcd3e216bc7b6ff04357cdc20dbeca327)
    Conflicts:
    	gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/aim_mapping.py
    	gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/aim_mapping_rpc.py
    	gbpservice/neutron/tests/unit/services/grouppolicy/test_aim_mapping_driver.py
    
    Change-Id: I614d5bca3f101ceab06e2fad1a59f5514b438473

---
+++ gbpservice/neutron/services/grouppolicy/drivers/cisco/apic/config.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2019 Cisco Systems Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_config import cfg
+
+
+# REVISIT: Auto-PTG is currently config driven to align with the
+# config driven behavior of the older driver but is slated for
+# removal.
+opts = [
+    cfg.BoolOpt('create_auto_ptg',
+                default=True,
+                help=_("Automatically create a PTG when a L2 Policy "
+                       "gets created. This is currently an aim_mapping "
+                       "policy driver specific feature.")),
+    cfg.BoolOpt('create_per_l3p_implicit_contracts',
+                default=True,
+                help=_("This configuration is set to True to migrate a "
+                       "deployment that has l3_policies without implicit "
+                       "AIM contracts (these are deployments which have "
+                       "AIM implicit contracts per tenant). A Neutron server "
+                       "restart is required for this configuration to take "
+                       "effect. The creation of the implicit contracts "
+                       "happens at the time of the AIM policy driver "
+                       "initialization. The configuration can be set to "
+                       "False to avoid recreating the implicit contracts "
+                       "on subsequent Neutron server restarts. This "
+                       "option will be removed in the O release")),
+]
+
+
+cfg.CONF.register_opts(opts, "aim_mapping")
